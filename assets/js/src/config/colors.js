94be663b9dda5de21627f8379ca9844526add7cc
Sanjeev Shrestha <sethstha@gmail.com>
2020-09-11 06:58:04 +0545

    Add  header component and colors

---
+++ assets/js/src/config/colors.js
@@ -0,0 +1,17 @@
+const colors = {
+	PRIMARY: '#787DFF',
+	SECONDARY: '#FD739C',
+	HEADING: '#07092F',
+	TEXT: '#424360',
+	LIGHT_TEXT: '#7C7D8F',
+	PLACEHOLDER: '#ACACBE',
+	INPUT_BORDER: '#DEDFE8',
+	BORDER: '#F2F3FA',
+	LIGHT_BLUEISH_GRAY: '#FAFBFF',
+	LIGHT_GRAY: '#f9f9f9',
+	LIGHT: '#FFFFFF',
+	ALERT_BORDER: '#FFA8A8',
+	ALERT: '#ff8787',
+};
+
+export default colors;

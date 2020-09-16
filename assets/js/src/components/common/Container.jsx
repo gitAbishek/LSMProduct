5f54f645568eb12aea09a6cc43df803afcf214b6
Sanjeev Shrestha <sethstha@gmail.com>
2020-09-16 22:05:47 +0545

    Add - container component

---
+++ assets/js/src/components/common/Container.jsx
@@ -0,0 +1,37 @@
+const { Children } = require('@wordpress/element/build-types');
+
+import { React } from '@wordpress/element';
+import styled from 'styled-components';
+import { BaseLine } from '../../config/defaultStyle';
+
+const Container = styled.div`
+	padding-right: ${BaseLine * 2}px;
+	padding-left: ${BaseLine * 2}px;
+
+	@media (min-width: 576px) {
+		max-width: 540px;
+	}
+
+	@media (min-width: 768px) {
+		max-width: 720px;
+	}
+
+	@media (min-width: 992px) {
+		max-width: 960px;
+	}
+
+	@media (min-width: 1200px) {
+		max-width: 1140px;
+	}
+
+	@media (min-width: 1400px) {
+		max-width: 1320px;
+	}
+`;
+
+export const ContainerFluid = styled.div`
+	padding-right: ${BaseLine * 2}px;
+	padding-left: ${BaseLine * 2}px;
+`;
+
+export default Container;

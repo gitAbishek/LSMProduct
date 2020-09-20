0c8b4b071fb11f09c289412c344b23560026a65c
Sanjeev Shrestha <sethstha@gmail.com>
2020-09-20 17:42:58 +0545

    Add - course page

--- assets/js/src/screens/course/Course.jsx
+++ assets/js/src/screens/course/Course.jsx
@@ -10,15 +10,18 @@ import Label from '../../components/common/Label';
 import Textarea from '../../components/common/Textarea';
 import Select from '../../components/common/Select';
 import ImageUpload from '../../components/common/ImageUpload';
+import styled from 'styled-components';
+import { BaseLine } from '../../config/defaultStyle';
+import Button from '../../components/common/Button';
 
 const Course = () => {
 	return (
 		<Fragment>
 			<MainToolbar />
 			<MainLayout>
-				<FlexRow align="flex-start">
-					<Flex style={{ width: 'calc(60% - 16px)' }}>
-						<form action="">
+				<form action="">
+					<CourseContainer>
+						<CourseLeftContainer>
 							<FormGroup>
 								<Label htmlFor="">Course Title</Label>
 								<Input placeholder="Your Course Title"></Input>
@@ -26,26 +29,17 @@ const Course = () => {
 
 							<FormGroup>
 								<Label htmlFor="">Course Description</Label>
-								<Textarea placeholder="Your Course Title"></Textarea>
+								<Textarea placeholder="Your Course Title" rows="5"></Textarea>
 							</FormGroup>
-							<FormGroup>
-								<Label htmlFor="">Categories</Label>
-								<ImageUpload title="Select files or click to upload" />
-							</FormGroup>
-						</form>
-					</Flex>
+							<FlexRow>
+								<Button type="primary">Start Adding Lessons</Button>
+							</FlexRow>
+						</CourseLeftContainer>
 
-					<Flex style={{ width: 'calc(40% - 16px)', marginLeft: 32 }}>
-						<form action="">
+						<CourseRightContainer>
 							<FormGroup>
-								<Label htmlFor="">Course Title</Label>
-								<Input placeholder="Your Course Title"></Input>
-							</FormGroup>
-
-							<FormGroup>
-								<Label htmlFor="">Categories</Label>
+								<Label htmlFor="">Course Category</Label>
 								<Select
-									isDisabled
 									options={[
 										{ value: 'chocolate', label: 'Chocolate' },
 										{ value: 'strawberry', label: 'Strawberry' },
@@ -53,12 +47,42 @@ const Course = () => {
 									]}
 								/>
 							</FormGroup>
-						</form>
-					</Flex>
-				</FlexRow>
+
+							<FormGroup>
+								<Label htmlFor="">Categories</Label>
+								<ImageUpload title="Drag image or click to upload" />
+								<FeaturedImageActions>
+									<Button>Remove Featured Image</Button>
+									<Button type="primary">Add New</Button>
+								</FeaturedImageActions>
+							</FormGroup>
+						</CourseRightContainer>
+					</CourseContainer>
+				</form>
 			</MainLayout>
 		</Fragment>
 	);
 };
 
+const CourseContainer = styled(FlexRow)`
+	align-items: flex-start;
+	margin-left: -${BaseLine * 2}px;
+	margin-right: -${BaseLine * 2}px;
+`;
+
+const CourseInner = styled(Flex)`
+	padding-left: ${BaseLine * 2}px;
+	padding-right: ${BaseLine * 2}px;
+`;
+const CourseLeftContainer = styled(CourseInner)`
+	flex: 1;
+`;
+
+const CourseRightContainer = styled(CourseInner)`
+	flex-basis: 400px;
+`;
+const FeaturedImageActions = styled(FlexRow)`
+	justify-content: space-between;
+	margin-top: ${BaseLine * 3}px;
+`;
 export default Course;

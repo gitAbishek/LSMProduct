b4948804239112de8a9030e74ee12176934b5e55
Sanjeev Shrestha <sethstha@gmail.com>
2020-09-29 07:13:38 +0545

    Only show getting started on first time

--- assets/js/src/screens/coursebuilder/CourseBuilder.jsx
+++ assets/js/src/screens/coursebuilder/CourseBuilder.jsx
@@ -1,15 +1,16 @@
-import { Fragment, React } from '@wordpress/element';
+import { Fragment, React, useState } from '@wordpress/element';
 
 import MainLayout from '../layouts/MainLayout';
 import MainToolbar from '../layouts/MainToolbar';
 
 import GettingStarted from './components/GettingStarted';
 const Dashboard = () => {
+	const [courseMode, setCourseMode] = useState('gettingStarted');
 	return (
 		<Fragment>
 			<MainToolbar />
 			<MainLayout>
-				<GettingStarted />
+				{courseMode === 'gettingStarted' && <GettingStarted />}
 				{/* <SortableSection /> */}
 			</MainLayout>
 		</Fragment>

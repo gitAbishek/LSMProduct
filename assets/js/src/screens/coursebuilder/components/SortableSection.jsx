4e1cf75b5fdb67a55d0bb77b00b07aee1fcc91f5
Sanjeev Shrestha <sethstha@gmail.com>
2020-10-03 17:49:54 +0545

    Add - working prototype for react beutiful dnd

--- assets/js/src/screens/coursebuilder/components/SortableSection.jsx
+++ assets/js/src/screens/coursebuilder/components/SortableSection.jsx
@@ -1,109 +1,52 @@
 import { React, useState } from '@wordpress/element';
 import { BiAlignLeft, BiCopy, BiTrash } from 'react-icons/bi';
-import {
-	SortableContainer,
-	SortableElement,
-	SortableHandle,
-} from 'react-sortable-hoc';
-import styled from 'styled-components';
-import FlexRow from '../../../components/common/FlexRow';
-import colors from '../../../config/colors';
-import { BaseLine } from '../../../config/defaultStyle';
-import Sortable from '../../../assets/icons/Sortable';
-import arrayMove from 'array-move';
-import fontSize from '../../../config/fontSize';
-import Icon from '../../../components/common/Icon';
-import Tooltip from 'rc-tooltip';
-import Flex from '../../../components/common/Flex';
-import Container from '../../../components/common/Container';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const SortableSection = () => {
-	const [items, setItem] = useState([
-		'Section 1',
-		'Section 2',
-		'Section 3',
-		'Section 4',
-		'Section 5',
-		'Section 6',
+	const [items, setItems] = useState([
+		{
+			id: '1122',
+			label: 'This is label',
+			datas: ['First Data', 'Second data'],
+		},
+		{
+			id: '1111',
+			label: 'Second Droppable',
+			data: ['second data one', 'second data 2'],
+		},
 	]);
 
-	const DragHandle = SortableHandle(() => (
-		<SortableIcon icon={<Sortable />}></SortableIcon>
-	));
-
-	const SortableSectionContainer = SortableContainer(({ children }) => {
-		return <StyledSortable>{children}</StyledSortable>;
-	});
-
-	const SortableItem = SortableElement(({ value }) => (
-		<SortablesectionItem>
-			<FlexRow>
-				<DragHandle />
-				<span>{value}</span>
-			</FlexRow>
-
-			<FlexRow>
-				<Tooltip placement="topRight" overlay="Duplicate">
-					<Icon icon={<BiCopy />}></Icon>
-				</Tooltip>
-				<Tooltip placement="topRight" overlay="Delete">
-					<Icon icon={<BiTrash />}></Icon>
-				</Tooltip>
-			</FlexRow>
-		</SortablesectionItem>
-	));
-
-	const onSortStart = () => {
-		document.body.style.cursor = 'move';
-	};
-	const onSortEnd = ({ oldIndex, newIndex }) => {
-		document.body.style.cursor = 'default';
-		setItem(arrayMove(items, oldIndex, newIndex));
+	const onDragEnd = () => {
+		return;
 	};
 
 	return (
-		<Container>
-			<SortableSectionContainer
-				onSortStart={onSortStart}
-				onSortEnd={onSortEnd}
-				useDragHandle
-				helperClass="sortable-dragging">
-				{items.map((value, index) => (
-					<SortableItem key={`item-${value}`} index={index} value={value} />
-				))}
-			</SortableSectionContainer>
-		</Container>
+		<DragDropContext onDragEnd={onDragEnd}>
+			<Droppable droppableId="uniqueID">
+				{(dropProvided) => (
+					<div
+						ref={dropProvided.innerRef}
+						{...dropProvided.draggableProps}
+						{...dropProvided.dragHandleProps}>
+						{items.map((item, index) => (
+							<Draggable key={item.id} draggableId={item.id} index={index}>
+								{(dragProvided) => (
+									<span
+										ref={dragProvided.innerRef}
+										{...dragProvided.draggableProps}
+										{...dragProvided.dragHandleProps}>
+										{item.label}
+									</span>
+								)}
+							</Draggable>
+						))}
+
+						{dropProvided.placeholder}
+					</div>
+				)}
+			</Droppable>
+		</DragDropContext>
 	);
 };
-
-const StyledSortable = styled.ul`
-	margin: 0;
-	padding: 0;
-	list-style-type: none;
-
-	li {
-		margin-bottom: ${BaseLine * 1.5}px;
-	}
-`;
-
-const SortablesectionItem = styled(Flex)`
-	background-color: ${colors.WHITE};
-	padding: ${BaseLine * 5}px;
-	margin-bottom: ${BaseLine * 4}px;
-`;
-
-const SortableIcon = styled(Icon)`
-	margin-right: ${BaseLine * 2}px;
-	color: ${colors.DISABLED};
-	font-size: ${fontSize.EXTRA_LARGE};
-
-	&:hover {
-		cursor: move;
-	}
-
-	&:active {
-		cursor: move;
-	}
-`;
 
 export default SortableSection;

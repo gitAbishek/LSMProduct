803c376a88ade71d59b33722c62573565be8a9ff
Sanjeev Shrestha <sethstha@gmail.com>
2020-10-10 18:37:22 +0545

    Add - action button for content

--- assets/js/src/screens/coursebuilder/components/content/Lesson.jsx
+++ assets/js/src/screens/coursebuilder/components/content/Lesson.jsx
@@ -1,10 +1,21 @@
 import { React } from '@wordpress/element';
 import PropTypes from 'prop-types';
+import Dropdown from 'rc-dropdown';
 import { Draggable } from 'react-beautiful-dnd';
-import { BiAlignLeft } from 'react-icons/bi';
+import { BiAlignLeft, BiTrash } from 'react-icons/bi';
+import Button from '../../../../components/common/Button';
+import DropdownOverlay from '../../../../components/common/DropdownOverlay';
 import FlexRow from '../../../../components/common/FlexRow';
+import Icon from '../../../../components/common/Icon';
+import OptionButton from '../../../../components/common/OptionButton';
 import DragHandle from '../DragHandle';
-import { ContentContainer, ContentIcon, ContentTitle } from '../styled';
+import {
+	ActionContainer,
+	ContentContainer,
+	ContentHeader,
+	ContentIcon,
+	ContentTitle,
+} from '../styled';
 
 const Lesson = (props) => {
 	const { id, title, index } = props;
@@ -15,11 +26,33 @@ const Lesson = (props) => {
 					ref={provided.innerRef}
 					{...provided.draggableProps}
 					isDragging={snapshot.isDragging}>
-					<FlexRow>
-						<DragHandle {...provided.dragHandleProps} />
-						<ContentIcon icon={<BiAlignLeft />} />
-						<ContentTitle>{title}</ContentTitle>
-					</FlexRow>
+					<ContentHeader>
+						<FlexRow>
+							<DragHandle {...provided.dragHandleProps} />
+							<ContentIcon icon={<BiAlignLeft />} />
+							<ContentTitle>{title}</ContentTitle>
+						</FlexRow>
+						<FlexRow>
+							<ActionContainer>
+								<Button>Edit</Button>
+								<Dropdown
+									trigger={'click'}
+									placement={'bottomRight'}
+									overlay={
+										<DropdownOverlay>
+											<ul>
+												<li>
+													<Icon icon={<BiTrash />} />
+													Delete
+												</li>
+											</ul>
+										</DropdownOverlay>
+									}>
+									<OptionButton />
+								</Dropdown>
+							</ActionContainer>
+						</FlexRow>
+					</ContentHeader>
 				</ContentContainer>
 			)}
 		</Draggable>
--- assets/js/src/screens/coursebuilder/components/styled.js
+++ assets/js/src/screens/coursebuilder/components/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import FlexRow from '../../../components/common/FlexRow';
 import Icon from '../../../components/common/Icon';
 import colors from '../../../config/colors';
 import defaultStyle, { BaseLine } from '../../../config/defaultStyle';
@@ -9,9 +10,14 @@ export const ContentContainer = styled.div`
 	box-shadow: ${(props) =>
 		props.isDragging ? '0 0 15px rgba(0, 0, 0, 0.1)' : 'none'};
 	border: 1px solid ${colors.BORDER};
-	padding: ${BaseLine * 2}px;
+	padding: ${BaseLine * 1.3}px ${BaseLine * 2}px;
 	border-radius: ${defaultStyle.borderRadius};
 	margin-bottom: ${BaseLine * 2}px;
+`;
+
+export const ContentHeader = styled.header`
+	display: flex;
+	justify-content: space-between;
 `;
 
 export const ContentTitle = styled.h5`
@@ -25,4 +31,10 @@ export const ContentIcon = styled(Icon)`
 	font-size: ${fontSize.HUGE};
 	margin-right: ${BaseLine}px;
 	color: ${colors.TEXT};
+`;
+
+export const ActionContainer = styled(FlexRow)`
+	button {
+		margin-left: ${BaseLine * 2}px;
+	}
 `;

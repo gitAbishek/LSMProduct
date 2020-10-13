773f171a72c76b53c5ee9176b2af85942d95591b
Sagar Tamang <mi5t4n@gmail.com>
2020-10-13 18:54:40 +0545

    Fix: Merge conflict texts

--- includes/Taxonomy/TaxonomyFactory.php
+++ includes/Taxonomy/TaxonomyFactory.php
@@ -17,8 +17,6 @@ class TaxonomyFactory {
 	 * @return ThemeGrill\Masteriyo\Taxonomy\Taxonomy;
 	 */
 	public static function create( $taxonomy, $labels = array(), $args = array() ) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 		if ( 'course_cat' === $taxonomy) {
 			return new Course\Categories();
 		} elseif ( 'course_tag' === $taxonomy ) {
@@ -29,23 +27,6 @@ class TaxonomyFactory {
 			return new Lesson\Categories();
 		} elseif ( 'lesson_tag' === $taxonomy ) {
 			return new Lesson\Tags();
-=======
-=======
->>>>>>> 0bc26be7c6b6c16d54b8cdf2f14a12be7a63874a
-		if ( 'courses_cat' === $taxonomy) {
-			return new Courses\Categories();
-		} elseif ( 'courses_tag' === $taxonomy ) {
-			return new Courses\Tags();
-		} elseif ( 'courses_difficulty' === $taxonomy ) {
-			return new Courses\Difficulties();
-		} elseif ( 'lessons_cat' === $taxonomy) {
-			return new Lessons\Categories();
-		} elseif ( 'lessons_tag' === $taxonomy ) {
-			return new Lessons\Tags();
-<<<<<<< HEAD
->>>>>>> ab1212c... Register taxonomies
-=======
->>>>>>> 0bc26be7c6b6c16d54b8cdf2f14a12be7a63874a
 		}
 	}
 }

77f2d046c48b0fa5d7c49929eddd763fb14181a6
Sagar Tamang <mi5t4n@gmail.com>
2020-11-09 09:11:20 +0545

    Added rest api initializer

---
+++ includes/RestApi/RestApi.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Initialize REST API.
+ *
+ * @since 0.1.0
+ *
+ * @package  ThemeGrill\Masteriyo\RestApi
+ */
+
+namespace ThemeGrill\Masteriyo\RestApi;
+
+defined( 'ABSPATH' ) || exit;
+
+use ThemeGrill\Masteriyo\Traits\Singleton;
+
+class RestApi {
+	use Singleton;
+
+	/**
+	 * REST API namespaces and endpoints.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @var array
+	 *
+	 */
+	protected $controllers = array();
+
+	/**
+	 * Hook into WordPress ready to init the REST API as needed.
+	 *
+	 * @since 0.1.0
+	 */
+	 public function init() {
+		 add_action( 'rest_api_init', array( $this, 'register_rest_routes' ) );
+	 }
+
+	/**
+	 * Register REST API routes.
+	 *
+	 * @since 0.1.0
+	 */
+	public function register_rest_routes() {
+		foreach ( $this->get_rest_namespaces() as $namespace => $controllers ) {
+			foreach ( $controllers as $controller_name => $controller_class ) {
+				$this->controllers[ $namespace ][ $controller_name ] = new $controller_class();
+				$this->controllers[ $namespace ][ $controller_name ]->register_routes();
+			}
+		}
+	}
+
+	 /**
+	 * Get API namespaces - new namespaces should be registered here.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array List of Namespaces and Main controller classes.
+	 */
+	protected function get_rest_namespaces() {
+		return apply_filters(
+			'masteriyo_rest_api_get_rest_namespaces',
+			array(
+				'masteriyo/v1' => $this->get_v1_controllers(),
+			)
+		);
+	}
+
+	/**
+	 * List of controllers in the masteriyo/v1 namespace.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	protected function get_v1_controllers() {
+		return array(
+			'course-categories' => '\\ThemeGrill\\Masteriyo\\RestApi\\Controllers\\Version1\\CourseCategoriesController'
+		);
+	}
+
+	/**
+	 * Return the path to the package.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return string
+	 */
+	public static function get_path() {
+		return dirname( __DIR__ );
+	}
+}

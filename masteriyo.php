d42b47070bbaf1139c573c52269c0d2618148d83
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-11-25 13:17:23 +0545

    Added MASTERIYO_TEMPLATE_DEBUG_MODE constant

--- masteriyo.php
+++ masteriyo.php
@@ -32,6 +32,7 @@ define( 'MASTERIYO_PLUGIN_FILE', __FILE__ );
 define( 'MASTERIYO_PLUGIN_DIR', dirname( __FILE__ ) );
 define( 'MASTERIYO_ASSETS', dirname( __FILE__ ) . '/assets' );
 define( 'MASTERIYO_TEMPLATES', dirname( __FILE__ ) . '/templates' );
+define( 'MASTERIYO_TEMPLATE_DEBUG_MODE', false );
 define( 'MASTERIYO_LANGUAGES', dirname( __FILE__ ) . '/i18n/languages' );
 
 require_once dirname( __FILE__ ) . '/vendor/autoload.php';
@@ -63,7 +64,8 @@ $masteriyo_container->add(
 $masteriyo_container->add( 'section', \ThemeGrill\Masteriyo\Models\Section::class )
 	->addArgument( \ThemeGrill\Masteriyo\Repository\SectionRepository::class );
 
-$masteriyo_container->add( 'section_repository',
+$masteriyo_container->add(
+	'section_repository',
 	function() {
 		return apply_filters( 'masteriyo_section_repository', new SectionRepository() );
 	}
@@ -78,13 +80,15 @@ $masteriyo_container->add(
 $masteriyo_container->add( 'lesson', \ThemeGrill\Masteriyo\Models\Lesson::class )
 	->addArgument( \ThemeGrill\Masteriyo\Repository\LessonRepository::class );
 
-$masteriyo_container->add( \ThemeGrill\Masteriyo\Repository\LessonRepository::class,
+$masteriyo_container->add(
+	\ThemeGrill\Masteriyo\Repository\LessonRepository::class,
 	function() {
 		return apply_filters( 'masteriyo_lesson_repository', new LessonRepository() );
 	}
 );
 
-$masteriyo_container->add( 'lesson_repository',
+$masteriyo_container->add(
+	'lesson_repository',
 	function() {
 		return apply_filters( 'masteriyo_lesson_repository', new LessonRepository() );
 	}
@@ -93,7 +97,8 @@ $masteriyo_container->add( 'lesson_repository',
 $masteriyo_container->add( 'quiz', \ThemeGrill\Masteriyo\Models\Quiz::class )
 	->addArgument( \ThemeGrill\Masteriyo\Repository\QuizRepository::class );
 
-$masteriyo_container->add( 'quiz_repository',
+$masteriyo_container->add(
+	'quiz_repository',
 	function() {
 		return apply_filters( 'masteriyo_quiz_repository', new QuizRepository() );
 	}

b66a4703eb98fb68e6e362da51c440834b642d43
Sanjeev Shrestha <sethstha@gmail.com>
2021-02-20 20:21:43 +0545

    Add - flex component in typescript

--- assets/js/src/components/common/Flex.tsx
+++ assets/js/src/components/common/Flex.tsx
@@ -1,13 +1,50 @@
 import styled from 'styled-components';
 
+interface StyledProps {
+	flex?: number;
+	direction?:
+		| 'row'
+		| 'row-reverse'
+		| 'column'
+		| 'column-reverse'
+		| 'initial'
+		| 'inherit';
+	justify?:
+		| 'flex-start'
+		| 'flex-end'
+		| 'center'
+		| 'space-between'
+		| 'space-around'
+		| 'space-evenly'
+		| 'initial'
+		| 'inherit';
+	align?:
+		| 'strecth'
+		| 'center'
+		| 'flex-start'
+		| 'flex-end'
+		| 'baseline'
+		| 'initial'
+		| 'inherit';
+	wrap?: 'wrap' | 'nowrap';
+	alignSelf?:
+		| 'auto'
+		| 'strecth'
+		| 'center'
+		| 'flex-start'
+		| 'flex-end'
+		| 'baseline'
+		| 'initial'
+		| 'inherit';
+}
 const Flex = styled.div`
 	display: flex;
-	flex: ${(props) => props.flex};
-	flex-direction: ${(props) => props.direction || 'column'};
-	align-items: ${(props) => props.align || null};
-	justify-content: ${(props) => props.justify || null};
-	flex-wrap: ${(props) => props.wrap || null};
-	align-self: ${(props) => props.alignSelf || null};
+	flex: ${(props: StyledProps) => props.flex || null};
+	flex-direction: ${(props: StyledProps) => props.direction || 'column'};
+	align-items: ${(props: StyledProps) => props.align || null};
+	justify-content: ${(props: StyledProps) => props.justify || null};
+	flex-wrap: ${(props: StyledProps) => props.wrap || null};
+	align-self: ${(props: StyledProps) => props.alignSelf || null};
 `;
 
 export default Flex;

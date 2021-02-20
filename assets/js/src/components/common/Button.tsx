b0ea41d723d9687bfc05bc4f5591b3e1b079180a
Sanjeev Shrestha <sethstha@gmail.com>
2021-02-20 20:12:06 +0545

    Make better button component

--- assets/js/src/components/common/Button.tsx
+++ assets/js/src/components/common/Button.tsx
@@ -2,27 +2,30 @@ import defaultStyle, { BaseLine } from 'Config/defaultStyle';
 import styled, { css } from 'styled-components';
 
 import Icon from './Icon';
-import PropTypes from 'prop-types';
-import { React } from '@wordpress/element';
+import React from 'react';
 import colors from 'Config/colors';
 import fontSize from 'Config/fontSize';
 import { lighten } from 'polished';
 
-const Button = (props) => {
-	const { icon, type, size, children } = props;
+interface Props {
+	icon?: any;
+	appearance?: 'default' | 'primary' | 'secondary';
+	size?: 'small' | 'medium' | 'large';
+}
+
+const Button: React.FC<Props> = (props) => {
+	const { icon, appearance, size } = props;
 	return (
-		<StyledButton type={type} size={size} {...props}>
+		<StyledButton appearance={appearance} size={size} {...props}>
 			{icon && <Icon icon={icon} />}
-			<span>{children}</span>
+			<span>{props.children}</span>
 		</StyledButton>
 	);
 };
 
-Button.propTypes = {
-	icon: PropTypes.object,
-	type: PropTypes.string,
-	size: PropTypes.string,
-	children: PropTypes.any,
+Button.defaultProps = {
+	appearance: 'default',
+	size: 'medium',
 };
 
 export default Button;
@@ -30,13 +33,10 @@ export default Button;
 const StyledButton = styled.button`
 	cursor: pointer;
 	transition: all 0.35s ease-in-out;
-	border: 1px solid ${colors.BORDER};
-	padding: 12px 16px;
+	border: 1px solid transparent;
 	font-weight: 500;
 	font-size: ${fontSize.SMALL};
 	border-radius: ${defaultStyle.borderRadius};
-	background-color: ${colors.WHITE};
-	color: ${colors.TEXT};
 	line-height: 1;
 	display: flex;
 	align-items: center;
@@ -46,45 +46,60 @@ const StyledButton = styled.button`
 		margin-right: ${BaseLine}px;
 	}
 
-	/* Button Types */
-	${(props) =>
-		(props.primary &&
-			css`
-				color: ${colors.WHITE};
-				border-color: ${colors.PRIMARY};
-				background-color: ${colors.PRIMARY};
+	${(props: Props) =>
+		props.appearance === 'default' &&
+		css`
+			color: ${colors.TEXT};
+			border-color: ${colors.BORDER};
+			background-color: ${colors.WHITE};
 
-				&:hover {
-					background-color: ${lighten(0.05, colors.PRIMARY)};
-				}
-			`) ||
-		(props.secondary &&
-			css`
-				color: ${colors.WHITE};
+			&:hover {
+				background-color: ${lighten(0.05, colors.PRIMARY)};
+			}
+		`}
+
+	${(props: Props) =>
+		props.appearance === 'primary' &&
+		css`
+			color: ${colors.WHITE};
+			border-color: ${colors.PRIMARY};
+			background-color: ${colors.PRIMARY};
+
+			&:hover {
+				background-color: ${lighten(0.05, colors.PRIMARY)};
+			}
+		`}
+
+	${(props: Props) =>
+		props.appearance === 'secondary' &&
+		css`
+			color: ${colors.WHITE};
 				border-color: ${colors.SECONDARY};
 				background-color: ${colors.SECONDARY};
 
 				&:hover {
 					background-color: ${lighten(0.05, colors.SECONDARY)};
 				}
-			`) ||
-		css`
-			&:hover {
-				color: ${colors.PRIMARY};
-				border-color: ${colors.PRIMARY};
 			}
-		`};
+		`}
+		
+	${(props: Props) =>
+		props.size === 'medium' &&
+		css`
+			padding: 12px 16px;
+		`}
 
-	/* Button Size */
-	${(props) =>
-		(props.size === 'small' &&
-			css`
-				padding: 6px 8px;
-				font-size: ${fontSize.SMALL};
-			`) ||
-		(props.size === 'large' &&
-			css`
-				padding: 14px 16px;
-				font-size: ${fontSize.LARGE};
-			`)}
+	${(props: Props) =>
+		props.size === 'small' &&
+		css`
+			padding: 6px 8px;
+			font-size: ${fontSize.SMALL};
+		`}
+	
+	${(props: Props) =>
+		props.size === 'large' &&
+		css`
+			padding: 14px 16px;
+			font-size: ${fontSize.LARGE};
+		`}
 `;

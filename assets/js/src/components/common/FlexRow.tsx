2b5951a3a584d1591de317f2b2435be4c2727613
Sanjeev Shrestha <sethstha@gmail.com>
2021-02-20 20:24:10 +0545

    Add type declaration on flexrow

--- assets/js/src/components/common/FlexRow.tsx
+++ assets/js/src/components/common/FlexRow.tsx
@@ -1,12 +1,44 @@
 import styled from 'styled-components';
 
+interface StyledProps {
+	flex?: number;
+	width?: string;
+	justify?:
+		| 'flex-start'
+		| 'flex-end'
+		| 'center'
+		| 'space-between'
+		| 'space-around'
+		| 'space-evenly'
+		| 'initial'
+		| 'inherit';
+	align?:
+		| 'strecth'
+		| 'center'
+		| 'flex-start'
+		| 'flex-end'
+		| 'baseline'
+		| 'initial'
+		| 'inherit';
+	wrap?: 'wrap' | 'nowrap';
+	alignSelf?:
+		| 'auto'
+		| 'strecth'
+		| 'center'
+		| 'flex-start'
+		| 'flex-end'
+		| 'baseline'
+		| 'initial'
+		| 'inherit';
+}
+
 const FlexRow = styled.div`
 	display: flex;
-	flex: ${(props) => props.flex};
-	width: ${(props) => props.width};
-	justify-content: ${(props) => props.justify || 'inherit'};
-	align-items: ${(props) => props.align || 'center'};
-	flex-wrap: ${(props) => props.wrap || 'nowrap'};
+	flex: ${(props: StyledProps) => props.flex};
+	width: ${(props: StyledProps) => props.width};
+	justify-content: ${(props: StyledProps) => props.justify || 'inherit'};
+	align-items: ${(props: StyledProps) => props.align || 'center'};
+	flex-wrap: ${(props: StyledProps) => props.wrap || 'nowrap'};
 `;
 
 export default FlexRow;

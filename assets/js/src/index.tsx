e0f8e701e5b8a167aedb11c7aa2e0c14ff54361f
Sanjeev Shrestha <sethstha@gmail.com>
2021-02-21 09:05:58 +0545

    restructure css

--- assets/js/src/index.tsx
+++ assets/js/src/index.tsx
@@ -1,8 +1,9 @@
-import './masteriyo.css';
+import './assets/css/masteriyo.css';
 
 import App from './App';
 import React from 'react';
 import ReactDom from 'react-dom';
+
 const appRoot = document.getElementById('masteriyo');
 
 if (appRoot) {

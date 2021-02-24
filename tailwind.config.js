b793c224c5a416be0c9c29d7eb6104a402497fb0
Sanjeev Shrestha <sethstha@gmail.com>
2021-02-24 15:19:12 +0545

    Add - primary and accent color on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#78A6FF',
+					50: '#EBF2FF',
+					100: '#DEE9FF',
+					200: '#C4D8FF',
+					300: '#ABC8FF',
+					400: '#92B7FF',
+					500: '#78A6FF',
+					600: '#4584FF',
+					700: '#1263FF',
+					800: '#004CDE',
+					900: '#003AAB',
+				},
+				accent: {
+					DEFAULT: '#FD739C',
+					50: '#FFE4EC',
+					100: '#FED8E3',
+					200: '#FEBED1',
+					300: '#FEA5C0',
+					400: '#FD8CAE',
+					500: '#FD739C',
+					600: '#FC4178',
+					700: '#FC0E55',
+					800: '#D40341',
+					900: '#A20232',
+				},
+			},
+		},
 	},
 	variants: {
 		extend: {},

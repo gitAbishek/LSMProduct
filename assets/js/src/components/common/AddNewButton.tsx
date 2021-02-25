31de87bf7a3ff7dd1c0c266f584ca92ef12207c7
Sanjeev Shrestha <sethstha@gmail.com>
2021-02-25 07:18:18 +0545

    No need of classname for add new button

--- assets/js/src/components/common/AddNewButton.tsx
+++ assets/js/src/components/common/AddNewButton.tsx
@@ -1,7 +1,6 @@
 import Icon from 'Components/common/Icon';
 import { Plus } from '../../assets/icons';
 import React from 'react';
-import classNames from 'classnames';
 
 interface Props {}
 
@@ -9,9 +8,9 @@ const AddNewButton: React.FC<Props> = (props) => {
 	return (
 		<button
 			{...props}
-			className={classNames(
-				'mto-mt-8 mto-flex mto-items-center mto-cursor-pointer mto-transition-all mto-duration-300 mto-ease-in-out hover:mto-text-primary'
-			)}>
+			className="
+				mto-mt-8 mto-flex mto-items-center mto-cursor-pointer mto-transition-all mto-duration-300 mto-ease-in-out hover:mto-text-primary'
+			">
 			<Icon
 				icon={<Plus />}
 				className="mto-w-8 mto-h-8 mto-bg-primary mto-rounded-full mto-flex mto-justify-center mto-items-center mto-text-white mto-text-lg mto-mr-2"

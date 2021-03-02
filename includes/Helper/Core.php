7e3efb3f6b16e7081c39f995ff9e01d6c93418e0
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-03-02 16:04:10 +0545

    Created plain functions for respective methods in Template class

--- includes/Helper/Core.php
+++ includes/Helper/Core.php
@@ -313,3 +313,95 @@ function masteriyo_get_user( $user ) {
 
 	return apply_filters( 'masteriyo_get_user', $user_obj, $user );
 }
+
+/**
+ * Get template part.
+ *
+ * MASTERIYO_TEMPLATE_DEBUG_MODE will prevent overrides in themes from taking priority.
+ *
+ * @since 0.1.0
+ *
+ * @param mixed  $slug Template slug.
+ * @param string $name Template name (default: '').
+ */
+function masteriyo_get_part( $slug, $name = '' ) {
+	return masteriyo( 'template' )->get_part( $slug, $name );
+}
+
+/**
+ * Get other templates and include the file.
+ *
+ * @since 0.1.0
+ *
+ * @param string $template_name Template name.
+ * @param array  $args          Arguments. (default: array).
+ * @param string $template_path Template path. (default: '').
+ * @param string $default_path  Default path. (default: '').
+ */
+function masteriyo_get_template( $template_name, $args = array(), $template_path = '', $default_path = '' ) {
+	return masteriyo( 'template' )->get( $template_name, $args, $template_path, $default_path );
+}
+
+/**
+ * Like get_template, but returns the HTML instead of outputting.
+ *
+ * @since 0.1.0
+ *
+ * @see get_template
+ *
+ * @param string $template_name Template name.
+ * @param array  $args          Arguments. (default: array).
+ * @param string $template_path Template path. (default: '').
+ * @param string $default_path  Default path. (default: '').
+ *
+ * @return string
+ */
+function masteriyo_get_template_html( $template_name, $args = array(), $template_path = '', $default_path = '' ) {
+	return masteriyo( 'template' )->get_html( $template_name, $args, $template_path, $default_path );
+}
+
+/**
+ * Add a template to the template cache.
+ *
+ * @since 0.1.0
+ *
+ * @param string $cache_key Object cache key.
+ * @param string $template Located template.
+ */
+function masteriyo_set_template_cache( $cache_key, $template ) {
+	return masteriyo( 'template' )->set_cache( $cache_key, $template );
+}
+
+/**
+ * Get template cache.
+ *
+ * @since 0.1.0
+ *
+ * @param string $cache_key Object cache key.
+ *
+ * @return string
+ */
+function masteriyo_get_template_cache( $cache_key ) {
+	return masteriyo( 'template' )->get_cache( $cache_key );
+}
+
+/**
+ * Locate a template and return the path for inclusion.
+ *
+ * This is the load order:
+ *
+ * yourtheme/$template_path/$template_name
+ * yourtheme/$template_name
+ * $default_path/$template_name
+ *
+ * @since 0.1.0
+ *
+ * @param string $template_name Template name.
+ * @param string $template_path Template path. (default: '').
+ * @param string $default_path  Default path. (default: '').
+ *
+ * @return string
+ */
+function masteriyo_locate_template( $template_name, $template_path = '', $default_path = '' ) {
+	return masteriyo( 'template' )->locate( $template_name, $template_path, $default_path );
+}

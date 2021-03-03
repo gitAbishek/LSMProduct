4764358922f8a8e09fd5476dbc059cc71f2bd686
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-03-03 12:52:02 +0545

    Initialized the shortcodes

---
+++ includes/Shortcodes/Shortcodes.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Shortcodes initializer.
+ *
+ * @since 0.1.0
+ * @class Shortcodes
+ * @package ThemeGrill\Masteriyo\Shortcodes
+ */
+
+namespace ThemeGrill\Masteriyo\Shortcodes;
+
+use ThemeGrill\Masteriyo\Traits\Singleton;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Shortcodes initializer.
+ */
+class Shortcodes {
+	use Singleton;
+
+	/**
+	 * Register shortcodes.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return void
+	 */
+	public function register_shortcodes() {
+		foreach ( $this->get_shortcodes() as $shortcode ) {
+			masteriyo( $shortcode )->register();
+		}
+	}
+
+	/**
+	 * Get shortcodes list.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	protected function get_shortcodes() {
+		$namespace = '\\ThemeGrill\\Masteriyo\\Shortcodes';
+
+		return array(
+			'profile'      => "{$namespace}\\ProfileShortcode",
+			'courses-list' => "{$namespace}\\CoursesListShortcode",
+			'checkout'     => "{$namespace}\\CheckoutShortcode",
+			'cart'         => "{$namespace}\\CartShortcode",
+		);
+	}
+}

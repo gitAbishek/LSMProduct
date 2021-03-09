093324efbff5a0289842906ae7a8fa880fbeeb65
Sanjeev Shrestha <sethstha@gmail.com>
2021-03-09 08:49:04 +0545

    Add modal header

---
+++ assets/js/src/components/common/ModalHeader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+
+const ModalHeader = React.forwardRef<HTMLParagraphElement, Props>(
+	function ModalHeader(props, ref) {
+		const { children, className, ...other } = props;
+
+		return (
+			<p
+				className={classNames(
+					'mto-mt-4 mto-mb-2 mto-text-lg mto-font-semibold mto-text-gray-700',
+					className
+				)}
+				ref={ref}
+				{...other}>
+				{children}
+			</p>
+		);
+	}
+);
+
+export default ModalHeader;

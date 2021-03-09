2bb2697231a1d0278a1ecb57c05b6aba05670408
Sagar Tamang <mi5t4n@gmail.com>
2021-03-09 14:57:48 +0545

    Added formatting functions.

--- includes/Helper/Core.php
+++ includes/Helper/Core.php
@@ -5,6 +5,8 @@
  * @since 0.1.0
  */
 
+use ThemeGrill\Masteriyo\DateTime;
+
 /**
  * Get course.
  *
@@ -407,41 +409,155 @@ function masteriyo_locate_template( $template_name, $template_path = '', $defaul
 }
 
 /**
- * Retrieve page ids - used for pages like course list. returns -1 if no page is found.
+ * Converts a string (e.g. 'yes' or 'no') to a bool.
  *
  * @since 0.1.0
- *
- * @param string $page Page slug.
- *
- * @return int
+ * @param string|bool $string String to convert. If a bool is passed it will be returned as-is.
+ * @return bool
  */
-function masteriyo_get_page_id( $page ) {
-	$setting = masteriyo( 'setting' );
+function masteriyo_string_to_bool( $string ) {
+	if ( is_bool( $string ) ) {
+		return $string;
+	}
 
-	$setting->set_name( 'masteriyo_' . $page . '_page_id' );
+	$string = strtolower( $string );
 
-	masteriyo( 'setting.store' )->read( $setting );
-
-	$page = apply_filters( 'masteriyo_get_' . $page . '_page_id', $setting->get_value() );
-
-	return $page ? absint( $page ) : -1;
+	return ( 'yes' === $string || 1 === $string || 'true' === $string || '1' === $string );
 }
 
 /**
- * Retrieve page permalink.
+ * Converts a bool to a 'yes' or 'no'.
  *
- * @param string      $page page slug.
- * @param string|bool $fallback Fallback URL if page is not set. Defaults to home URL.
- *
+ * @since 0.1.0
+ * @param bool|string $bool Bool to convert. If a string is passed it will first be converted to a bool.
  * @return string
  */
-function masteriyo_get_page_permalink( $page, $fallback = null ) {
-	$page_id   = masteriyo_get_page_id( $page );
-	$permalink = 0 < $page_id ? get_permalink( $page_id ) : '';
+function masteriyo_bool_to_string( $bool ) {
+	if ( ! is_bool( $bool ) ) {
+		$bool = masteriyo_string_to_bool( $bool );
+	}
+	return true === $bool ? 'yes' : 'no';
+}
 
-	if ( ! $permalink ) {
-		$permalink = is_null( $fallback ) ? get_home_url() : $fallback;
+/**
+ * Convert a date string to a DateTime.
+ *
+ * @since  0.1.0
+ * @param  string $time_string Time string.
+ * @return ThemeGrill\Masteriyo\DateTime
+ */
+function masteriyo_string_to_datetime( $time_string ) {
+	// Strings are defined in local WP timezone. Convert to UTC.
+	if ( 1 === preg_match( '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(Z|((-|\+)\d{2}:\d{2}))$/', $time_string, $date_bits ) ) {
+		$offset    = ! empty( $date_bits[7] ) ? iso8601_timezone_to_offset( $date_bits[7] ) : masteriyo_timezone_offset();
+		$timestamp = gmmktime( $date_bits[4], $date_bits[5], $date_bits[6], $date_bits[2], $date_bits[3], $date_bits[1] ) - $offset;
+	} else {
+		$timestamp = masteriyo_string_to_timestamp( get_gmt_from_date( gmdate( 'Y-m-d H:i:s', masteriyo_string_to_timestamp( $time_string ) ) ) );
+	}
+	$datetime = new DateTime( "@{$timestamp}", new DateTimeZone( 'UTC' ) );
+
+	// Set local timezone or offset.
+	if ( get_option( 'timezone_string' ) ) {
+		$datetime->setTimezone( new DateTimeZone( masteriyo_timezone_string() ) );
+	} else {
+		$datetime->set_utc_offset( masteriyo_timezone_offset() );
 	}
 
-	return apply_filters( 'masteriyo_get_' . $page . '_page_permalink', $permalink );
+	return $datetime;
+}
+
+/**
+ * Get timezone offset in seconds.
+ *
+ * @since  0.1.0
+ * @return float
+ */
+function masteriyo_timezone_offset() {
+	$timezone = get_option( 'timezone_string' );
+
+	if ( $timezone ) {
+		$timezone_object = new DateTimeZone( $timezone );
+		return $timezone_object->getOffset( new DateTime( 'now' ) );
+	} else {
+		return floatval( get_option( 'gmt_offset', 0 ) ) * HOUR_IN_SECONDS;
+	}
+}
+
+/**
+ * Convert mysql datetime to PHP timestamp, forcing UTC. Wrapper for strtotime.
+ *
+ * Based on wcs_strtotime_dark_knight() from WC Subscriptions by Prospress.
+ *
+ * @since  0.1.0
+ * @param  string   $time_string    Time string.
+ * @param  int|null $from_timestamp Timestamp to convert from.
+ * @return int
+ */
+function masteriyo_string_to_timestamp( $time_string, $from_timestamp = null ) {
+	$original_timezone = date_default_timezone_get();
+
+	// @codingStandardsIgnoreStart
+	date_default_timezone_set( 'UTC' );
+
+	if ( null === $from_timestamp ) {
+		$next_timestamp = strtotime( $time_string );
+	} else {
+		$next_timestamp = strtotime( $time_string, $from_timestamp );
+	}
+
+	date_default_timezone_set( $original_timezone );
+	// @codingStandardsIgnoreEnd
+
+	return $next_timestamp;
+}
+
+
+/**
+ * Masteriyo Timezone - helper to retrieve the timezone string for a site until.
+ * a WP core method exists (see https://core.trac.wordpress.org/ticket/24730).
+ *
+ * Adapted from https://secure.php.net/manual/en/function.timezone-name-from-abbr.php#89155.
+ *
+ * @since 0.1.0
+ * @return string PHP timezone string for the site
+ */
+function masteriyo_timezone_string() {
+	// Added in WordPress 5.3 Ref https://developer.wordpress.org/reference/functions/wp_timezone_string/.
+	if ( function_exists( 'wp_timezone_string' ) ) {
+		return wp_timezone_string();
+	}
+
+	// If site timezone string exists, return it.
+	$timezone = get_option( 'timezone_string' );
+	if ( $timezone ) {
+		return $timezone;
+	}
+
+	// Get UTC offset, if it isn't set then return UTC.
+	$utc_offset = floatval( get_option( 'gmt_offset', 0 ) );
+	if ( ! is_numeric( $utc_offset ) || 0.0 === $utc_offset ) {
+		return 'UTC';
+	}
+
+	// Adjust UTC offset from hours to seconds.
+	$utc_offset = (int) ( $utc_offset * 3600 );
+
+	// Attempt to guess the timezone string from the UTC offset.
+	$timezone = timezone_name_from_abbr( '', $utc_offset );
+	if ( $timezone ) {
+		return $timezone;
+	}
+
+	// Last try, guess timezone string manually.
+	foreach ( timezone_abbreviations_list() as $abbr ) {
+		foreach ( $abbr as $city ) {
+			// WordPress restrict the use of date(), since it's affected by timezone settings, but in this case is just what we need to guess the correct timezone.
+			if ( (bool) date( 'I' ) === (bool) $city['dst'] && $city['timezone_id'] && intval( $city['offset'] ) === $utc_offset ) { // phpcs:ignore WordPress.DateTime.RestrictedFunctions.date_date
+				return $city['timezone_id'];
+			}
+		}
+	}
+
+	// Fallback to UTC.
+	return 'UTC';
 }

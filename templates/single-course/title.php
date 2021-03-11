752a9c6ed9e3257ca0fd8d2b2310788dd6d8a949
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-03-11 23:05:42 +0545

    Used 'featured' attribute for showing 'Hot' badge in single course page

--- templates/single-course/title.php
+++ templates/single-course/title.php
@@ -13,7 +13,10 @@ global $course;
 
 <h2 class="mto-font-bold mto-text-4xl mto-flex mto-mb-14">
 	<?php echo $course->get_name(); ?>
+
+	<?php if ( $course->is_featured() ) : ?>
 	<span class="mto-self-start mto-rounded-full mto-text-white mto-uppercase mto-font-medium mto-text-xs mto-bg-red-500 mto-px-3 mto-py-1 mto-ml-2">Hot</span>
+	<?php endif ?>
 </h2>
 
 <?php

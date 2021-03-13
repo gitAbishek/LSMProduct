26d9e33e1a4c78481624f197f775aea5e7011027
Shanesh Nepali <sns.themegrill@gmail.com>
2021-03-13 09:51:14 +0545

    Fixed - Card list hover effect of course list page

--- templates/content-course.php
+++ templates/content-course.php
@@ -24,34 +24,12 @@ if ( empty( $course ) || ! $course->is_visible() ) {
 }
 ?>
 <div class="course-card md:flex-grow mto-min-h-429">
-	<div class="course-image">
+	<div class="mto-group course-image mto-relative mto-overflow-hidden">
 		<span class="price-tag"><?php echo $course->get_price(); ?>$69</span>
 		<img class="mto-absoulte mto-w-full mto-h-full mto-object-cover" src="https://via.placeholder.com/150" alt="You are your only limit">
 		<!-- Hidden Card -->
-		<div class="course-author-detail mto-opacity-0 group-hover:mto-opacity-100 mto-transition-opacity mto-absolute group-hover:mto-top-0 group-hover:mto-bottom-0 mto-flex-col mto-justify-between mto-bg-primary">
-			<div class="mto-px-4 mto-py-4">
-				<div class="mto-mt-4 mto-mb-3">
-					<span class="mto-inline-block">
-						<svg class="mto-inline-block mto-fill-current mto-text-white mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
-						</svg>
-						<svg class=" mto-inline-block mto-fill-current mto-text-white mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
-						</svg>
-						<svg class=" mto-inline-block mto-fill-current mto-text-white mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
-						</svg>
-						<svg class=" mto-inline-block mto-fill-current mto-text-white mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
-						</svg>
-						<svg class=" mto-inline-block mto-fill-current mto-text-white mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path d="M5.025 20.775A.998.998 0 006 22a1 1 0 00.555-.168L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 00-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path>
-						</svg>
-					</span>
-
-					<span class="mto-inline-block mto-bg-secondary mto-rounded-full mto-px-4 mto-py-1 mto-text-xs mto-uppercase mto-font-medium mto-text-white mto-ml-2">Book</span>
-				</div>
-
+		<div class="mto-hidden group-hover:mto-block course-overlay">
+			<div class="mto-p-6">
 				<h2 class="mto-font-bold mto-text-white mto-text-base mto-capitalize mb-2">
 					<?php echo esc_html( $course->get_name() ); ?>
 				</h2>
@@ -61,14 +39,14 @@ if ( empty( $course ) || ! $course->is_visible() ) {
 						<span class="mto-ml-1.5 mto-font-medium mto-text-sm mto-text-white">Morris Perera</span>
 					</div>
 
-					<p class="mto-font-sm mto-text-white mto-mt-4">
+					<div class="mto-font-sm mto-text-white mto-my-4 mto-line-clamp-3">
 						<?php echo $course->get_description(); ?>
-					</p>
+					</div>
 
-					<a href="#" class="course-author-readmore">Read More</a>
+					<a href="#" class="btn course-author-readmore hover:mto-bg-white">Read More</a>
 				</div>
 			</div>
-			<div class="course-time-share mto-border mto-border-opacity-10">
+			<div class="course-time-share mto-bg-primary mto-absolute mto-bottom-0 mto-right-0 mto-left-0 mto-border mto-border-opacity-10">
 				<div class="course-readtime mto-w-full mto-flex mto-justify-center mto-py-2">
 					<svg class="mto-inline-block mto-fill-current mto-text-white mto-w-4 mto-mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
@@ -89,14 +67,16 @@ if ( empty( $course ) || ! $course->is_visible() ) {
 				</div>
 
 				<div class="course-share mto-w-full mto-flex mto-justify-center mto-py-2">
-					<svg class="mto-text-white mto-inline-block mto-fill-current mto-w-4 mto-mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-						<circle fill="none" cx="17.5" cy="18.5" r="1.5"></circle>
-						<circle fill="none" cx="5.5" cy="11.5" r="1.5"></circle>
-						<circle fill="none" cx="17.5" cy="5.5" r="1.5"></circle>
-						<path d="M5.5 15c.91 0 1.733-.358 2.357-.93l6.26 3.577A3.483 3.483 0 0014 18.5c0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5c-.91 0-1.733.358-2.357.93l-6.26-3.577c.063-.247.103-.502.108-.768l6.151-3.515c.625.572 1.448.93 2.358.93C19.43 9 21 7.43 21 5.5S19.43 2 17.5 2 14 3.57 14 5.5c0 .296.048.578.117.853L8.433 9.602A3.496 3.496 0 005.5 8C3.57 8 2 9.57 2 11.5S3.57 15 5.5 15zm12 2c.827 0 1.5.673 1.5 1.5s-.673 1.5-1.5 1.5-1.5-.673-1.5-1.5.673-1.5 1.5-1.5zm0-13c.827 0 1.5.673 1.5 1.5S18.327 7 17.5 7 16 6.327 16 5.5 16.673 4 17.5 4zm-12 6c.827 0 1.5.673 1.5 1.5S6.327 13 5.5 13 4 12.327 4 11.5 4.673 10 5.5 10z"></path>
-					</svg>
+					<a href="#" class="hover-effect">
+						<svg class="mto-text-white mto-inline-block mto-fill-current mto-w-4 mto-h-4 mto-mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+							<circle fill="none" cx="17.5" cy="18.5" r="1.5"></circle>
+							<circle fill="none" cx="5.5" cy="11.5" r="1.5"></circle>
+							<circle fill="none" cx="17.5" cy="5.5" r="1.5"></circle>
+							<path d="M5.5 15c.91 0 1.733-.358 2.357-.93l6.26 3.577A3.483 3.483 0 0014 18.5c0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5c-.91 0-1.733.358-2.357.93l-6.26-3.577c.063-.247.103-.502.108-.768l6.151-3.515c.625.572 1.448.93 2.358.93C19.43 9 21 7.43 21 5.5S19.43 2 17.5 2 14 3.57 14 5.5c0 .296.048.578.117.853L8.433 9.602A3.496 3.496 0 005.5 8C3.57 8 2 9.57 2 11.5S3.57 15 5.5 15zm12 2c.827 0 1.5.673 1.5 1.5s-.673 1.5-1.5 1.5-1.5-.673-1.5-1.5.673-1.5 1.5-1.5zm0-13c.827 0 1.5.673 1.5 1.5S18.327 7 17.5 7 16 6.327 16 5.5 16.673 4 17.5 4zm-12 6c.827 0 1.5.673 1.5 1.5S6.327 13 5.5 13 4 12.327 4 11.5 4.673 10 5.5 10z"></path>
+						</svg>
 
-					<span class="mto-inline-block mto-text-sm mto-text-white">Share</span>
+						<span class="mto-inline-block mto-text-sm mto-text-white">Share</span>
+					</a>
 				</div>
 			</div>
 		</div>
@@ -107,17 +87,17 @@ if ( empty( $course ) || ! $course->is_visible() ) {
 				<svg class="mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 					<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
 				</svg>
-				<svg class=" mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<svg class="mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 					<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
 				</svg>
-				<svg class=" mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<svg class="mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 					<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
 				</svg>
-				<svg class=" mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-					<path d="M21.947 9.179a1.001 1.001 0 00-.868-.676l-5.701-.453-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.213 4.107-1.49 6.452a1 1 0 001.53 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082c.297-.268.406-.686.278-1.065z"></path>
-				</svg>
-				<svg class=" mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<svg class="mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 					<path d="M5.025 20.775A.998.998 0 006 22a1 1 0 00.555-.168L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 00-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 00-1.822-.001L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.214 4.107-1.491 6.452zM12 5.429l2.042 4.521.588.047h.001l3.972.315-3.271 2.944-.001.002-.463.416.171.597v.003l1.253 4.385L12 15.798V5.429z"></path>
+				</svg>
+				<svg class="mto-inline-block mto-fill-current mto-text-gray-800 mto-w-4 mto-h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+					<path d="M6.516 14.323l-1.49 6.452a.998.998 0 001.529 1.057L12 18.202l5.445 3.63a1.001 1.001 0 001.517-1.106l-1.829-6.4 4.536-4.082a1 1 0 00-.59-1.74l-5.701-.454-2.467-5.461a.998.998 0 00-1.822 0L8.622 8.05l-5.701.453a1 1 0 00-.619 1.713l4.214 4.107zm2.853-4.326a.998.998 0 00.832-.586L12 5.43l1.799 3.981a.998.998 0 00.832.586l3.972.315-3.271 2.944c-.284.256-.397.65-.293 1.018l1.253 4.385-3.736-2.491a.995.995 0 00-1.109 0l-3.904 2.603 1.05-4.546a1 1 0 00-.276-.94l-3.038-2.962 4.09-.326z"/>
 				</svg>
 			</span>
 

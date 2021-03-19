70c9dfcff6dd79da37341a19c87f67b87badcab6
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-03-19 16:18:48 +0545

    added login form templates

---
+++ templates/content-login-form.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Login form template content.
+ *
+ * @version 0.1.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * masteriyo_before_login_form_content hook.
+ */
+do_action( 'masteriyo_before_login_form_content' );
+
+?>
+
+<section class="mto-container mto-mx-auto">
+	<div class="mto-flex mto-justify-center mto-items-center mto-h-screen">
+
+		<div class="mto-login-wrapper">
+			<h2>Sign In</h2>
+
+			<div class="social-login">
+				<div class="mto-w-full">
+					<form id="mto-login-form">
+						<input type="hidden" name="remember" value="true">
+						<div class="mto-rounded-md mtoshadow-sm mto-space-y-px">
+							<div class="mto-mb-6">
+								<label for="username-email-address" class="mto-block mto-text-sm mto-font-semibold mto-mb-4">Username or Email</label>
+								<input id="username-email-address" name="text" type="text" required class="mto-px-4 mto-rounded mto-block mto-w-full mto-py-2 mto-border mto-border-gray-300 focus:mto-outline-none focus:mto-shadow-outline focus:mto-border-primary" placeholder="">
+							</div>
+
+							<div class="mto-mb-6">
+								<label for="password" class="mto-block mto-text-sm mto-font-semibold mto-pb-4">Password</label>
+								<input id="password" name="password" type="password" autocomplete="current-password" required class="mto-px-4 mto-rounded mto-block mto-w-full mto-py-2 mto-border mto-border-gray-300 focus:mto-outline-none focus:mto-shadow-outline focus:mto-border-primary" placeholder="">
+							</div>
+						</div>
+
+						<div class="mto-flex mto-items-center mto-justify-between  mto-mt-6 mto-mb-6 md:mto-mb-10">
+							<div class="mto-flex mto-items-center">
+								<input id="remember_me" name="remember_me" type="checkbox" class="mto-h-4 mto-w-4 mto-text-primary-600 focus:mto-primary-500 mto-border-gray-300 mto-rounded">
+								<label for="remember_me" class="mto-ml-2 mto-block mto-text-sm mto-text-gray-900">
+								Remember me
+								</label>
+							</div>
+
+							<div class="mto-text-sm">
+								<a href="#" class="mto-font-medium mto-text-primary-800 hover:mto-text-primary-700 hover:mto-underline">
+								Forgot your password?
+								</a>
+							</div>
+						</div>
+
+						<div class="mto-block md:mto-flex mto-items-center mto-justify-between">
+							<button type="submit" class="btn sign-in">
+								Sign in
+							</button>
+
+							<div class="mto-text-sm mto-mt-6 md:mto-my-0 mto-text-center md:mto-text-right">
+								Don't have an account?
+								<a href="#" class="mto-font-medium mto-text-primary-800 hover:mto-text-primary-700 hover:mto-underline">
+									Sign Up Now
+								</a>
+							</div>
+						</div>
+						<div id="mto-login-error-msg" class="mto-text-red-700 mto-hidden"></div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php
+
+/**
+ * masteriyo_after_login_form_content hook.
+ */
+do_action( 'masteriyo_after_login_form_content' );

1fc978ae5b82398602d3c7f0f8b4a2420be56a5f
Shanesh Nepali <sns.themegrill@gmail.com>
2021-03-30 17:14:20 +0545

    form reset password design

--- templates/profile/form-login.php
+++ templates/profile/form-login.php
@@ -14,37 +14,37 @@ do_action( 'masteriyo_before_login_form_content' );
 
 ?>
 
-<section class="mto-container mto-mx-auto">
+<section class="login mto-container mto-mx-auto">
 	<div class="mto-flex mto-justify-center mto-items-center mto-h-screen">
 
 		<div class="mto-login-wrapper">
 			<h2>Sign In</h2>
 
-			<div class="social-login">
+			<div class="login-form">
 				<div class="mto-w-full">
 					<form id="mto-login-form">
 						<input type="hidden" name="remember" value="true">
 						<div class="mto-rounded-md mtoshadow-sm mto-space-y-px">
-							<div class="mto-mb-6">
+							<div class="username mto-mb-6">
 								<label for="username-email-address" class="mto-block mto-text-sm mto-font-semibold mto-mb-4">Username or Email</label>
 								<input id="username-email-address" name="text" type="text" required class="mto-px-4 mto-rounded mto-block mto-w-full mto-py-2 mto-border mto-border-gray-300 focus:mto-outline-none focus:mto-shadow-outline focus:mto-border-primary" placeholder="">
 							</div>
 
-							<div class="mto-mb-6">
+							<div class="password mto-mb-6">
 								<label for="password" class="mto-block mto-text-sm mto-font-semibold mto-pb-4">Password</label>
 								<input id="password" name="password" type="password" autocomplete="current-password" required class="mto-px-4 mto-rounded mto-block mto-w-full mto-py-2 mto-border mto-border-gray-300 focus:mto-outline-none focus:mto-shadow-outline focus:mto-border-primary" placeholder="">
 							</div>
 						</div>
 
 						<div class="mto-flex mto-items-center mto-justify-between  mto-mt-6 mto-mb-6 md:mto-mb-10">
-							<div class="mto-flex mto-items-center">
+							<div class="remember-me mto-flex mto-items-center">
 								<input id="remember_me" name="remember_me" type="checkbox" class="mto-h-4 mto-w-4 mto-text-primary-600 focus:mto-primary-500 mto-border-gray-300 mto-rounded">
-								<label for="remember_me" class="mto-ml-2 mto-block mto-text-sm mto-text-gray-900">
+								<label for="remember_me" class="mto-mb-0 mto-block mto-text-sm mto-text-gray-900">
 								Remember me
 								</label>
 							</div>
 
-							<div class="mto-text-sm">
+							<div class="forgot-password mto-text-sm">
 								<a href="<?php echo esc_url( masteriyo_get_account_endpoint_url( 'reset-password' ) ); ?>" class="mto-font-medium mto-text-primary-800 hover:mto-text-primary-700 hover:mto-underline">
 								Forgot your password?
 								</a>
@@ -52,12 +52,12 @@ do_action( 'masteriyo_before_login_form_content' );
 						</div>
 
 						<div class="mto-block md:mto-flex mto-items-center mto-justify-between">
-							<button type="submit" class="btn sign-in">
+							<button type="submit" class="btn mto-capitalize sign-in">
 								Sign in
 							</button>
 
-							<div class="mto-text-sm mto-mt-6 md:mto-my-0 mto-text-center md:mto-text-right">
-								Don't have an account?
+							<div class="sign-up mto-text-sm mto-mt-6 md:mto-my-0 mto-text-center md:mto-text-right">
+								<span>Don't have an account?</span>
 								<a
 									href="<?php echo esc_url( masteriyo_get_account_endpoint_url( 'signup' ) ); ?>"
 									class="mto-font-medium mto-text-primary-800 hover:mto-text-primary-700 hover:mto-underline"
@@ -66,9 +66,11 @@ do_action( 'masteriyo_before_login_form_content' );
 								</a>
 							</div>
 						</div>
+
 						<div id="mto-login-error-msg" class="mto-text-red-700 mto-hidden"></div>
 					</form>
 				</div>
+				
 			</div>
 		</div>
 	</div>
--- templates/profile/form-reset-password.php
+++ templates/profile/form-reset-password.php
@@ -11,7 +11,46 @@ do_action( 'masteriyo_before_password_reset_form' );
 
 ?>
 
-<div class="reset">Masteriyo Password Reset Form.</div>
+<section class="login mto-container mto-mx-auto">
+	<div class="mto-flex mto-justify-center mto-items-center mto-h-screen">
+
+		<div class="mto-login-wrapper">
+			<h2 class="mto-mb-4">Reset Password</h2>
+      <p>Enter your user account's verified email address and we will send you a password reset link.</p>
+			<div class="reset-form mto-mt-2">
+				<div class="mto-w-full">
+					<form id="mto-reset-form">
+						<input type="hidden" name="remember" value="true">
+						<div class="mto-rounded-md mtoshadow-sm">
+							<div class="reset-username mto-mb-6">
+								<label for="reset-username-email-address" class="mto-block mto-text-sm mto-font-semibold mto-mb-4">Username or Email</label>
+								<input id="reset-username-email-address" name="text" type="text" required class="mto-px-4 mto-rounded mto-block mto-w-full mto-py-2 mto-border mto-border-gray-300 focus:mto-outline-none focus:mto-shadow-outline focus:mto-border-primary" placeholder="">
+							</div>
+						</div>
+
+						<div class="mto-block md:mto-flex mto-items-center md:mto-space-x-6">
+							<button type="submit" class="btn mto-capitalize sign-in">
+                Send Reset Email
+							</button>
+
+							<div class="go-back mto-text-sm mto-mt-6 md:mto-my-0 mto-text-center md:mto-text-left">
+								<a
+									href="#"
+									class="mto-font-medium mto-text-primary-800 hover:mto-text-primary-700 hover:mto-underline"
+								>
+									Go back to sign-in
+								</a>
+							</div>
+						</div>
+
+						<div id="mto-login-error-msg" class="mto-text-red-700 mto-hidden"></div>
+					</form>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php
 

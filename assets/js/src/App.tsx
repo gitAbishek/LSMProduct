758a22ad946feff358ea780fee97c2b2f91b4936
Sanjeev Shrestha <sethstha@gmail.com>
2021-04-06 17:01:13 +0545

    Use module for the import sorting

--- assets/js/src/App.tsx
+++ assets/js/src/App.tsx
@@ -1,7 +1,7 @@
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-import * as screens from './screens';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import {
 	Redirect,
@@ -9,57 +9,59 @@ import {
 	HashRouter as Router,
 	Switch,
 } from 'react-router-dom';
+import { ToastProvider } from 'react-toast-notifications';
 
+import * as screens from './screens';
 import AddNewQuiz from './screens/quiz/AddNewQuiz';
 import EditQuiz from './screens/quiz/EditQuiz';
-import React from 'react';
-import { ToastProvider } from 'react-toast-notifications';
 
 const App = () => {
 	const queryClient = new QueryClient();
 
 	return (
-		<ToastProvider>
-			<QueryClientProvider client={queryClient}>
-				<Router>
-					<div className="masteriyo">
-						<Switch>
-							<Route path="/courses" exact>
-								<screens.AllCourses />
-							</Route>
-							<Route path="/courses/add-new-course" exact>
-								<screens.AddNewCourse />
-							</Route>
-							<Route path="/builder/:courseId" exact>
-								<screens.SectionBuilder />
-							</Route>
-							<Route path="/courses/:sectionId/add-new-lesson" exact>
-								<screens.AddNewLesson />
-							</Route>
-							<Route path="/courses/:sectionId/add-new-quiz" exact>
-								<AddNewQuiz />
-							</Route>
-							<Route path="/quiz/:quizId/edit/:step?" exact>
-								<EditQuiz />
-							</Route>
-							<Route path="/builder/lesson/:lessonId" exact>
-								<screens.EditLesson />
-							</Route>
-							<Route path="/courses/edit/:courseId" exact>
-								<screens.EditCourse />
-							</Route>
-							<Route path="/settings" exact>
-								<screens.Settings />
-							</Route>
+		<ChakraProvider>
+			<ToastProvider>
+				<QueryClientProvider client={queryClient}>
+					<Router>
+						<div className="masteriyo">
+							<Switch>
+								<Route path="/courses" exact>
+									<screens.AllCourses />
+								</Route>
+								<Route path="/courses/add-new-course" exact>
+									<screens.AddNewCourse />
+								</Route>
+								<Route path="/builder/:courseId" exact>
+									<screens.SectionBuilder />
+								</Route>
+								<Route path="/courses/:sectionId/add-new-lesson" exact>
+									<screens.AddNewLesson />
+								</Route>
+								<Route path="/courses/:sectionId/add-new-quiz" exact>
+									<AddNewQuiz />
+								</Route>
+								<Route path="/quiz/:quizId/edit/:step?" exact>
+									<EditQuiz />
+								</Route>
+								<Route path="/builder/lesson/:lessonId" exact>
+									<screens.EditLesson />
+								</Route>
+								<Route path="/courses/edit/:courseId" exact>
+									<screens.EditCourse />
+								</Route>
+								<Route path="/settings" exact>
+									<screens.Settings />
+								</Route>
 
-							<Route>
-								<Redirect to="/courses" />
-							</Route>
-						</Switch>
-					</div>
-				</Router>
-			</QueryClientProvider>
-		</ToastProvider>
+								<Route>
+									<Redirect to="/courses" />
+								</Route>
+							</Switch>
+						</div>
+					</Router>
+				</QueryClientProvider>
+			</ToastProvider>
+		</ChakraProvider>
 	);
 };
 

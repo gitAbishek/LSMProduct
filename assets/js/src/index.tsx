5d06b99a1b5fb5e12f16c02ac96250060590fc21
Sanjeev Shrestha <sethstha@gmail.com>
2021-04-07 09:39:42 +0545

    Remove theme provider as we are not using it

--- assets/js/src/index.tsx
+++ assets/js/src/index.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './App';
-import { ThemeProvider } from './context/ThemeContext';
 
 const appRoot = document.getElementById('masteriyo');
 
 if (appRoot) {
-	ReactDom.render(
-		<ThemeProvider>
-			<App />
-		</ThemeProvider>,
-		appRoot
-	);
+	ReactDom.render(<App />, appRoot);
 }

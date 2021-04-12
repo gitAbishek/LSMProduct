e1de34709188004f54273ce9466c5e9137075505
Sanjeev Shrestha <sethstha@gmail.com>
2021-04-12 07:40:01 +0545

    Add add new content button

--- assets/js/src/screens/sections/SectionBuilder.tsx
+++ assets/js/src/screens/sections/SectionBuilder.tsx
@@ -1,30 +1,40 @@
-import { addSection, fetchCourse, fetchSections } from '../../utils/api';
-import { useHistory, useParams } from 'react-router-dom';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-
+import { __ } from '@wordpress/i18n';
 import AddNewButton from 'Components/common/AddNewButton';
+import Spinner from 'Components/common/Spinner';
 import MainToolbar from 'Layouts/MainToolbar';
 import React from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useHistory, useParams } from 'react-router-dom';
+
+import routes from '../../constants/routes';
+import urls from '../../constants/urls';
+import API from '../../utils/api';
 import Section from './components/Section';
-import Spinner from 'Components/common/Spinner';
-import { __ } from '@wordpress/i18n';
 
 const SectionBuilder = () => {
 	const { courseId }: any = useParams();
 	const queryClient = useQueryClient();
 	const history = useHistory();
+	const courseAPI = new API(urls.courses);
+	const sectionAPI = new API(urls.sections);
 
-	const courseQuery = useQuery(['builderCourse', courseId], () =>
-		fetchCourse(courseId)
+	const courseQuery = useQuery(
+		['builderCourse', courseId],
+		() => courseAPI.get(courseId),
+		{
+			onError: () => {
+				history.push(routes.courses.list);
+			},
+		}
 	);
 
 	courseQuery.isError && history.push('/');
 	const sectionQuery = useQuery(['builderSections', courseId], () =>
-		fetchSections(courseId)
+		sectionAPI.get(courseId)
 	);
 
 	const addSectionMutation = useMutation(
-		(newSection: any) => addSection(newSection),
+		(newSection: any) => sectionAPI.store(newSection),
 		{
 			onSuccess: () => {
 				queryClient.invalidateQueries('builderSections');
@@ -41,21 +51,16 @@ const SectionBuilder = () => {
 
 	return (
 		<>
-			<MainToolbar />
 			<div className="mto-container mto-mx-auto">
-				{sectionQuery.isLoading ? (
-					<Spinner />
-				) : (
-					sectionQuery?.data?.map((section: any, index: number) => (
-						<Section
-							key={section.id}
-							id={section.id}
-							name={section.name}
-							description={section.description}
-							courseId={courseId}
-						/>
-					))
-				)}
+				{sectionQuery?.data?.map((section: any, index: number) => (
+					<Section
+						key={section.id}
+						id={section.id}
+						name={section.name}
+						description={section.description}
+						courseId={courseId}
+					/>
+				))}
 				<div className="mto-flex mto-justify-center mto-p-12">
 					<AddNewButton onClick={onAddNewSectionClick}>
 						{__('Add New Section', 'masteriyo')}

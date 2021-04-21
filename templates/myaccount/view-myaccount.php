30987ad976476b03c1bab12abbc879aadcdd0a51
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-04-21 11:33:52 +0545

    Make view-myaccount template dynamic

--- templates/myaccount/view-myaccount.php
+++ templates/myaccount/view-myaccount.php
@@ -20,28 +20,30 @@ do_action( 'masteriyo_before_view_myaccount_content' );
   </div>
   <div class="myaccount-content">
     <div class="account-content-wrap">
-			  <h2 class="account-content---name mto-mb-0"><?php echo esc_attr( $user->get_display_name() ); ?></h2>
-  			<span class="account-holder--membership mto-text-xs mto-text-pColor">Gold Member</span>
+		<h2 class="account-content---name mto-mb-0"><?php echo esc_html( $user->get_display_name() ); ?></h2>
+		<span class="account-holder--membership mto-text-xs mto-text-pColor"><?php echo esc_html__( 'Gold Member', 'masteriyo' ); ?></span>
     </div>
     <div class="account-holder-detail-more-info mto-mt-10">
       <div class="account-holder-detail-more-info-table mto-flex">
         <ul class="account-col-1">
-          <li><strong>Email</strong></li>
-          <li><strong>Contact Number</strong></li>
-          <li><strong>Address</strong></li>
-          <li><strong>City</strong></li>
-          <li><strong>State</strong></li>
-          <li><strong>Zip Code</strong></li>
-          <li><strong>Country</strong></li>
+          <li><strong><?php echo esc_html__( 'Email', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'First Name', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'Last Name', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'Address', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'City', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'State', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'Zip Code', 'masteriyo' ); ?></strong></li>
+          <li><strong><?php echo esc_html__( 'Country', 'masteriyo' ); ?></strong></li>
         </ul>
         <ul class="account-col-2">
-          <li><?php echo esc_attr( $user->get_user_email() ); ?></li>
-          <li>+8 123-489-1236</li>
-          <li>123 Moon Street, Mars</li>
-          <li>Nuwa</li>
-          <li>Abiboo</li>
-          <li>8899</li>
-          <li>Sinara</li>
+          <li><?php echo esc_html( $user->get_user_email() ); ?></li>
+          <li><?php echo esc_html( $user->get_first_name() ); ?></li>
+          <li><?php echo esc_html( $user->get_last_name() ); ?></li>
+          <li><?php echo esc_html( $user->get_address() ); ?></li>
+          <li><?php echo esc_html( $user->get_city() ); ?></li>
+          <li><?php echo esc_html( $user->get_state() ); ?></li>
+          <li><?php echo esc_html( $user->get_zip_code() ); ?></li>
+          <li><?php echo esc_html( $user->get_country() ); ?></li>
       </div>
     </div>
   </div>

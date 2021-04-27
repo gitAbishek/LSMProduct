0ceec21a5276efbaaf4e37a16ddbaed54aec22bb
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-04-27 18:01:09 +0545

    Created Order Repository Interface

---
+++ includes/Contracts/OrderRepositoryInterface.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Order repository interface.
+ *
+ * @since 0.1.0
+ *
+ * @package ThemeGrill\Masteriyo\Contracts
+ */
+
+namespace ThemeGrill\Masteriyo\Contracts;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Order repository interface
+ *
+ * Functions that must be defined by order repository classes.
+ */
+interface OrderRepositoryInterface {
+
+	/**
+	 * Read order items of a specific type from the database for this order.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param Order $order Order object.
+	 * @param string  $type Order item type.
+	 * @return array
+	 */
+	public function read_items( $order, $type );
+
+	/**
+	 * Remove all line items from an order.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param Order $order Order object.
+	 * @param string  $type Order item type. Default null.
+	 */
+	public function delete_items( $order, $type = null );
+
+	/**
+	 * Get token ids for an order.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param Order $order Order object.
+	 * @return array
+	 */
+	public function get_payment_token_ids( $order );
+
+	/**
+	 * Update token ids for an order.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param Order $order Order object.
+	 * @param array    $token_ids Token IDs.
+	 */
+	public function update_payment_token_ids( $order, $token_ids );
+}

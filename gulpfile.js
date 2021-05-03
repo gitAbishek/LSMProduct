895d7d707fdc1f44500c016775dbdf91cafd1449
Sagar Tamang <mi5t4n@gmail.com>
2021-05-03 19:43:17 +0545

    Replace Grunt tasks with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+'use strict';
+
 require('dotenv').config();
-const { dest, series, src, watch } = require('gulp');
+var fs = require('fs')
+var pkg = JSON.parse(fs.readFileSync('./package.json'))
+const { dest, series, src, watch, parallel, task } = require('gulp');
+const { exec } = require('child_process');
+const zip = require('gulp-zip');
 const { sass } = require('@mr-hope/gulp-sass');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
@@ -11,6 +17,55 @@ if (!process.env.WORDPRESS_URL) {
 	console.error('Please set WORDPRESS_URL on your environment variable');
 	process.exit(1);
 }
+
+const fileList = {
+	includes: {
+		src: 'includes/**/*',
+		dest: 'build/includes'
+	},
+	assets: {
+		src: [
+			'assets/**/*',
+			'!assets/js/src/**/*',
+			'!assets/scss/**/*',
+		],
+		dest: 'build/assets'
+	},
+	templates: {
+		src: 'templates/**/*',
+		dest: 'build/templates'
+	},
+	i18n: {
+		src: 'i18n/**/*',
+		dest: 'build/i18n'
+	},
+	bootstrap: {
+		src: 'bootstrap/**/*',
+		dest: 'build/bootstrap'
+	},
+	composer: {
+		src: [
+			'composer.json',
+			'composer.lock'
+		],
+		dest: 'build'
+	},
+	npm: {
+		src: [
+			'package.json',
+			'package.lock'
+		],
+		dest: 'build'
+	},
+	other: {
+		src: [
+			'readme.txt',
+			'changelog.txt',
+			'masteriyo.php'
+		],
+		dest: 'build'
+	}
+};
 
 // paths for the automation
 const paths = {
@@ -32,6 +87,10 @@ const paths = {
 		src: 'templates/**/*.php',
 	},
 };
+
+function removePreviousMinifiedAssets() {
+	return exec('find assets/ -name "*min*" -type f -delete');
+}
 
 function compileSass() {
 	return src(paths.sass.src)
@@ -75,4 +134,45 @@ function watchChanges() {
 	watch(paths.images.src, series(optimizeImages, reloadBrowserSync));
 }
 
-exports.dev = series(startBrowserSync, watchChanges);
+function removeBuild() {
+	return exec( 'rm -rf build');
+}
+
+function removeRelease() {
+	return exec( 'rm -rf release');
+}
+
+const copyToBuild = [
+	() => src(fileList.includes.src).pipe(dest( fileList.includes.dest)),
+	() => src(fileList.assets.src).pipe(dest(fileList.assets.dest)),
+	() => src(fileList.templates.src).pipe(dest(fileList.templates.dest)),
+	() => src(fileList.i18n.src).pipe(dest(fileList.i18n.dest)),
+	() => src(fileList.bootstrap.src).pipe(dest(fileList.bootstrap.dest)),
+	() => src(fileList.composer.src).pipe(dest(fileList.composer.dest)),
+	() => src(fileList.other.src).pipe(dest(fileList.other.dest)),
+];
+
+function runComposerInBuild() {
+	return exec( 'cd build && composer install --no-dev')
+}
+
+function compressBuildWithoutVersion() {
+	return src('build/**/*')
+		.pipe(zip(`${pkg.name}.zip`))
+		.pipe(dest('release'));
+}
+
+function compressBuildWithVersion() {
+	return src('build/**/*')
+		.pipe(zip(`${pkg.name}-${pkg.version}.zip`))
+		.pipe(dest('release'));
+}
+
+const compileAssets = series(removePreviousMinifiedAssets, parallel(compileSass, minifyJs, optimizeImages));
+const build         = series(removeBuild, compileAssets, parallel(copyToBuild), runComposerInBuild);
+const dev           = series(removePreviousMinifiedAssets, startBrowserSync, watchChanges);
+const release       = series(removeRelease, build, parallel(compressBuildWithVersion, compressBuildWithoutVersion));
+
+exports.dev = dev;
+exports.build = build;
+exports.release = release;

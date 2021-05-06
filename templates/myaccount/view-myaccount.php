3f510715f596582f04b2903c8b2b7cb723143bbf
Shanesh Nepali <sns.themegrill@gmail.com>
2021-05-06 13:35:20 +0545

    fix myaccount page

--- templates/myaccount/view-myaccount.php
+++ templates/myaccount/view-myaccount.php
@@ -14,18 +14,20 @@ do_action( 'masteriyo_before_view_myaccount_content' );
 
 ?>
 
-<div class="myaccount mto-flex mto-space-x-16">
-  <div class="myaccount-img">
-		<img class="mto-inline-block mto-h-40 mto-w-40 mto-rounded-full mto-ring-2 mto-ring-white mto-shadow-lg" src="<?php echo esc_attr( $user->get_avatar_url() ); ?>" alt="" />
+<div class="mto-myaccount mto-flex">
+  <div class="mto-myaccount--imgwrap">
+		<img class="mto-myaccount-img" src="<?php echo esc_attr( $user->get_avatar_url() ); ?>" alt="" />
   </div>
-  <div class="myaccount-content">
-    <div class="account-content-wrap">
-		<h2 class="account-content---name mto-mb-0"><?php echo esc_html( $user->get_display_name() ); ?></h2>
-		<span class="account-holder--membership mto-text-xs mto-text-pColor"><?php echo esc_html__( 'Gold Member', 'masteriyo' ); ?></span>
+
+  <div class="mto-myaccount--detail">
+    <div class="mto-myaccount mto-myaccount--header">
+	  	<h2 class="mto-myaccount--name"><?php echo esc_html( $user->get_display_name() ); ?></h2>
+
+  	  <span class="mto-myaccount--membership"><?php echo esc_html__( 'Gold Member', 'masteriyo' ); ?></span>
     </div>
-    <div class="account-holder-detail-more-info mto-mt-10">
-      <div class="account-holder-detail-more-info-table mto-flex">
-        <ul class="account-col-1">
+    
+    <div class="mto-myaccount--body mto-flex">
+        <ul class="mto-title account-col-1">
           <li><strong><?php echo esc_html__( 'Email', 'masteriyo' ); ?></strong></li>
           <li><strong><?php echo esc_html__( 'First Name', 'masteriyo' ); ?></strong></li>
           <li><strong><?php echo esc_html__( 'Last Name', 'masteriyo' ); ?></strong></li>
@@ -35,7 +37,8 @@ do_action( 'masteriyo_before_view_myaccount_content' );
           <li><strong><?php echo esc_html__( 'Zip Code', 'masteriyo' ); ?></strong></li>
           <li><strong><?php echo esc_html__( 'Country', 'masteriyo' ); ?></strong></li>
         </ul>
-        <ul class="account-col-2">
+        
+        <ul class="mto-content account-col-2">
           <li><?php echo esc_html( $user->get_user_email() ); ?></li>
           <li><?php echo esc_html( $user->get_first_name() ); ?></li>
           <li><?php echo esc_html( $user->get_last_name() ); ?></li>
@@ -46,7 +49,6 @@ do_action( 'masteriyo_before_view_myaccount_content' );
           <li><?php echo esc_html( $user->get_country() ); ?></li>
       </div>
     </div>
-  </div>
 </div>
 
 <?php

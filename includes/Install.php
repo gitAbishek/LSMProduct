5fbe5110619d80c54a371b5a51b8257095c887cc
Aashil <aashil.themegrill@gmail.com>
2021-05-07 17:39:06 +0545

    Removed create_pages method from here

--- includes/Install.php
+++ includes/Install.php
@@ -20,7 +20,6 @@ class Install {
 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 		self::create_roles();
 		self::init_db();
-		self::create_pages();
 	}
 
 	/**
@@ -216,41 +215,6 @@ class Install {
 
 		foreach ( $tables as $table ) {
 			$wpdb->query( 'DROP TABLE IF EXISTS ' . esc_sql( $table ) );
-		}
-	}
-
-	/**
-	 * Create pages that the plugin relies on, storing page IDs in variables.
-	 *
-	 * @since 0.1.0
-	 */
-	public static function create_pages() {
-		include_once dirname( __FILE__ ) . '/Helper/Core.php';
-
-		$pages = apply_filters(
-			'masteriyo_create_pages',
-			array(
-				'course-list'        => array(
-					'name'    => _x( 'course-list', 'Page slug', 'masteriyo' ),
-					'title'   => _x( 'Course List', 'Page title', 'masteriyo' ),
-					'content' => '',
-				),
-				'myaccount'          => array(
-					'name'    => _x( 'myaccount', 'Page slug', 'masteriyo' ),
-					'title'   => _x( 'My Masteriyo Acoount', 'Page title', 'masteriyo' ),
-					'content' => '<!-- wp:shortcode -->[' . apply_filters( 'masteriyo_myaccount_shortcode_tag', 'masteriyo_myaccount' ) . ']<!-- /wp:shortcode -->',
-				),
-				'masteriyo-checkout' => array(
-					// Checkout slug is 'masteriyo-checkout' as 'checkout' slug might be used by other plugins like WooCommerce.
-					'name'    => _x( 'masteriyo-checkout', 'Page slug', 'masteriyo' ),
-					'title'   => _x( 'Masteriyo Checkout', 'Page title', 'masteriyo' ),
-					'content' => '<!-- wp:shortcode -->[' . apply_filters( 'masteriyo_checkout_shortcode_tag', 'masteriyo_checkout' ) . ']<!-- /wp:shortcode -->',
-				),
-			)
-		);
-
-		foreach ( $pages as $key => $page ) {
-			masteriyo_create_page( esc_sql( $page['name'] ), 'masteriyo_' . $key . '_page_id', $page['title'], $page['content'], ! empty( $page['parent'] ) ? masteriyo_get_page_id( $page['parent'] ) : '' );
 		}
 	}
 }

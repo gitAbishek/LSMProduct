dd3a513d26ca00cd12c2d9699148e47d4c7fb342
Aashil <aashil.themegrill@gmail.com>
2021-05-13 15:46:41 +0545

    Changed the admin notice and removed rest api availability filter

--- includes/Masteriyo.php
+++ includes/Masteriyo.php
@@ -90,7 +90,6 @@ class Masteriyo extends Container {
 	 * @since 0.1.0
 	 */
 	protected function init_hooks() {
-		// add_action( 'admin_init', array( $this, 'get_rest_api_availability' ) );
 		add_action( 'admin_init', array( $this, 'dismiss_admin_notice_messages' ) );
 		add_action( 'init', array( $this, 'after_wp_init' ) );
 		add_action( 'admin_bar_menu', array( $this, 'add_course_list_page_link' ), 35 );
@@ -408,9 +407,9 @@ class Masteriyo extends Container {
 		$user_id      = get_current_user_id();
 		$is_dismissed = masteriyo_string_to_bool( get_user_meta( $user_id, 'masteriyo_dismissed_nag_messages', true ) );
 
-		if ( version_compare( get_bloginfo( 'version' ), '4.7', '>=' ) && ! $is_dismissed ) {
+		if ( ! $is_dismissed ) {
 			// translators: %s: Dismiss link
-			$message = sprintf( esc_html__( 'REST API is disabled, you must enable it in order to work Masteriyo plugin properly. %1$1sDismiss this Message%2$2s', 'masteriyo' ), '<a href="?masteriyo-dismissed-nag">', '</a>' );
+			$message = sprintf( esc_html__( 'Minimum WordPress version required to work Masteriyo is v4.7. %1$1sDismiss this Message%2$2s', 'masteriyo' ), '<a href="?masteriyo-dismissed-nag">', '</a>' );
 			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 			printf( '<div class="notice notice-warning"><p><strong>%s</strong>: %s</p></div>', 'Masteriyo', $message );
 		}

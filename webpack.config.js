46b87f2b8828e163b9649517efaae66b45320d2f
Sanjeev Shrestha <sethstha@gmail.com>
2021-05-13 13:16:33 +0545

    Fix - webpack server not running due to sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -60,18 +60,6 @@ const config = {
 					},
 				],
 			},
-
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					'style-loader',
-					// Translates CSS into CommonJS
-					'css-loader',
-					// Compiles Sass to CSS
-					'sass-loader',
-				],
-			},
 		],
 	},
 

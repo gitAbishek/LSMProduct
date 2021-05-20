34a85f6442cffda0711377c4a0be4e4617a3e1d9
Sanjeev Shrestha <sethstha@gmail.com>
2021-05-20 14:47:22 +0545

    Show preview button on course builder

--- assets/js/src/screens/sections/SectionBuilder.tsx
+++ assets/js/src/screens/sections/SectionBuilder.tsx
@@ -25,6 +25,7 @@ const SectionBuilder = () => {
 	const builderAPI = new API(urls.builder);
 	const [builderData, setBuilderData] = useState<any>(null);
 	const [totalSectionsLength, setTotalSectionsLength] = useState<number>(0);
+
 	const courseQuery = useQuery(
 		['builderCourse', courseId],
 		() => courseAPI.get(courseId),
@@ -84,64 +85,69 @@ const SectionBuilder = () => {
 	};
 
 	return (
-		<Stack direction="column" spacing="8" alignItems="center">
-			<HeaderBuilder courseId={courseId} />
-			<Container maxW="container.xl">
-				<DragDropContext onDragEnd={onDragEnd}>
-					<Droppable droppableId="section" type="section">
-						{(droppableProvided) => (
-							<Box
-								ref={droppableProvided.innerRef}
-								{...droppableProvided.droppableProps}>
-								{(courseQuery.isLoading ||
-									builderQuery.isLoading ||
-									!builderData) && (
-									<Center minH="xs">
-										<Spinner />
-									</Center>
-								)}
-
-								{builderData &&
-									builderQuery.isSuccess &&
-									builderData.section_order.map(
-										(sectionId: any, index: any) => {
-											const section = builderData.sections[sectionId];
-											return (
-												<Section
-													key={section.id}
-													id={section.id}
-													index={index}
-													name={section.name}
-													description={section.description}
-													courseId={courseId}
-													contents={section.contents}
-													contentsMap={builderData.contents}
-												/>
-											);
-										}
-									)}
-
-								{addSection.isLoading && (
-									<Center minH="24">
-										<Spinner />
-									</Center>
-								)}
-								{courseQuery.isSuccess &&
-									builderQuery.isSuccess &&
-									builderData && (
-										<Center mb="8">
-											<AddNewButton onClick={onAddNewSectionPress}>
-												{__('Add New Section', 'masteriyo')}
-											</AddNewButton>
+		courseQuery.isSuccess && (
+			<Stack direction="column" spacing="8" alignItems="center">
+				<HeaderBuilder
+					courseId={courseId}
+					previewUrl={courseQuery.data.preview_permalink}
+				/>
+				<Container maxW="container.xl">
+					<DragDropContext onDragEnd={onDragEnd}>
+						<Droppable droppableId="section" type="section">
+							{(droppableProvided) => (
+								<Box
+									ref={droppableProvided.innerRef}
+									{...droppableProvided.droppableProps}>
+									{(courseQuery.isLoading ||
+										builderQuery.isLoading ||
+										!builderData) && (
+										<Center minH="xs">
+											<Spinner />
 										</Center>
 									)}
-								{droppableProvided.placeholder}
-							</Box>
-						)}
-					</Droppable>
-				</DragDropContext>
-			</Container>
-		</Stack>
+
+									{builderData &&
+										builderQuery.isSuccess &&
+										builderData.section_order.map(
+											(sectionId: any, index: any) => {
+												const section = builderData.sections[sectionId];
+												return (
+													<Section
+														key={section.id}
+														id={section.id}
+														index={index}
+														name={section.name}
+														description={section.description}
+														courseId={courseId}
+														contents={section.contents}
+														contentsMap={builderData.contents}
+													/>
+												);
+											}
+										)}
+
+									{addSection.isLoading && (
+										<Center minH="24">
+											<Spinner />
+										</Center>
+									)}
+									{courseQuery.isSuccess &&
+										builderQuery.isSuccess &&
+										builderData && (
+											<Center mb="8">
+												<AddNewButton onClick={onAddNewSectionPress}>
+													{__('Add New Section', 'masteriyo')}
+												</AddNewButton>
+											</Center>
+										)}
+									{droppableProvided.placeholder}
+								</Box>
+							)}
+						</Droppable>
+					</DragDropContext>
+				</Container>
+			</Stack>
+		)
 	);
 };
 

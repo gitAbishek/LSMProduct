f2be7e11f6600d32e224965d7b7a90053735366e
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-05-20 14:23:09 +0545

    Created the Roles class

---
+++ includes/Roles.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Roles class.
+ *
+ * @since 0.1.0
+ */
+
+namespace ThemeGrill\Masteriyo;
+
+class Roles {
+	/**
+	 * Retun all roles.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	public static function get_all() {
+		return apply_filters(
+			'masteriyo_get_roles',
+			array(
+				'masteriyo_manager'    => array(
+					'display_name' => esc_html__( 'Masteriyo Manager', 'masteriyo' ),
+					'capabilities' => Capabilities::get_manager_capabilities(),
+				),
+				'masteriyo_instructor' => array(
+					'display_name' => esc_html__( 'Masteriyo Instructor', 'masteriyo' ),
+					'capabilities' => Capabilities::get_instructor_capabilities(),
+				),
+				'masteriyo_student'    => array(
+					'display_name' => esc_html__( 'Masteriyo Student', 'masteriyo' ),
+					'capabilities' => Capabilities::get_student_capabilities(),
+				),
+			)
+		);
+	}
+}

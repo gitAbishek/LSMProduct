d19772a713ec8f17d4378ef43a0529615b5a5328
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2021-05-21 13:12:42 +0545

    Refactor the Capabilities class

--- includes/Capabilities.php
+++ includes/Capabilities.php
@@ -12,81 +12,34 @@ namespace ThemeGrill\Masteriyo;
  */
 class Capabilities {
 	/**
-	 * Get masteriyo manager capabilites.
+	 * Get masteriyo student capabilites.
 	 *
 	 * @since 0.1.0
 	 *
 	 * @return array
 	 */
-	public static function get_manager_capabilities() {
-		$core_capabilities = array(
-			'read_users',
-
-			'delete_course',
-			'delete_courses',
-			'delete_others_courses',
-			'delete_published_courses',
-			'read_courses',
-
-			'delete_section',
-			'delete_sections',
-			'delete_others_sections',
-			'delete_published_sections',
-			'read_sections',
-
-			'delete_lesson',
-			'delete_lessons',
-			'delete_others_lessons',
-			'delete_published_lessons',
-			'read_lessons',
-
-			'delete_quiz',
-			'delete_quizzes',
-			'delete_others_quizzes',
-			'delete_published_quizzes',
-			'read_quizzes',
-
-			'delete_question',
-			'delete_questions',
-			'delete_others_questions',
-			'delete_published_questions',
-			'read_questions',
-
-			'manage_masteriyo_setting',
-
-			'delete_order',
-			'delete_orders',
-			'delete_others_orders',
-			'delete_published_orders',
-			'read_orders',
-			'read_others_orders',
-			'edit_order',
-			'edit_orders',
-			'edit_others_orders',
-			'edit_published_orders',
-
-			'delete_faq',
-			'delete_faqs',
-			'delete_others_faqs',
-			'delete_published_faqs',
-			'read_faqs',
-
-			'delete_course_review',
-			'delete_course_reviews',
-			'delete_others_course_reviews',
-			'delete_published_course_reviews',
-			'read_course_reviews',
-			'edit_course_review',
-			'edit_course_reviews',
-			'edit_others_course_reviews',
-			'edit_published_course_reviews',
+	public static function get_student_capabilities() {
+		$caps_info = apply_filters(
+			'masteriyo_manager_core_caps_info',
+			array(
+				'course'            => array( 'read' ),
+				'section'           => array( 'read' ),
+				'lesson'            => array( 'read' ),
+				'faq'               => array( 'read' ),
+				'quiz'              => array( 'read' ),
+				'question'          => array( 'read' ),
+				'course_review'     => array( 'publish', 'read', 'edit_plural', 'delete_plural' ),
+				'order'             => array( 'publish', 'read', 'delete' ),
+				'users'             => array( 'read' ),
+			)
 		);
+		$core_capabilities = self::map_caps( $caps_info );
 
 		return apply_filters(
-			'masteriyo_get_manager_capabilities',
+			'masteriyo_get_student_capabilities',
 			array_merge(
 				array_fill_keys( array_unique( $core_capabilities ), true ),
-				get_role( 'editor' )->capabilities
+				get_role( 'contributor' )->capabilities
 			)
 		);
 	}
@@ -99,64 +52,20 @@ class Capabilities {
 	 * @return array
 	 */
 	public static function get_instructor_capabilities() {
-		$core_capabilities = array(
-			'read_users',
-
-			'publish_courses',
-			'delete_courses',
-			'delete_published_courses',
-			'read_courses',
-			'edit_course',
-			'edit_courses',
-			'edit_published_courses',
-
-			'publish_lessons',
-			'delete_lessons',
-			'delete_published_lessons',
-			'delete_lesson',
-			'read_lessons',
-			'edit_lesson',
-			'edit_lessons',
-			'edit_published_lessons',
-
-			'publish_sections',
-			'delete_sections',
-			'delete_published_sections',
-			'delete_section',
-			'read_sections',
-			'edit_section',
-			'edit_sections',
-			'edit_published_sections',
-
-			'publish_quizzes',
-			'delete_quizzes',
-			'delete_published_quizzes',
-			'delete_quiz',
-			'read_quizzes',
-			'edit_quize',
-			'edit_quizzes',
-			'edit_published_quizzes',
-
-			'publish_questions',
-			'delete_questions',
-			'delete_published_questions',
-			'delete_question',
-			'read_questions',
-			'edit_question',
-			'edit_questions',
-			'edit_published_questions',
-
-			'publish_faqs',
-			'delete_faqs',
-			'delete_published_faqs',
-			'delete_faq',
-			'read_faqs',
-			'edit_faq',
-			'edit_faqs',
-			'edit_published_faqs',
-
-			'read_course_reviews',
+		$caps_info = apply_filters(
+			'masteriyo_instructor_core_caps_info',
+			array(
+				'course'            => array( 'publish', 'read', 'edit', 'delete' ),
+				'section'           => array( 'publish', 'read', 'edit', 'delete' ),
+				'lesson'            => array( 'publish', 'read', 'edit', 'delete' ),
+				'faq'               => array( 'publish', 'read', 'edit', 'delete' ),
+				'quiz'              => array( 'publish', 'read', 'edit', 'delete' ),
+				'question'          => array( 'publish', 'read', 'edit', 'delete' ),
+				'course_review'     => array( 'read', 'edit_plural', 'delete_plural' ),
+				'users'             => array( 'read' ),
+			)
 		);
+		$core_capabilities = self::map_caps( $caps_info );
 
 		return apply_filters(
 			'masteriyo_get_instructor_capabilities',
@@ -168,46 +77,35 @@ class Capabilities {
 	}
 
 	/**
-	 * Get masteriyo student capabilites.
+	 * Get masteriyo manager capabilites.
 	 *
 	 * @since 0.1.0
 	 *
 	 * @return array
 	 */
-	public static function get_student_capabilities() {
-		$core_capabilities = array(
-			'read_users',
-			'read_courses',
-			'read_lessons',
-			'read_sections',
-			'read_quizzes',
-			'read_questions',
-			'read_faqs',
-
-			'publish_orders',
-			'delete_orders',
-			'delete_published_orders',
-			'delete_order',
-			'read_orders',
-			'edit_order',
-			'edit_orders',
-			'edit_published_orders',
-
-			'publish_course_reviews',
-			'delete_course_reviews',
-			'delete_published_course_reviews',
-			'delete_course_review',
-			'read_course_reviews',
-			'edit_course_review',
-			'edit_course_reviews',
-			'edit_published_course_reviews',
+	public static function get_manager_capabilities() {
+		$caps_info = apply_filters(
+			'masteriyo_manager_core_caps_info',
+			array(
+				'course'            => array( 'publish', 'read', 'edit', 'delete', 'delete_others' ),
+				'section'           => array( 'publish', 'read', 'edit', 'delete', 'delete_others' ),
+				'lesson'            => array( 'publish', 'read', 'edit', 'delete', 'delete_others' ),
+				'faq'               => array( 'publish', 'read', 'edit', 'delete', 'delete_others' ),
+				'quiz'              => array( 'publish', 'read', 'edit', 'delete', 'delete_others' ),
+				'question'          => array( 'publish', 'read', 'read_others', 'edit', 'delete', 'delete_others' ),
+				'order'             => array( 'read', 'read_others', 'edit', 'edit_others', 'delete', 'delete_others' ),
+				'masteriyo_setting' => array( 'manage_singular' ),
+				'course_review'     => array( 'read', 'edit_plural', 'edit_others', 'delete_plural', 'delete_others' ),
+				'users'             => array( 'read' ),
+			)
 		);
+		$core_capabilities = self::map_caps( $caps_info );
 
 		return apply_filters(
-			'masteriyo_get_student_capabilities',
+			'masteriyo_get_manager_capabilities',
 			array_merge(
 				array_fill_keys( array_unique( $core_capabilities ), true ),
-				get_role( 'contributor' )->capabilities
+				get_role( 'editor' )->capabilities
 			)
 		);
 	}
@@ -220,70 +118,102 @@ class Capabilities {
 	 * @return array
 	 */
 	public static function get_admin_capabilities() {
-		$core_capabilities = array(
-			'delete_course',
-			'delete_courses',
-			'delete_others_courses',
-			'delete_published_courses',
-			'read_courses',
-
-			'delete_lesson',
-			'delete_lessons',
-			'delete_others_lessons',
-			'delete_published_lessons',
-			'read_lessons',
-
-			'delete_section',
-			'delete_sections',
-			'delete_others_sections',
-			'delete_published_sections',
-			'read_sections',
-
-			'delete_quiz',
-			'delete_quizzes',
-			'delete_others_quizzes',
-			'delete_published_quizzes',
-			'read_quizzes',
-
-			'delete_question',
-			'delete_questions',
-			'delete_others_questions',
-			'delete_published_questions',
-			'read_questions',
-
-			'manage_masteriyo_setting',
-
-			'delete_order',
-			'delete_orders',
-			'delete_others_orders',
-			'delete_published_orders',
-			'read_orders',
-			'read_others_orders',
-			'edit_order',
-			'edit_orders',
-			'edit_others_orders',
-			'edit_published_orders',
-
-			'delete_faq',
-			'delete_faqs',
-			'delete_others_faqs',
-			'delete_published_faqs',
-			'read_faqs',
-
-			'delete_course_review',
-			'delete_course_reviews',
-			'delete_others_course_reviews',
-			'delete_published_course_reviews',
-			'read_course_reviews',
-			'edit_course_review',
-			'edit_course_reviews',
-			'edit_others_course_reviews',
-			'edit_published_course_reviews',
+		$caps_info = apply_filters(
+			'masteriyo_admin_core_caps_info',
+			array(
+				'course'            => array( 'read', 'delete', 'delete_others' ),
+				'section'           => array( 'read', 'delete', 'delete_others' ),
+				'lesson'            => array( 'read', 'delete', 'delete_others' ),
+				'faq'               => array( 'read', 'delete', 'delete_others' ),
+				'quiz'              => array( 'read', 'delete', 'delete_others' ),
+				'question'          => array( 'read', 'delete', 'delete_others' ),
+				'order'             => array( 'read', 'read_others', 'edit', 'edit_others', 'delete', 'delete_others' ),
+				'masteriyo_setting' => array( 'manage_singular' ),
+				'course_review'     => array( 'read', 'edit_plural', 'edit_others', 'delete_plural', 'delete_others' ),
+				'users'             => array( 'create', 'read', 'edit', 'delete' ),
+			)
 		);
+		$core_capabilities = self::map_caps( $caps_info );
 
 		return apply_filters(
 			'masteriyo_get_administrator_capabilities',
 			array_fill_keys( array_unique( $core_capabilities ), true )
 		);
+	}
+
+	/**
+	 * Get capability types.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
+	protected static function get_capability_types() {
+		$cap_types = array(
+			'course'            => array( 'course', 'courses' ),
+			'section'           => array( 'section', 'sections' ),
+			'lesson'            => array( 'lesson', 'lessons' ),
+			'faq'               => array( 'faq', 'faqs' ),
+			'question'          => array( 'question', 'questions' ),
+			'quiz'              => array( 'quiz', 'quizzes' ),
+			'order'             => array( 'order', 'orders' ),
+			'masteriyo_setting' => array( 'masteriyo_setting' ),
+			'course_review'     => array( 'course_review', 'course_reviews' ),
+			'users'             => array( 'user', 'users' ),
+		);
+
+		return apply_filters( 'masteriyo_cap_types', $cap_types );
+	}
+
+	/**
+	 * Map cap types to a list of capabilities.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param array $caps_info
+	 *
+	 * @return array
+	 */
+	protected static function map_caps( $caps_info ) {
+		$caps = array();
+		$all_cap_types = self::get_capability_types();
+
+		foreach ( $caps_info as $cap_type => $permissions ) {
+			if ( empty( $all_cap_types[ $cap_type ] ) ) {
+				continue;
+			}
+			$cap_types = $all_cap_types[ $cap_type ];
+			$singular_type = $cap_types[0];
+			$plural_type = isset( $cap_types[1] ) ? $cap_types[1] : $singular_type;
+
+			foreach ( $permissions as $permission_type ) {
+				if ( 'edit' === $permission_type ) {
+					$caps[] = "edit_{$singular_type}";
+					$caps[] = "edit_{$plural_type}";
+					$caps[] = "edit_published_{$plural_type}";
+					continue;
+				}
+				if ( 'edit_plural' === $permission_type ) {
+					$caps[] = "edit_{$plural_type}";
+					continue;
+				}
+				if ( 'delete' === $permission_type ) {
+					$caps[] = "delete_{$singular_type}";
+					$caps[] = "delete_{$plural_type}";
+					$caps[] = "delete_published_{$plural_type}";
+					continue;
+				}
+				if ( 'delete_plural' === $permission_type ) {
+					$caps[] = "delete_{$plural_type}";
+					continue;
+				}
+				if ( 'manage_singular' === $permission_type ) {
+					$caps[] = "manage_{$singular_type}";
+					continue;
+				}
+				$caps[] = "{$permission_type}_{$plural_type}";
+			}
+		}
+		return $caps;
 	}
 }

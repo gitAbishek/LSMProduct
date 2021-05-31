7c4533bd73d9ca7c775c16e82dec99708c00bec3
Shanesh Nepali <sns.themegrill@gmail.com>
2021-05-31 12:24:50 +0545

    fix white color and list padding

--- assets/js/edit-myaccount.js
+++ assets/js/edit-myaccount.js
@@ -1,13 +1,13 @@
 /**
  * global masteriyo_data
  */
-(function( $, mto_data ) {
+(function ($, mto_data) {
 	let isSaving = false;
 
 	/**
 	 * Tabs handler.
 	 */
-	$( document.body ).on( 'click', '.mto-tab', function() {
+	$(document.body).on('click', '.mto-tab', function () {
 		$(this).siblings('.mto-tab').removeClass('mto-active-tab');
 		$(this).addClass('mto-active-tab');
 		$('.mto-tab-content').addClass('mto-hidden');
@@ -17,10 +17,10 @@
 	/**
 	 * Edit profile form submission handler.
 	 */
-	$( document.body ).on( 'submit', 'form#mto-edit-profile-form', function(e) {
+	$(document.body).on('submit', 'form#mto-edit-profile-form', function (e) {
 		e.preventDefault();
 
-		if ( isSaving ) return;
+		if (isSaving) return;
 
 		isSaving = true;
 
@@ -38,8 +38,9 @@
 
 		// Show saving process indicator.
 		$('#mto-btn-submit-edit-profile-form')
-		.text( mto_data.labels.saving )
-		.siblings('.mto-notify-message').remove();
+			.text(mto_data.labels.saving)
+			.siblings('.mto-notify-message')
+			.remove();
 
 		$.ajax({
 			type: 'POST',
@@ -48,37 +49,34 @@
 				'Content-Type': 'application/json',
 				'X-WP-Nonce': mto_data.nonce,
 			},
-			url: mto_data.rootApiUrl + 'masteriyo/v1/users/' + mto_data.current_user_id,
+			url:
+				mto_data.rootApiUrl + 'masteriyo/v1/users/' + mto_data.current_user_id,
 			data: JSON.stringify(userData),
-			success: function( res ) {
+			success: function (res) {
 				// Update username on the sidebar.
-				$('#label-username').text( res.display_name );
+				$('#label-username').text(res.display_name);
 
 				// Show success message.
-				$('#mto-btn-submit-edit-profile-form')
-				.after('<div class="mto-notify-message mto-success-msg"><span>' + mto_data.labels.profile_update_success + '</span></div>');
+				$('#mto-btn-submit-edit-profile-form').after(
+					'<div class="mto-notify-message mto-success-msg"><span>' +
+						mto_data.labels.profile_update_success +
+						'</span></div>'
+				);
 			},
-			error: function( xhr ) {
+			error: function (xhr) {
 				// Show failure message.
-				$('#mto-btn-submit-edit-profile-form')
-				.after('<div class="mto-notify-message mto-error-msg mto-text-red-700 mto-bg-red-100 mto-border-red-300"><span>' + xhr.responseJSON.message + '</span></div>');
+				$('#mto-btn-submit-edit-profile-form').after(
+					'<div class="mto-notify-message mto-error-msg mto-text-red-700 mto-bg-red-100 mto-border-red-300"><span>' +
+						xhr.responseJSON.message +
+						'</span></div>'
+				);
 			},
-			complete: function() {
+			complete: function () {
 				isSaving = false;
 
 				// Remove saving process indicator.
-				$('#mto-btn-submit-edit-profile-form').text( mto_data.labels.save );
+				$('#mto-btn-submit-edit-profile-form').text(mto_data.labels.save);
 			},
 		});
-	});
-
-	/**
-	 * Mobile view sidebar menu toggler.
-	 */
-	$('.menu-open').click(function(){
-		$('#vertical-menu').removeClass('mto--ml-9999');
-	});
-	$('.menu-close').click(function(){
-		$('#vertical-menu').addClass('mto--ml-9999');
 	});
 })(jQuery, window.masteriyo_data);
--- assets/js/single-course.js
+++ assets/js/single-course.js
@@ -1,75 +1,77 @@
 function selectTab(tabIndex) {
-    var countTab;
-    //Hide All Tabs
-    for(countTab=1;countTab<=4;countTab++){
-        var tab_content = document.getElementById('tab'+ countTab+'Content');
-        var tab_handle = document.getElementById('tab' + countTab);
+	var countTab;
+	//Hide All Tabs
+	for (countTab = 1; countTab <= 4; countTab++) {
+		var tab_content = document.getElementById('tab' + countTab + 'Content');
+		var tab_handle = document.getElementById('tab' + countTab);
 
-        if ( tab_content ) {
-            tab_content.style.display="none";
-        }
-        if ( tab_handle ) {
-            tab_handle.classList.remove("active-tab");
-        }
-    }
+		if (tab_content) {
+			tab_content.style.display = 'none';
+		}
+		if (tab_handle) {
+			tab_handle.classList.remove('active-tab');
+		}
+	}
 
-    //Show the Selected Tab
-    document.getElementById('tab' + tabIndex + 'Content').style.display="block";
-    document.getElementById('tab' + tabIndex).classList.add("active-tab");
+	//Show the Selected Tab
+	document.getElementById('tab' + tabIndex + 'Content').style.display = 'block';
+	document.getElementById('tab' + tabIndex).classList.add('active-tab');
 }
 
-(function($) {
-    
-    // FAQ Tab
-    $(document.body).on('click', '.mto-faq--item-header', function(){
-        $(this).siblings('.mto-faq--item-body').first().slideToggle('swing');
+(function ($) {
+	// FAQ Tab
+	$(document.body).on('click', '.mto-faq--item-header', function () {
+		$(this).siblings('.mto-faq--item-body').first().slideToggle('swing');
+	});
 
-    });
+	// Curriculam Tab
+	$(document.body).on('click', '.mto-cheader', function () {
+		$(this).parent('.mto-stab--citems').toggleClass('active');
+		if (
+			$('.mto-stab--citems').length === $('.mto-stab--citems.active').length
+		) {
+			expandAllSections();
+		}
+		if (
+			$('.mto-stab--citems').length ===
+			$('.mto-stab--citems').not('.active').length
+		) {
+			collapseAllSections();
+		}
+	});
+	var isCollapsedAll = true;
+	$(document.body).on('click', '.mto-expand-collape-all', function () {
+		if (isCollapsedAll) {
+			expandAllSections();
+		} else {
+			collapseAllSections();
+		}
+	});
 
-    // Curriculam Tab
-    $(document.body).on('click', '.mto-cheader', function(){
-        $(this).parent('.mto-stab--citems').toggleClass('active');
-        if ( $('.mto-stab--citems').length === $('.mto-stab--citems.active').length ) {
-            expandAllSections();
-        }
-        if ( $('.mto-stab--citems').length === $('.mto-stab--citems').not('.active').length ) {
-            collapseAllSections();
-        }
-    });
-    var isCollapsedAll = true;
-    $(document.body).on('click', '.mto-expand-collape-all', function() {
-        if ( isCollapsedAll ) {
-            expandAllSections();
-        } else {
-            collapseAllSections();
-        }
-    });
+	// Expand all
+	function expandAllSections() {
+		$('.mto-stab--citems').addClass('active');
+		$('.mto-expand-collape-all').text('Collapse All');
+		isCollapsedAll = false;
+	}
 
-    // Expand all
-    function expandAllSections() {
-        $('.mto-stab--citems').addClass('active');
-        $('.mto-expand-collape-all').text( 'Collapse All' );
-        isCollapsedAll = false;
-    }
+	// Collapse all
+	function collapseAllSections() {
+		$('.mto-stab--citems').removeClass('active');
+		$('.mto-expand-collape-all').text('Expand All');
+		isCollapsedAll = true;
+	}
 
-    // Collapse all
-    function collapseAllSections() {
-        $('.mto-stab--citems').removeClass('active');
-        $('.mto-expand-collape-all').text( 'Expand All' );
-        isCollapsedAll = true;
-    }
+	// Sticky
+	var stickyTop = $('.mto-sticky').offset().top;
 
-    // Sticky
-    var stickyTop = $('.mto-sticky').offset().top;
+	$(window).scroll(function () {
+		var windowTop = $(window).scrollTop();
 
-    $(window).scroll(function() {
-        var windowTop = $(window).scrollTop();
-
-    if (stickyTop < windowTop) {
-        $('.mto-sticky').css({'position': 'fixed', 'top': '20px'});
-    } else {
-        $('.mto-sticky').css('position', 'relative');
-    }
-    });
-
+		if (stickyTop < windowTop) {
+			$('.mto-sticky').css({ position: 'fixed', top: '20px' });
+		} else {
+			$('.mto-sticky').css('position', 'relative');
+		}
+	});
 })(jQuery);

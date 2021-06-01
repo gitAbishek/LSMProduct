3ae9fdff367814542c06305d33d27feb72b67150
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-01 07:44:09 +0545

    Add processing order setting options

--- assets/js/back-end/screens/settings/components/EmailSetttings.tsx
+++ assets/js/back-end/screens/settings/components/EmailSetttings.tsx
@@ -36,6 +36,12 @@ const EmailSetttings: React.FC<Props> = (props) => {
 		control,
 	});
 
+	const showProcessingOrder = useWatch({
+		name: 'emails.processing_order.enable',
+		defaultValue: emailData?.processing_order.enable,
+		control,
+	});
+
 	const tabStyles = {
 		justifyContent: 'flex-start',
 		w: '180px',
@@ -188,6 +194,63 @@ const EmailSetttings: React.FC<Props> = (props) => {
 										<Editor
 											name="emails.new_order.content"
 											defaultValue={emailData?.new_order.content}
+											control={control}
+										/>
+									</FormControl>
+								</Stack>
+							</Collapse>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.processing_order.enable"
+										render={({ field }) => (
+											<Switch
+												{...field}
+												defaultChecked={emailData?.processing_order.enable}
+											/>
+										)}
+									/>
+								</Stack>
+							</FormControl>
+							<Collapse in={showProcessingOrder}>
+								<Stack direction="column" spacing="6">
+									<FormControl>
+										<FormLabel minW="160px">
+											{__('Subject', 'masteriyo')}
+										</FormLabel>
+										<Textarea
+											defaultValue={emailData?.processing_order?.subject}
+											{...register('emails.processing_order.subject')}
+										/>
+									</FormControl>
+
+									<FormControl>
+										<FormLabel minW="160px">
+											{__('Heading', 'masteriyo')}
+										</FormLabel>
+										<ImageUpload
+											name="emails.processing_order.heading"
+											mediaId={emailData?.processing_order.heading}
+											setValue={setValue}
+											register={register}
+										/>
+									</FormControl>
+
+									<FormControl>
+										<FormLabel minW="160px">
+											{__('Content', 'masteriyo')}
+										</FormLabel>
+										<Editor
+											name="emails.processing_order.content"
+											defaultValue={emailData?.processing_order.content}
 											control={control}
 										/>
 									</FormControl>

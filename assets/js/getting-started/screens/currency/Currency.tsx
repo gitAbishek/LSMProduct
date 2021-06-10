b861656e206f2ead2482127943c81de6b39649c5
Aashil <ws.aashil@gmail.com>
2021-06-10 16:25:32 +0545

    Added package support for Preview and cleanup codes

--- assets/js/getting-started/screens/currency/Currency.tsx
+++ assets/js/getting-started/screens/currency/Currency.tsx
@@ -18,253 +18,143 @@ import {
 	Text,
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
-import React, { useEffect, useState } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import React from 'react';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
+import { currency } from '../../../back-end/utils/currency';
+import getSymbolFromCurrency from 'currency-symbol-map';
+import CurrencyInput from 'react-currency-input-field';
 interface Props {
-	setTabIndex?: any;
 	dashboardURL: string;
+	prevStep: () => void;
+	nextStep: () => void;
 }
 
-declare var masteriyo: any;
-
-const Currency: React.FC<Props> = (props) => {
-	const { setTabIndex, dashboardURL } = props;
-	const [previewPrice, SetPreviewPrice] = useState('$999.6789');
-	const {
-		register,
-		formState: { errors },
+const Preview = ({ control }) => {
+	// Watch entire currency form.
+	const watchData = useWatch({
 		control,
-	} = useFormContext();
-
-	const [data, setData] = useState<any>({
-		currencyValue: 'USD',
-		currencyLabel: 'United State Dollar ($)',
-		position: 'left',
-		thousandSeparator: ',',
-		decimalSeparator: '.',
-		decimalNumber: '4',
 	});
 
-	useEffect(() => {
-		handlePreview();
-	}, [data]);
+	if ('undefined' != typeof watchData.general) {
+		let currencySymbol: string;
+		const {
+			currency,
+			currency_position,
+			thousands_separator,
+			decimal_separator,
+			number_of_decimals,
+		} = watchData.general;
 
-	// options for dev server.
-	const options = [
-		{ label: 'United Arab Emirates dirham (د.إ)', value: 'AED' },
-		{ label: 'Afghan afghani (؋)', value: 'AFN' },
-		{ label: 'Albanian lek (L)', value: 'ALL' },
-		{ label: 'United State Dollar ($)', value: 'USD' },
-		{ label: 'Nepalese rupee (₨)', value: 'NPR' },
-		{ label: 'Turkish lira (₺)', value: 'TRY' },
-	];
+		currencySymbol = getSymbolFromCurrency(currency);
 
-	// Currencies option for live server.
-	const currencies: any =
-		'undefined' != typeof masteriyo && masteriyo.currencies;
-
-	const handleChangeSelect = (e?: any) => {
-		let name = e.target.name;
-		let index = e.target.selectedIndex;
-		let label = e.target[index].text;
-		let value = e.target.value;
-
-		if ('currencyLabel' === name) {
-			setData((prevState?: any) => ({
-				...prevState,
-				[name]: label,
-				['currencyValue']: value,
-			}));
-		} else {
-			setData((prevState?: any) => ({
-				...prevState,
-				[name]: value,
-			}));
+		if ('left' === currency_position) {
+			return (
+				<CurrencyInput
+					prefix={currencySymbol}
+					groupSeparator={thousands_separator}
+					decimalSeparator={decimal_separator}
+					decimalScale={number_of_decimals}
+					value={9999.99}
+				/>
+			);
+		} else if ('right' === currency_position) {
+			return (
+				<CurrencyInput
+					suffix={currencySymbol}
+					groupSeparator={thousands_separator}
+					decimalSeparator={decimal_separator}
+					decimalScale={number_of_decimals}
+					value={9999.99}
+				/>
+			);
 		}
-	};
-
-	const handleChangeInput = (e?: any) => {
-		const { name, value } = e.target;
-
-		setData((prevState?: any) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-
-	const handleChangeNumber = (e?: any) => {
-		const value = e;
-
-		setData((prevState?: any) => ({
-			...prevState,
-			['decimalNumber']: value,
-		}));
-	};
-
-	/**
-	 * Handles the price preview.
-	 */
-	function handlePreview() {
-		let preview: any;
-		let regExp = /\(([^)]+)\)/;
-		let currencySymbolMatch = '$';
-
-		if ('undefined' != typeof data.currencyLabel) {
-			let matches = regExp.exec(data.currencyLabel);
-
-			//matches[1] contains the value between the parentheses
-			currencySymbolMatch = matches[1];
-		}
-
-		let currencyPosition =
-			'undefined' != typeof data.position ? data.position : 'left';
-
-		let price = parseFloat(`999${data.decimalSeparator}6789`).toFixed(
-			data.decimalNumber
-		);
-		price = price.toLocaleString();
-
-		if ('left' == currencyPosition) {
-			preview = currencySymbolMatch + price;
-		} else if ('right' == currencyPosition) {
-			preview = price + currencySymbolMatch;
-		}
-
-		SetPreviewPrice(preview);
 	}
+
+	// Default value.
+	return <CurrencyInput prefix={'$'} value={9999.99} />;
+};
+
+const Currency: React.FC<Props> = (props) => {
+	const { dashboardURL, prevStep, nextStep } = props;
+	const { register, control } = useFormContext();
 
 	return (
 		<Box rounded="3px">
 			<Box bg="white" p="30" shadow="box">
 				<Stack direction="column" spacing="8">
-					<FormControl id="currency">
+					<FormControl>
 						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
+							<FormLabel sx={{ fontWeight: 'bold' }}>
 								{__('Currency', 'masteriyo')}
 							</FormLabel>
-							<Controller
-								name="currency"
-								control={control}
-								render={({ field }) => (
-									<Select
-										name="currencyLabel"
-										value={data.currencyValue}
-										onChange={(e) => {
-											handleChangeSelect(e);
-											field.onChange(e);
-										}}
-										w="md">
-										{'undefined' != typeof masteriyo
-											? currencies.map((data: any, index: number) => {
-													return (
-														<option key={index} value={data.value}>
-															{data.label}
-														</option>
-													);
-											  })
-											: options.map((data: any, index: number) => {
-													return (
-														<option key={index} value={data.value}>
-															{data.label}
-														</option>
-													);
-											  })}
-									</Select>
-								)}
-							/>
+							<Select
+								w="md"
+								{...register('general.currency')}
+								defaultValue="USD">
+								{Object.entries(currency).map(([code, name]) => (
+									<option value={code} key={code}>
+										{name} ({getSymbolFromCurrency(code)})
+									</option>
+								))}
+							</Select>
 						</Flex>
 					</FormControl>
 
-					<FormControl id="currency-position">
+					<FormControl>
 						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
+							<FormLabel sx={{ fontWeight: 'bold' }}>
 								{__('Currency Position', 'masteriyo')}
 							</FormLabel>
-							<Controller
-								name="currency_position"
-								control={control}
-								render={({ field }) => (
-									<Select
-										w="md"
-										name="position"
-										value={data.position}
-										onChange={(e) => {
-											handleChangeSelect(e);
-											field.onChange(e);
-										}}>
-										<option value="left">{__('Left', 'masteriyo')}</option>
-										<option value="right">{__('Right', 'masteriyo')}</option>
-									</Select>
-								)}
-							/>
+							<Select
+								w="md"
+								defaultValue="left"
+								{...register('general.currency_position')}>
+								<option value="left">{__('Left', 'masteriyo')}</option>
+								<option value="right">{__('Right', 'masteriyo')}</option>
+							</Select>
 						</Flex>
 					</FormControl>
 
-					<FormControl id="thousand-separator">
+					<FormControl>
 						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
+							<FormLabel sx={{ fontWeight: 'bold' }}>
 								{__('Thousands Separator', 'masteriyo')}
 							</FormLabel>
-							<Controller
-								name="thousand_separator"
-								control={control}
-								render={({ field }) => (
-									<Input
-										value={data.thousandSeparator}
-										w="md"
-										onChange={(e) => {
-											handleChangeInput(e);
-											field.onChange(e);
-										}}
-										name="thousandSeparator"
-									/>
-								)}
+							<Input
+								defaultValue=","
+								w="md"
+								{...register('general.thousands_separator')}
 							/>
 						</Flex>
 					</FormControl>
 
-					<FormControl id="decimal-separator">
+					<FormControl>
 						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
+							<FormLabel sx={{ fontWeight: 'bold' }}>
 								{__('Decimal Separator', 'masteriyo')}
 							</FormLabel>
-							<Controller
-								name="decimal_separator"
-								control={control}
-								render={({ field }) => (
-									<Input
-										value={data.decimalSeparator}
-										w="md"
-										onChange={(e) => {
-											handleChangeInput(e);
-											field.onChange(e);
-										}}
-										name="decimalSeparator"
-									/>
-								)}
+							<Input
+								defaultValue="."
+								w="md"
+								{...register('general.decimal_separator')}
 							/>
 						</Flex>
 					</FormControl>
 
-					<FormControl id="no-of-decimal">
+					<FormControl>
 						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
+							<FormLabel sx={{ fontWeight: 'bold' }}>
 								{__('Number of Decimal', 'masteriyo')}
 							</FormLabel>
 							<Controller
-								name="number_of_decimals"
-								control={control}
+								name="general.number_of_decimals"
 								render={({ field }) => (
 									<NumberInput
+										{...field}
 										w="md"
 										min={1}
 										max={4}
-										value={data.decimalNumber}
-										name="decimalNumber"
-										onChange={(e) => {
-											handleChangeNumber(e);
-											field.onChange(e);
-										}}>
+										defaultValue="2">
 										<NumberInputField />
 										<NumberInputStepper>
 											<NumberIncrementStepper />
@@ -280,31 +170,28 @@ const Currency: React.FC<Props> = (props) => {
 						<strong>
 							<Text fontSize="sm">{__('Preview', 'masteriyo')}</Text>
 						</strong>
-						<Box bg="#F9F9F9" rounded="3" boxSizing="border-box" w="md">
-							<Heading color="#78A6FF" as="h2" size="md">
-								{previewPrice}
+						<Box rounded="3" boxSizing="border-box" w="md">
+							<Heading color="blue.500" as="h2" size="md">
+								<Preview control={control} />
 							</Heading>
 						</Box>
 					</Flex>
 
 					<Flex justify="space-between" align="center">
 						<Button
-							onClick={() => setTabIndex(0)}
+							onClick={prevStep}
 							rounded="3px"
 							colorScheme="blue"
 							variant="outline">
 							{__('Back', 'masteriyo')}
 						</Button>
 						<ButtonGroup>
-							<Button onClick={() => setTabIndex(2)} variant="ghost">
+							<Button variant="ghost">
 								<Link href={dashboardURL ? dashboardURL : '#'}>
 									{__('Skip to Dashboard', 'masteriyo')}
 								</Link>
 							</Button>
-							<Button
-								onClick={() => setTabIndex(2)}
-								rounded="3px"
-								colorScheme="blue">
+							<Button onClick={nextStep} rounded="3px" colorScheme="blue">
 								{__('Next', 'masteriyo')}
 							</Button>
 						</ButtonGroup>

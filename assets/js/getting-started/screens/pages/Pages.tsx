3aeafd819d073d40ddf533f0c92304ff881477b4
Aashil <ws.aashil@gmail.com>
2021-06-10 16:30:52 +0545

    Used Pages API for pages options and added skeleton for loading

--- assets/js/getting-started/screens/pages/Pages.tsx
+++ assets/js/getting-started/screens/pages/Pages.tsx
@@ -9,147 +9,117 @@ import {
 	Select,
 	Stack,
 	Text,
+	Skeleton,
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useFormContext } from 'react-hook-form';
+import PagesAPI from '../../../back-end/utils/pages';
 interface Props {
-	setTabIndex?: any;
 	mutationLoading: boolean;
 	dashboardURL: string;
+	prevStep: () => void;
 }
 
 const Pages: React.FC<Props> = (props) => {
-	const { setTabIndex, mutationLoading, dashboardURL } = props;
-	const {
-		register,
-		formState: { errors },
-	} = useFormContext();
+	const { mutationLoading, dashboardURL, prevStep } = props;
+	const { register } = useFormContext();
+	const pageAPI = new PagesAPI();
+	const pagesQuery = useQuery('pages', () => pageAPI.list());
 
-	// Options for dev server.
-	const options = [
-		{ label: 'Select a page..', value: '' },
-		{ label: 'Masteriyo Checkout', value: '' },
-		{ label: 'Masteriyo Course List', value: 'AFN' },
-		{ label: 'Masteriyo My Account', value: 'ALL' },
-	];
-
-	const pagesOptions: any =
-		//@ts-ignore
-		'undefined' != typeof masteriyo && masteriyo.pagesOptions;
+	const renderPagesOption = () => {
+		try {
+			return (
+				<>
+					<option value="">{__('Select a page...', 'masteriyo')}</option>
+					{pagesQuery?.data?.map(
+						(page: { id: number; title: { rendered: string } }) => (
+							<option value={page.id} key={page.id}>
+								{page.title.rendered}
+							</option>
+						)
+					)}
+				</>
+			);
+		} catch (error) {
+			console.error(error);
+			return;
+		}
+	};
 
 	return (
 		<Box rounded="3px">
 			<Box bg="white" p="30" shadow="box">
 				<Stack direction="column" spacing="8">
-					<FormControl id="course-list-page">
-						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
-								{__('Course List', 'masteriyo')}
-							</FormLabel>
-							<Select w="md" {...register('course_list_page_id')}>
-								{
-									//@ts-ignore
-									'undefined' != typeof masteriyo
-										? pagesOptions.map((data: any, index: number) => {
-												return (
-													<option key={index} value={data.value}>
-														{data.label}
-													</option>
-												);
-										  })
-										: options.map((data: any, index: number) => {
-												return (
-													<option key={index} value={data.value}>
-														{data.label}
-													</option>
-												);
-										  })
-								}
-							</Select>
-						</Flex>
-					</FormControl>
+					{pagesQuery.isLoading ? (
+						<>
+							<Stack spacing="8">
+								<Skeleton h="6" />
+								<Skeleton h="6" />
+								<Skeleton h="6" />
+							</Stack>
+						</>
+					) : (
+						<>
+							<FormControl>
+								<Flex justify="space-between" align="center">
+									<FormLabel sx={{ fontWeight: 'bold' }}>
+										{__('Course List', 'masteriyo')}
+									</FormLabel>
+									<Select w="md" {...register('pages.course_list_page_id')}>
+										{renderPagesOption()}
+									</Select>
+								</Flex>
+							</FormControl>
 
-					<FormControl id="my-account-page">
-						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
-								<Text fontSize="sm">{__('My Account', 'masteriyo')}</Text>
-							</FormLabel>
-							<Select w="md" {...register('myaccount_page_id')}>
-								{
-									//@ts-ignore
-									'undefined' != typeof masteriyo
-										? pagesOptions.map((data: any, index: number) => {
-												return (
-													<option key={index} value={data.value}>
-														{data.label}
-													</option>
-												);
-										  })
-										: options.map((data: any, index: number) => {
-												return (
-													<option key={index} value={data.value}>
-														{data.label}
-													</option>
-												);
-										  })
-								}
-							</Select>
-						</Flex>
-					</FormControl>
+							<FormControl>
+								<Flex justify="space-between" align="center">
+									<FormLabel sx={{ fontWeight: 'bold' }}>
+										<Text fontSize="sm">{__('My Account', 'masteriyo')}</Text>
+									</FormLabel>
+									<Select w="md" {...register('pages.myaccount_page_id')}>
+										{renderPagesOption()}
+									</Select>
+								</Flex>
+							</FormControl>
 
-					<FormControl id="checkout-page">
-						<Flex justify="space-between" align="center">
-							<FormLabel style={{ fontWeight: 'bold' }}>
-								{__('Checkout', 'masteriyo')}
-							</FormLabel>
-							<Select w="md" {...register('checkout_page_id')}>
-								{
-									//@ts-ignore
-									'undefined' != typeof masteriyo
-										? pagesOptions.map((data: any, index: number) => {
-												return (
-													<option key={index} value={data.value}>
-														{data.label}
-													</option>
-												);
-										  })
-										: options.map((data: any, index: number) => {
-												return (
-													<option key={index} value={data.value}>
-														{data.label}
-													</option>
-												);
-										  })
-								}
-							</Select>
-						</Flex>
-					</FormControl>
+							<FormControl>
+								<Flex justify="space-between" align="center">
+									<FormLabel sx={{ fontWeight: 'bold' }}>
+										{__('Checkout', 'masteriyo')}
+									</FormLabel>
+									<Select w="md" {...register('pages.checkout_page_id')}>
+										{renderPagesOption()}
+									</Select>
+								</Flex>
+							</FormControl>
 
-					<Flex justify="space-between" align="center">
-						<Button
-							onClick={() => setTabIndex(3)}
-							rounded="3px"
-							colorScheme="blue"
-							variant="outline">
-							{__('Back', 'masteriyo')}
-						</Button>
-						<ButtonGroup>
-							<Button onClick={() => setTabIndex(5)} variant="ghost">
-								<Link href={dashboardURL ? dashboardURL : '#'}>
-									{__('Skip to Dashboard', 'masteriyo')}
-								</Link>
-							</Button>
-							<Button
-								type="submit"
-								// onClick={() => setTabIndex(5)}
-								isLoading={mutationLoading}
-								rounded="3px"
-								colorScheme="blue">
-								{__('Continue', 'masteriyo')}
-							</Button>
-						</ButtonGroup>
-					</Flex>
+							<Flex justify="space-between" align="center">
+								<Button
+									onClick={prevStep}
+									rounded="3px"
+									colorScheme="blue"
+									variant="outline">
+									{__('Back', 'masteriyo')}
+								</Button>
+								<ButtonGroup>
+									<Button variant="ghost">
+										<Link href={dashboardURL ? dashboardURL : '#'}>
+											{__('Skip to Dashboard', 'masteriyo')}
+										</Link>
+									</Button>
+									<Button
+										type="submit"
+										isLoading={mutationLoading}
+										rounded="3px"
+										colorScheme="blue">
+										{__('Finish', 'masteriyo')}
+									</Button>
+								</ButtonGroup>
+							</Flex>
+						</>
+					)}
 				</Stack>
 			</Box>
 		</Box>

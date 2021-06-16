a23260abd12f84f53f130631ba8b1cba4136a39b
Sagar Tamang <mi5t4n@gmail.com>
2021-06-16 12:23:53 +0545

    Added user course helper functions.

---
+++ includes/Helper/UserCourse.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User course functions.
+ *
+ * @since 0.1.0
+ * @version 0.1.0
+ * @package ThemeGrill\Masteriyo\Helper
+ */
+
+/**
+ * Get user course.
+ *
+ * @since 0.1.0
+ *
+ * @param int $user_course_id User course ID.
+ * @return ThemeGrill\Masteriyo\Models\UserCourse|NULL
+ */
+function masteriyo_get_user_course( $user_course_id ) {
+	try {
+		$user_course = masteriyo( 'user-course' );
+		$user_course->set_id( $user_course_id );
+
+		$user_course_repo = masteriyo( 'user-course.store' );
+		$user_course_repo->read( $user_course );
+	} catch ( \Except $e ) {
+		return null;
+	}
+}

b39a1e17ae122362a3130eeee848610a201a173b
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-16 09:03:22 +0545

    Show thumbnail image on lesson interactive page

--- assets/js/interactive/pages/lesson/InteractiveLesson.tsx
+++ assets/js/interactive/pages/lesson/InteractiveLesson.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import { Box, Container, Heading, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Box, Container, Heading, Text, Image } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import API from '../../../back-end/utils/api';
 import urls from '../../../back-end/constants/urls';
 import FullScreenLoader from '../../../back-end/components/layout/FullScreenLoader';
+import MediaAPI from '../../../back-end/utils/media';
 
 const InteractiveLesson = () => {
 	const { lessonId }: any = useParams();
 	const lessonAPI = new API(urls.lessons);
+	const [mediaId, setMediaId] = useState(0);
+	const imageAPi = new MediaAPI();
 
 	const lessonQuery = useQuery(
 		[`section${lessonId}`, lessonId],
 		() => lessonAPI.get(lessonId),
 		{
+			onSuccess: (data: any) => {
+				console.log(data);
+				setMediaId(data.featured_image);
+			},
 			onError: () => {
 				return;
 			},
+		}
+	);
+
+	const imageQuery = useQuery(
+		[`image${mediaId}`, mediaId],
+		() => imageAPi.get(mediaId),
+		{
+			enabled: mediaId !== 0,
 		}
 	);
 
@@ -24,13 +39,14 @@ const InteractiveLesson = () => {
 		return <FullScreenLoader />;
 	}
 
-	console.log(lessonQuery.data);
+	console.log(imageQuery.data);
 
 	return (
 		<Container centerContent maxW="container.xl" py="8">
 			<Box bg="white" p="8" shadow="box" w="full">
 				<Heading as="h5">{lessonQuery?.data?.name}</Heading>
 				<Text>{lessonQuery?.data?.description}</Text>
+				<Image src={imageQuery?.data?.source_url} />
 			</Box>
 		</Container>
 	);

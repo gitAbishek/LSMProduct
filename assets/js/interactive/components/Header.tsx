a42f688c455b29c7fd5c9cea333bbc769fc57f70
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-21 08:14:57 +0545

    Add basic functionality for the collapsible header

--- assets/js/interactive/components/Header.tsx
+++ assets/js/interactive/components/Header.tsx
@@ -13,76 +13,99 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 	Progress,
+	Slide,
 	Stack,
 	Text,
 	useDisclosure,
 } from '@chakra-ui/react';
 import React from 'react';
 import { Logo } from '../../back-end/constants/images';
-import { BiBell, BiHeart, BiInfoCircle, BiSearch } from 'react-icons/bi';
+import {
+	BiBell,
+	BiDownArrow,
+	BiHeart,
+	BiInfoCircle,
+	BiSearch,
+} from 'react-icons/bi';
 import { __ } from '@wordpress/i18n';
 
 const Header = () => {
-	const { onOpen, onClose, isOpen } = useDisclosure();
+	const {
+		onOpen: onProgressOpen,
+		onClose: onProgressClose,
+		isOpen: isProgressOpen,
+	} = useDisclosure();
+
+	const { isOpen: isHeaderOpen, onToggle: onHeaderToggle } = useDisclosure();
 
 	return (
-		<Box bg="white" shadow="box">
-			<Container maxW="container.xl">
-				<Stack
-					direction="row"
-					spacing="4"
-					align="center"
-					py="4"
-					justifyContent="space-between">
-					<Image src={Logo} maxW="180px" />
+		<>
+			<IconButton
+				onClick={onHeaderToggle}
+				icon={<BiDownArrow />}
+				aria-label="toggle header"
+			/>
+			<Slide direction="top" in={isHeaderOpen} style={{ zIndex: 999 }}>
+				<Container maxW="container.xl">
+					<Stack
+						direction="row"
+						spacing="4"
+						align="center"
+						py="4"
+						justifyContent="space-between">
+						<Image src={Logo} maxW="180px" />
 
-					<Stack direction="row" spacing="6" align="center">
-						<Stack direction="column" spacing="1">
-							<Stack direction="row" justify="space-between" align="center">
-								<Stack direction="row">
-									<Heading>35%</Heading>
-									<Text>Complete</Text>
+						<Stack direction="row" spacing="6" align="center">
+							<Stack direction="column" spacing="1">
+								<Stack direction="row" justify="space-between" align="center">
+									<Stack direction="row">
+										<Heading>35%</Heading>
+										<Text>Complete</Text>
+									</Stack>
+									<Stack direction="row">
+										<Text>11/21 Steps | </Text>
+										<Text>2 lessons left | </Text>
+										<Text>1 Quiz left</Text>
+										<Popover
+											isOpen={isProgressOpen}
+											onClose={onProgressClose}
+											onOpen={onProgressOpen}>
+											<PopoverTrigger>
+												<IconButton
+													py="0"
+													variant="link"
+													minH="auto"
+													icon={<BiInfoCircle />}
+													aria-label="open progress"
+												/>
+											</PopoverTrigger>
+											<PopoverContent p="8">
+												<PopoverArrow />
+												<CircularProgress value={60} />
+											</PopoverContent>
+										</Popover>
+									</Stack>
 								</Stack>
-								<Stack direction="row">
-									<Text>11/21 Steps | </Text>
-									<Text>2 lessons left | </Text>
-									<Text>1 Quiz left</Text>
-									<Popover isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
-										<PopoverTrigger>
-											<IconButton
-												py="0"
-												variant="link"
-												minH="auto"
-												icon={<BiInfoCircle />}
-												aria-label="open progress"
-											/>
-										</PopoverTrigger>
-										<PopoverContent p="8">
-											<PopoverArrow />
-											<CircularProgress value={60} />
-										</PopoverContent>
-									</Popover>
-								</Stack>
+								<Progress value={80} size="xs" />
 							</Stack>
-							<Progress value={80} size="xs" />
+							<Button
+								colorScheme="blue"
+								rounded="3xl"
+								textTransform="uppercase"
+								fontWeight="bold">
+								{__('Submit Quiz', 'masteriyo')}
+							</Button>
 						</Stack>
-						<Button
-							colorScheme="blue"
-							rounded="3xl"
-							textTransform="uppercase"
-							fontWeight="bold">
-							{__('Submit Quiz', 'masteriyo')}
-						</Button>
+						<Stack direction="row" spacing="2" align="center">
+							<Icon as={BiSearch} />
+							<Icon as={BiHeart} />
+							<Icon as={BiBell} />
+							<Avatar size="sm" />
+						</Stack>
 					</Stack>
-					<Stack direction="row" spacing="2" align="center">
-						<Icon as={BiSearch} />
-						<Icon as={BiHeart} />
-						<Icon as={BiBell} />
-						<Avatar size="sm" />
-					</Stack>
-				</Stack>
-			</Container>
-		</Box>
+				</Container>
+			</Slide>
+		</>
 	);
 };
 

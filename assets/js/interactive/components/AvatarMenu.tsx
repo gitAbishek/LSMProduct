f34a28a80d26875d952d33b4e04668ccc516451f
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-21 10:08:04 +0545

    Add - user dropdown menu

--- assets/js/interactive/components/AvatarMenu.tsx
+++ assets/js/interactive/components/AvatarMenu.tsx
@@ -4,20 +4,25 @@ import {
 	MenuButton,
 	MenuList,
 	MenuItem,
-	MenuItemOption,
-	MenuGroup,
-	MenuOptionGroup,
-	MenuIcon,
-	MenuCommand,
 	MenuDivider,
 } from '@chakra-ui/react';
 import React from 'react';
+import { __ } from '@wordpress/i18n';
 
 const AvatarMenu = () => {
 	return (
-		<>
-			<Avatar size="sm" />
-		</>
+		<Menu placement="bottom-end">
+			<MenuButton>
+				<Avatar size="sm" />
+			</MenuButton>
+			<MenuList fontSize="sm">
+				<MenuItem>{__('My Profile', 'masteriyo')}</MenuItem>
+				<MenuItem>{__('Settings', 'masteriyo')}</MenuItem>
+				<MenuItem>{__('My Wishlist', 'masteriyo')}</MenuItem>
+				<MenuDivider />
+				<MenuItem>{__('Log Out', 'masteriyo')}</MenuItem>
+			</MenuList>
+		</Menu>
 	);
 };
 

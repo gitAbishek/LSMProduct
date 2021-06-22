f850e26a4b0e5f358e50915af83039d34129e9f1
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-22 07:05:27 +0545

    Complete design for the notification

--- assets/js/interactive/components/Notification.tsx
+++ assets/js/interactive/components/Notification.tsx
@@ -24,6 +24,12 @@ const Notification = () => {
 	const listItemStyles = {
 		d: 'flex',
 		justifyContent: 'space-between',
+		borderBottom: '1px',
+		borderColor: 'gray.100',
+		pb: '2',
+		_last: {
+			borderColor: 'transparent',
+		},
 	};
 
 	return (
@@ -37,7 +43,24 @@ const Notification = () => {
 				<PopoverArrow />
 				<PopoverHeader>{__('Notification', 'masteriyo')}</PopoverHeader>
 				<PopoverBody>
-					<List>
+					<List spacing="2">
+						<ListItem sx={listItemStyles}>
+							<Stack direction="row" spacing="2" maxW="240px">
+								<Avatar size="sm" />
+								<Stack direction="column" spacing="1">
+									<Heading as="h5" fontSize="sm">
+										Sarah Jones
+									</Heading>
+									<Text fontSize="xs">
+										Asked a question in{' '}
+										<Text as="strong">How to build WordPress website</Text>
+									</Text>
+								</Stack>
+							</Stack>
+							<Text fontSize="xs" color="gray.300" ml="4">
+								2 min Ago
+							</Text>
+						</ListItem>
 						<ListItem sx={listItemStyles}>
 							<Stack direction="row" spacing="2" maxW="240px">
 								<Avatar size="sm" />

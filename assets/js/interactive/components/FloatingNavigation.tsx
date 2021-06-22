141c7236bafa5d47d3e660dca0de63212fc85b10
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-22 09:17:14 +0545

    Add - previus navigation with an animation

--- assets/js/interactive/components/FloatingNavigation.tsx
+++ assets/js/interactive/components/FloatingNavigation.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import FloatingNextButton from './FloatingNextButton';
+import FloatingPrevButton from './FloatingPrevButton';
 
 const FloatingNavigation = () => {
 	return (
 		<>
 			<FloatingNextButton />
+			<FloatingPrevButton />
 		</>
 	);
 };

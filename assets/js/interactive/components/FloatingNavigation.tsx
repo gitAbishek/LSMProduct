53c6f304e12334ba457a047babb5857c01ccdda7
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-22 10:19:26 +0545

    Only show previous navigation if there is previous content to show

--- assets/js/interactive/components/FloatingNavigation.tsx
+++ assets/js/interactive/components/FloatingNavigation.tsx
@@ -9,8 +9,15 @@ import {
 import { BiAlarm, BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import React from 'react';
 import { Stack } from '@chakra-ui/react';
+import { ContentNavigationSchema } from '../schemas';
 
-const FloatingNavigation = () => {
+interface Props {
+	navigation: ContentNavigationSchema;
+}
+
+const FloatingNavigation: React.FC<Props> = (props) => {
+	const { navigation } = props;
+
 	const {
 		isOpen: isPrevOpen,
 		onOpen: onPrevOpen,
@@ -24,51 +31,58 @@ const FloatingNavigation = () => {
 
 	return (
 		<>
-			<Box
-				bg="white"
-				shadow="box"
-				position="fixed"
-				p="6"
-				pl="80px"
-				top="50vh"
-				left="0"
-				transition="all 0.35s ease-in-out"
-				transform={isPrevOpen ? 'translate(0, -50%)' : 'translate(-100%, -50%)'}
-				rounded="xs">
-				<HStack spacing="4">
-					<Stack direction="row" spacing="2" align="center">
-						<Stack direction="column" spacing="0">
-							<Text fontSize="xs" color="gray.400">
-								Introduction
-							</Text>
-							<Text fontWeight="bold">Short Quiz</Text>
-						</Stack>
-						<Center
-							p="2"
-							bg="white"
-							rounded="full"
-							w="42px"
-							h="42px"
-							shadow="0 0 10px rgba(0,0,0,0.1)"
-							fontSize="x-large"
-							color="blue.500">
-							<Icon as={BiAlarm} />
-						</Center>
-					</Stack>
-				</HStack>
-			</Box>
-			<Box
-				position="fixed"
-				top="50%"
-				transform="translateY(-50%)"
-				left="0"
-				cursor="pointer"
-				color="gray.200"
-				onMouseEnter={onPrevOpen}
-				onMouseLeave={onPrevClose}
-				_hover={{ color: 'blue.500' }}>
-				<Icon as={BiChevronLeft} fontSize="5rem" />
-			</Box>
+			{navigation.previous && (
+				<>
+					<Box
+						bg="white"
+						shadow="box"
+						position="fixed"
+						p="6"
+						pl="80px"
+						top="50vh"
+						left="0"
+						transition="all 0.35s ease-in-out"
+						transform={
+							isPrevOpen ? 'translate(0, -50%)' : 'translate(-100%, -50%)'
+						}
+						rounded="xs">
+						<HStack spacing="4">
+							<Stack direction="row" spacing="2" align="center">
+								<Stack direction="column" spacing="0">
+									<Text fontSize="xs" color="gray.400">
+										{navigation?.previous?.parent?.name}
+									</Text>
+									<Text fontWeight="bold">{navigation?.previous.name}</Text>
+								</Stack>
+								<Center
+									p="2"
+									bg="white"
+									rounded="full"
+									w="42px"
+									h="42px"
+									shadow="0 0 10px rgba(0,0,0,0.1)"
+									fontSize="x-large"
+									color="blue.500">
+									<Icon as={BiAlarm} />
+								</Center>
+							</Stack>
+						</HStack>
+					</Box>
+					<Box
+						position="fixed"
+						top="50%"
+						transform="translateY(-50%)"
+						left="0"
+						cursor="pointer"
+						color="gray.200"
+						onMouseEnter={onPrevOpen}
+						onMouseLeave={onPrevClose}
+						_hover={{ color: 'blue.500' }}>
+						<Icon as={BiChevronLeft} fontSize="5rem" />
+					</Box>
+				</>
+			)}
+
 			<Box
 				bg="white"
 				shadow="box"

f443a6e887d6c9a41bf21a9afcb03f122a474db2
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-22 08:38:38 +0545

    Adds complete design for the next navigation

--- assets/js/interactive/components/FloatingNavigation.tsx
+++ assets/js/interactive/components/FloatingNavigation.tsx
@@ -1,18 +1,43 @@
-import { Box, Icon } from '@chakra-ui/react';
-import { BiChevronRight } from 'react-icons/bi';
+import { Box, Icon, Text, Center, HStack } from '@chakra-ui/react';
+import { BiAlarm, BiChevronRight } from 'react-icons/bi';
 import React from 'react';
+import { Stack } from '@chakra-ui/react';
 
 const FloatingNavigation = () => {
-	const floatingBoxStyles = {
-		position: 'fixed',
-		top: '50vh',
-		transform: 'translateY(-50%)',
-		right: 0,
-	};
 	return (
 		<>
-			<Box sx={floatingBoxStyles}>
-				<Icon as={BiChevronRight} fontSize="xxx-large" />
+			<Box
+				bg="white"
+				shadow="box"
+				position="fixed"
+				pl="4"
+				py="2"
+				top="50vh"
+				right="0"
+				rounded="xs"
+				transform="translateY(-50%)">
+				<HStack spacing="4">
+					<Stack direction="row" spacing="2" align="center">
+						<Center
+							p="2"
+							bg="white"
+							rounded="full"
+							w="42px"
+							h="42px"
+							shadow="0 0 10px rgba(0,0,0,0.1)"
+							fontSize="x-large"
+							color="blue.500">
+							<Icon as={BiAlarm} />
+						</Center>
+						<Stack direction="column" spacing="0">
+							<Text fontSize="xs" color="gray.400">
+								Introduction
+							</Text>
+							<Text fontWeight="bold">Short Quiz</Text>
+						</Stack>
+					</Stack>
+					<Icon as={BiChevronRight} fontSize="5rem" color="gray.200" />
+				</HStack>
 			</Box>
 		</>
 	);

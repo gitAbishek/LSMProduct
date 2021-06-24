cf67a11c2b5a89f13201a531c153bc78576ca7b9
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-24 08:29:29 +0545

    Add separate component for the quiz settings

--- assets/js/back-end/screens/quiz/components/QuizSettings.tsx
+++ assets/js/back-end/screens/quiz/components/QuizSettings.tsx
@@ -1,8 +1,41 @@
-import { Box } from '@chakra-ui/react';
+import {
+	Box,
+	FormControl,
+	FormErrorMessage,
+	FormLabel,
+	Input,
+} from '@chakra-ui/react';
+import { __ } from '@wordpress/i18n/build-types';
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
+
+interface Props {
+	quizData;
+}
 
 const QuizSettings = () => {
-	return <Box>Content</Box>;
+	const {
+		register,
+		formState: { errors },
+	} = useFormContext();
+
+	return (
+		<>
+			<FormControl isInvalid={!!errors?.name}>
+				<FormLabel>{__('Quiz Name', 'masteriyo')}</FormLabel>
+				<Input
+					defaultValue={defaultValue}
+					placeholder={__('Your Quiz Name', 'masteriyo')}
+					{...register('name', {
+						required: __('You must provide name for the quiz', 'masteriyo'),
+					})}
+				/>
+				<FormErrorMessage>
+					{errors?.name && errors?.name?.message}
+				</FormErrorMessage>
+			</FormControl>
+		</>
+	);
 };
 
 export default QuizSettings;

e972d459a3cf6ddfa72be699adcd01a1990c2904
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-24 10:12:59 +0545

    Make quiz edit settings work and hide update on questions

--- assets/js/back-end/screens/quiz/EditQuiz.tsx
+++ assets/js/back-end/screens/quiz/EditQuiz.tsx
@@ -24,10 +24,12 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import routes from '../../constants/routes';
 import urls from '../../constants/urls';
+import { QuizSchema } from '../../schemas';
 import API from '../../utils/api';
 import Description from './components/Description';
 import Name from './components/Name';
 import Questions from './components/question/Questions';
+import QuizSettings from './components/QuizSettings';
 
 const EditQuiz: React.FC = () => {
 	const { quizId }: any = useParams();
@@ -47,7 +49,7 @@ const EditQuiz: React.FC = () => {
 		p: '0',
 	};
 	// gets total number of content on section
-	const quizQuery = useQuery(
+	const quizQuery = useQuery<QuizSchema>(
 		[`quiz${quizId}`, quizId],
 		() => quizAPI.get(quizId),
 		{
@@ -55,8 +57,8 @@ const EditQuiz: React.FC = () => {
 			onError: () => {
 				history.push(routes.notFound);
 			},
-			onSuccess: (data: any) => {
-				setCourseId(data.course_id);
+			onSuccess: (data: QuizSchema) => {
+				setCourseId(data?.course_id);
 			},
 		}
 	);
@@ -64,14 +66,16 @@ const EditQuiz: React.FC = () => {
 	const updateQuiz = useMutation(
 		(data: object) => quizAPI.update(quizId, data),
 		{
-			onSuccess: (data: any) => {
+			onSuccess: (data: QuizSchema) => {
 				toast({
 					title: __('Quiz Added', 'masteriyo'),
-					description: data.name + __(' is successfully added.', 'masteriyo'),
+					description: data?.name + __(' is successfully added.', 'masteriyo'),
 					isClosable: true,
 					status: 'success',
 				});
-				history.push(routes.builder.replace(':courseId', data.course_id));
+				history.push(
+					routes.builder.replace(':courseId', data?.course_id?.toString())
+				);
 			},
 		}
 	);
@@ -93,56 +97,66 @@ const EditQuiz: React.FC = () => {
 						<Stack direction="column" spacing="8">
 							<Flex aling="center" justify="space-between">
 								<Heading as="h1" fontSize="x-large">
-									{__('Edit Quiz:', 'masteriyo')} {quizQuery.data.name}
+									{__('Edit Quiz:', 'masteriyo')} {quizQuery?.data?.name}
 								</Heading>
 							</Flex>
 
 							<Tabs index={tabIndex} onChange={(index) => setTabIndex(index)}>
 								<TabList justifyContent="center" borderBottom="1px">
 									<Tab sx={tabStyles}>{__('Info', 'masteriyo')}</Tab>
-									<Tab sx={tabStyles}>{__('Settings', 'masteriyo')}</Tab>
 									<Tab sx={tabStyles}>{__('Questions', 'masteriyo')}</Tab>
+									<Tab sx={tabStyles}>{__('Settings', 'masteriyo')}</Tab>
 								</TabList>
 								<TabPanels>
 									<TabPanel sx={tabPanelStyles}></TabPanel>
-									<TabPanel sx={tabPanelStyles}>settings</TabPanel>
 									<TabPanel sx={tabPanelStyles}>
 										<Questions
-											courseId={quizQuery.data.course_id}
+											courseId={quizQuery?.data?.course_id}
 											quizId={quizId}
 										/>
 									</TabPanel>
+									<TabPanel sx={tabPanelStyles}></TabPanel>
 								</TabPanels>
 							</Tabs>
 							<form onSubmit={methods.handleSubmit(onSubmit)}>
 								<Stack direction="column" spacing="6">
 									{tabIndex === 0 && (
 										<>
-											<Name defaultValue={quizQuery.data.name} />
-											<Description defaultValue={quizQuery.data.description} />
+											<Name defaultValue={quizQuery?.data?.name} />
+											<Description
+												defaultValue={quizQuery?.data?.description}
+											/>
 										</>
 									)}
 
-									<Box py="3">
-										<Divider />
-									</Box>
-									<ButtonGroup>
-										<Button
-											colorScheme="blue"
-											type="submit"
-											isLoading={updateQuiz.isLoading}>
-											{__('Update', 'masteriyo')}
-										</Button>
-										<Button
-											variant="outline"
-											onClick={() =>
-												history.push(
-													routes.builder.replace(':courseId', courseId)
-												)
-											}>
-											{__('Cancel', 'masteriyo')}
-										</Button>
-									</ButtonGroup>
+									{tabIndex === 2 && (
+										<QuizSettings quizData={quizQuery?.data} />
+									)}
+
+									{tabIndex !== 1 && (
+										<>
+											<Box py="3">
+												<Divider />
+											</Box>
+											<ButtonGroup>
+												<Button
+													colorScheme="blue"
+													type="submit"
+													isLoading={updateQuiz.isLoading}>
+													{__('Update', 'masteriyo')}
+												</Button>
+												<Button
+													variant="outline"
+													onClick={() =>
+														history.push(
+															routes.builder.replace(':courseId', courseId)
+														)
+													}>
+													{__('Cancel', 'masteriyo')}
+												</Button>
+											</ButtonGroup>
+										</>
+									)}
 								</Stack>
 							</form>
 						</Stack>

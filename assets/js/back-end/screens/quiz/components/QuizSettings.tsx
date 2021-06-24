a683922a0ffd9b9f22e6c7305db484d6886286be
Sanjeev Shrestha <sethstha@gmail.com>
2021-06-24 08:49:37 +0545

    Add full mark field on quiz settings

--- assets/js/back-end/screens/quiz/components/QuizSettings.tsx
+++ assets/js/back-end/screens/quiz/components/QuizSettings.tsx
@@ -3,10 +3,16 @@ import {
 	FormErrorMessage,
 	FormLabel,
 	Input,
+	NumberDecrementStepper,
+	NumberIncrementStepper,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	Stack,
 } from '@chakra-ui/react';
-import { __ } from '@wordpress/i18n/build-types';
+import { __ } from '@wordpress/i18n';
 import React from 'react';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
 import { QuizSchema } from '../../../schemas';
 
 interface Props {
@@ -21,21 +27,57 @@ const QuizSettings: React.FC<Props> = (props) => {
 	} = useFormContext<QuizSchema>();
 
 	return (
-		<>
+		<Stack direction="column" spacing="6">
+			<FormControl isInvalid={!!errors?.full_mark}>
+				<FormLabel>{__('Full Mark', 'masteriyo')}</FormLabel>
+				<Controller
+					name="full_mark"
+					defaultValue={quizData?.full_mark || 100}
+					rules={{
+						required: __('Full Mark is required for the quiz', 'masteriyo'),
+					}}
+					render={({ field }) => (
+						<NumberInput {...field} w="full">
+							<NumberInputField borderRadius="sm" shadow="input" />
+							<NumberInputStepper>
+								<NumberIncrementStepper />
+								<NumberDecrementStepper />
+							</NumberInputStepper>
+						</NumberInput>
+					)}
+				/>
+				<FormErrorMessage>
+					{errors?.full_mark && errors?.full_mark?.message}
+				</FormErrorMessage>
+			</FormControl>
+
 			<FormControl isInvalid={!!errors?.pass_mark}>
-				<FormLabel>{__('Quiz Name', 'masteriyo')}</FormLabel>
+				<FormLabel>{__('Pass Mark', 'masteriyo')}</FormLabel>
 				<Input
 					defaultValue={quizData?.pass_mark}
-					placeholder={__('Your Quiz Name', 'masteriyo')}
+					placeholder={__('Quiz Pass Mark', 'masteriyo')}
 					{...register('pass_mark', {
 						required: __('Please Provide Pass Mark for the quiz', 'masteriyo'),
 					})}
+				/>
+				<Controller
+					name="full_mark"
+					defaultValue={quizData?.pass_mark || 100}
+					render={({ field }) => (
+						<NumberInput {...field} w="full">
+							<NumberInputField borderRadius="sm" shadow="input" />
+							<NumberInputStepper>
+								<NumberIncrementStepper />
+								<NumberDecrementStepper />
+							</NumberInputStepper>
+						</NumberInput>
+					)}
 				/>
 				<FormErrorMessage>
 					{errors?.pass_mark && errors?.pass_mark?.message}
 				</FormErrorMessage>
 			</FormControl>
-		</>
+		</Stack>
 	);
 };
 

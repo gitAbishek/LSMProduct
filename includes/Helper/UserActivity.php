d2197431f318bfb744ef4afadebecedc105fa3c6
Sagar Tamang <mi5t4n@gmail.com>
2021-06-25 08:59:24 +0545

    Moved the masteriyo_get_course_progress().

--- includes/Helper/UserActivity.php
+++ includes/Helper/UserActivity.php
@@ -25,26 +25,3 @@ function masteriyo_get_user_activity( $user_activity_id ) {
 
 	return $user_activity;
 }
-
-/**
- * Get course progress.
- *
- * @since 0.1.0
- *
- * @param int $course_progress_id Course progress ID.
- *
- * @return UserActivity
- */
-function masteriyo_get_course_progress( $course_progress_id ) {
-	try {
-		$course_progress = masteriyo( 'course-progress' );
-		$course_progress->set_id( $course_progress_id );
-
-		$course_progress_repo = masteriyo( 'course-progress.store' );
-		$course_progress_repo->read( $course_progress );
-	} catch ( \Exception $e ) {
-		return null;
-	}
-
-	return $course_progress;
-}

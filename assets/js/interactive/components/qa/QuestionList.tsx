46f5b0ee6c3164f6da54f136ed6f6dfa183575a1
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-06 08:49:27 +0545

    Add header style for the interactive qa and other style

--- assets/js/interactive/components/qa/QuestionList.tsx
+++ assets/js/interactive/components/qa/QuestionList.tsx
@@ -1,6 +1,7 @@
 import {
 	Box,
 	Button,
+	ButtonGroup,
 	FormControl,
 	Heading,
 	Icon,
@@ -14,7 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
-import { BiChevronRight, BiSearch } from 'react-icons/bi';
+import { BiChevronLeft, BiChevronRight, BiSearch } from 'react-icons/bi';
 import Message from './Message';
 
 const QuestionList = () => {
@@ -43,10 +44,7 @@ const QuestionList = () => {
 	];
 	return (
 		<>
-			<Slide
-				direction="left"
-				in={isListOpen}
-				style={{ position: 'absolute', top: 0 }}>
+			<Slide direction="left" in={isListOpen} style={{ position: 'absolute' }}>
 				<Stack direction="column" spacing="1">
 					<Box as="form" action="" p="4">
 						<FormControl>
@@ -92,25 +90,53 @@ const QuestionList = () => {
 					</Stack>
 				</Stack>
 			</Slide>
-			<Slide
-				direction="right"
-				in={isChatOpen}
-				style={{ position: 'absolute', top: 20 }}>
-				<Stack direction="column" spacing="2" px="4">
-					<Message
-						name="Arlene McCoy"
-						avatar="https://i.pravatar.cc/150?img=3"
-						message="What is Screencast videos?"
-						sender="user"
-						time="12 min ago"
-					/>
-					<Message
-						name="Arlene McCoy"
-						avatar="https://i.pravatar.cc/150?img=3"
-						message="What is Screencast videos?"
-						sender="author"
-						time="12 min ago"
-					/>
+			<Slide direction="right" in={isChatOpen} style={{ position: 'absolute' }}>
+				<Stack direction="column" spacing="8">
+					<Box as="header">
+						<ButtonGroup px="4" py="2">
+							<Button
+								leftIcon={<Icon fontSize="xl" as={BiChevronLeft} />}
+								variant="link"
+								onClick={() => {
+									onChatToggle();
+									onListToggle();
+								}}>
+								{__('Back', 'masteriyo')}
+							</Button>
+						</ButtonGroup>
+						<Stack direction="column" p="4" bg="gray.50" spacing="1">
+							<Text fontWeight="bold">What is an instructional video?</Text>
+							<Text fontSize="x-small" color="gray.400">
+								3 answers
+							</Text>
+						</Stack>
+					</Box>
+
+					<Stack direction="column" spacing="2" px="4">
+						<Message
+							name="Arlene McCoy"
+							avatar="https://i.pravatar.cc/150?img=3"
+							message="What is Screencast videos?"
+							sender="user"
+							time="12 min ago"
+						/>
+						<Message
+							name="Arlene McCoy"
+							avatar="https://i.pravatar.cc/150?img=3"
+							message="What is Screencast videos?"
+							sender="author"
+							time="12 min ago"
+						/>
+					</Stack>
+
+					<Stack direction="column" spacing="3" w="full" p="4" pb="6">
+						<FormControl>
+							<Input type="text" placeholder="What is your question?" />
+						</FormControl>
+						<Button colorScheme="blue" type="submit" isFullWidth>
+							Send
+						</Button>
+					</Stack>
 				</Stack>
 			</Slide>
 		</>

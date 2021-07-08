7036ad818294f245ad4ed6de24cff5b2200f9f41
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-08 08:03:05 +0545

    Separate chat container on another component

--- assets/js/interactive/components/qa/QuestionList.tsx
+++ assets/js/interactive/components/qa/QuestionList.tsx
@@ -1,7 +1,6 @@
 import {
 	Box,
 	Button,
-	ButtonGroup,
 	Center,
 	FormControl,
 	Heading,
@@ -13,18 +12,16 @@ import {
 	Spinner,
 	Stack,
 	Text,
-	Textarea,
 	useDisclosure,
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
-import { BiChevronLeft, BiChevronRight, BiSearch } from 'react-icons/bi';
+import { BiChevronRight, BiSearch } from 'react-icons/bi';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import urls from '../../../back-end/constants/urls';
 import API from '../../../back-end/utils/api';
 import { QuestionAnswerSchema } from '../../schemas';
-import Message from './Message';
 
 const QuestionList: React.FC = () => {
 	const { courseId }: any = useParams();
@@ -34,7 +31,6 @@ const QuestionList: React.FC = () => {
 	const { isOpen: isListOpen, onToggle: onListToggle } = useDisclosure({
 		defaultIsOpen: true,
 	});
-
 	const { isOpen: isChatOpen, onToggle: onChatToggle } = useDisclosure();
 
 	const qaQuery = useQuery([`qa${courseId}`, courseId], () => qaAPI.list());
@@ -97,63 +93,6 @@ const QuestionList: React.FC = () => {
 							</FormControl>
 							<Button colorScheme="blue" type="submit" isFullWidth>
 								Ask a Question
-							</Button>
-						</Stack>
-					</Stack>
-				</Slide>
-				<Slide
-					direction="right"
-					in={isChatOpen}
-					style={{ position: 'absolute' }}>
-					<Stack
-						direction="column"
-						spacing="8"
-						justify="space-between"
-						h="full">
-						<Stack direction="column" spacing="8">
-							<Box as="header">
-								<ButtonGroup px="4" py="2">
-									<Button
-										leftIcon={<Icon fontSize="xl" as={BiChevronLeft} />}
-										variant="link"
-										onClick={() => {
-											onChatToggle();
-											onListToggle();
-										}}>
-										{__('Back', 'masteriyo')}
-									</Button>
-								</ButtonGroup>
-								<Stack direction="column" p="4" bg="gray.50" spacing="1">
-									<Text fontWeight="bold">What is an instructional video?</Text>
-									<Text fontSize="x-small" color="gray.400">
-										3 answers
-									</Text>
-								</Stack>
-							</Box>
-
-							<Stack direction="column" spacing="4" px="4">
-								<Message
-									name="Arlene McCoy"
-									avatar="https://i.pravatar.cc/150?img=3"
-									message="What is Screencast videos?"
-									sender="user"
-									time="12 min ago"
-								/>
-								<Message
-									name="Arlene McCoy"
-									avatar="https://i.pravatar.cc/150?img=3"
-									message="What is Screencast videos?"
-									sender="author"
-									time="12 min ago"
-								/>
-							</Stack>
-						</Stack>
-						<Stack direction="column" spacing="3" w="full" p="4" pb="6">
-							<FormControl>
-								<Textarea />
-							</FormControl>
-							<Button colorScheme="blue" type="submit" isFullWidth>
-								Send
 							</Button>
 						</Stack>
 					</Stack>

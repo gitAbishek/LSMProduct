bdd380a4e7553f1e746ca8b618cd6ce0f6742e0a
Aashil <aashil.themegrill@gmail.com>
2021-07-08 13:02:55 +0545

    Added currency symbol on currency option and align tabs to vertical

--- assets/js/back-end/screens/settings/components/GeneralSettings.tsx
+++ assets/js/back-end/screens/settings/components/GeneralSettings.tsx
@@ -1,14 +1,17 @@
 import {
-	Box,
-	Flex,
 	FormControl,
 	FormLabel,
-	Heading,
 	Input,
 	Select,
 	Stack,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
+import getSymbolFromCurrency from 'currency-symbol-map';
 import React, { useEffect, useState } from 'react';
 import ReactFlagsSelect from 'react-flags-select';
 import { useFormContext } from 'react-hook-form';
@@ -23,144 +26,151 @@ const GeneralSettings: React.FC<Props> = (props) => {
 	const [country, setCountry] = useState(generalData?.country);
 	const { register, setValue } = useFormContext();
 
+	const tabStyles = {
+		justifyContent: 'flex-start',
+		w: '180px',
+		borderLeft: 0,
+		borderRight: '2px solid',
+		borderRightColor: 'transparent',
+		marginLeft: 0,
+		marginRight: '-2px',
+		pl: 0,
+		fontSize: 'sm',
+		textAlign: 'left',
+	};
+
+	const tabListStyles = {
+		borderLeft: 0,
+		borderRight: '2px solid',
+		borderRightColor: 'gray.200',
+	};
+
 	useEffect(() => {
 		setValue('general.country', country);
 	}, [country, setValue]);
 
 	return (
-		<Stack direction="column" spacing="8">
-			<Box>
-				<Stack direction="column" spacing="6">
-					<Flex
-						align="center"
-						justify="space-between"
-						borderBottom="1px"
-						borderColor="gray.100"
-						pb="3">
-						<Heading fontSize="lg" fontWeight="semibold">
-							Store
-						</Heading>
-					</Flex>
-					<Stack direction="row" spacing="8">
-						<FormControl>
-							<FormLabel minW="xs">{__('Country', 'masteriyo')}</FormLabel>
-							<input
-								type="hidden"
-								{...register('general.country')}
-								defaultValue={generalData?.country}
-							/>
-							<ReactFlagsSelect
-								selected={country || ''}
-								onSelect={(code) => setCountry(code)}
-							/>
-						</FormControl>
-						<FormControl>
-							<FormLabel minW="xs">{__('City', 'masteriyo')}</FormLabel>
-							<Input
-								type="text"
-								{...register('general.city')}
-								defaultValue={generalData?.city}
-							/>
-						</FormControl>
-					</Stack>
-					<Stack direction="row" spacing="8">
-						<FormControl>
-							<FormLabel minW="xs">
-								{__('Adress Line 1', 'masteriyo')}
-							</FormLabel>
-							<Input
-								type="text"
-								{...register('general.address_line1')}
-								defaultValue={generalData?.address_line1}
-							/>
-						</FormControl>
-						<FormControl>
-							<FormLabel minW="xs">
-								{__('Adress Line 2', 'masteriyo')}
-							</FormLabel>
-							<Input
-								type="text"
-								{...register('general.address_line2')}
-								defaultValue={generalData?.address_line2}
-							/>
-						</FormControl>
-					</Stack>
-				</Stack>
-			</Box>
-			<Box>
-				<Stack direction="column" spacing="6">
-					<Flex
-						align="center"
-						justify="space-between"
-						borderBottom="1px"
-						borderColor="gray.100"
-						pb="3">
-						<Heading fontSize="lg" fontWeight="semibold">
-							{__('Currency Options', 'masteriyo')}
-						</Heading>
-					</Flex>
-					<Stack direction="row" spacing="8">
-						<FormControl>
-							<FormLabel minW="xs">{__('Currency', 'masteriyo')}</FormLabel>
-							<Select
-								{...register('general.currency')}
-								defaultValue={generalData?.currency}>
-								{Object.entries(currency).map(([code, name]) => (
-									<option value={code} key={code}>
-										{name}
-									</option>
-								))}
-							</Select>
-						</FormControl>
-						<FormControl>
-							<FormLabel minW="xs">
-								{__('Currency Position', 'masteriyo')}
-							</FormLabel>
-							<Select
-								{...register('general.currency_position')}
-								defaultValue={generalData?.currency_position}>
-								<option value="left">{__('Left', 'masteriyo')}</option>
-								<option value="right">{__('Right', 'masteriyo')}</option>
-							</Select>
-						</FormControl>
-					</Stack>
-					<Stack direction="row" spacing="8">
-						<FormControl>
-							<FormLabel minW="xs">
-								{__('Thousand Separator', 'masteriyo')}
-							</FormLabel>
-							<Input
-								type="text"
-								{...register('general.thousand_separator')}
-								defaultValue={generalData?.thousand_separator}
-							/>
-						</FormControl>
-						<FormControl>
-							<FormLabel minW="xs">
-								{__('Decimal Separator', 'masteriyo')}
-							</FormLabel>
-							<Input
-								type="text"
-								{...register('general.decimal_separator')}
-								defaultValue={generalData?.decimal_separator}
-							/>
-						</FormControl>
-					</Stack>
-					<Stack direction="row" spacing="8">
-						<FormControl>
-							<FormLabel minW="xs">
-								{__('Number of Decimals', 'masteriyo')}
-							</FormLabel>
-							<Input
-								type="text"
-								{...register('general.number_of_decimals')}
-								defaultValue={generalData?.number_of_decimals}
-							/>
-						</FormControl>
-					</Stack>
-				</Stack>
-			</Box>
-		</Stack>
+		<Tabs orientation="vertical">
+			<Stack direction="row" flex="1">
+				<TabList sx={tabListStyles}>
+					<Tab sx={tabStyles}>{__('Store', 'masteriyo')}</Tab>
+					<Tab sx={tabStyles}>{__('Currency Options', 'masteriyo')}</Tab>
+				</TabList>
+				<TabPanels flex="1">
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<Stack direction="row" spacing="8">
+								<FormControl>
+									<FormLabel minW="xs">{__('Country', 'masteriyo')}</FormLabel>
+									<input
+										type="hidden"
+										{...register('general.country')}
+										defaultValue={generalData?.country}
+									/>
+									<ReactFlagsSelect
+										selected={country || ''}
+										onSelect={(code) => setCountry(code)}
+									/>
+								</FormControl>
+								<FormControl>
+									<FormLabel minW="xs">{__('City', 'masteriyo')}</FormLabel>
+									<Input
+										type="text"
+										{...register('general.city')}
+										defaultValue={generalData?.city}
+									/>
+								</FormControl>
+							</Stack>
+							<Stack direction="row" spacing="8">
+								<FormControl>
+									<FormLabel minW="xs">
+										{__('Adress Line 1', 'masteriyo')}
+									</FormLabel>
+									<Input
+										type="text"
+										{...register('general.address_line1')}
+										defaultValue={generalData?.address_line1}
+									/>
+								</FormControl>
+								<FormControl>
+									<FormLabel minW="xs">
+										{__('Adress Line 2', 'masteriyo')}
+									</FormLabel>
+									<Input
+										type="text"
+										{...register('general.address_line2')}
+										defaultValue={generalData?.address_line2}
+									/>
+								</FormControl>
+							</Stack>
+						</Stack>
+					</TabPanel>
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<Stack direction="row" spacing="8">
+								<FormControl>
+									<FormLabel minW="xs">{__('Currency', 'masteriyo')}</FormLabel>
+									<Select
+										{...register('general.currency')}
+										defaultValue={generalData?.currency}>
+										{Object.entries(currency).map(([code, name]) => (
+											<option value={code} key={code}>
+												{name} ({getSymbolFromCurrency(code)})
+											</option>
+										))}
+									</Select>
+								</FormControl>
+								<FormControl>
+									<FormLabel minW="xs">
+										{__('Currency Position', 'masteriyo')}
+									</FormLabel>
+									<Select
+										{...register('general.currency_position')}
+										defaultValue={generalData?.currency_position}>
+										<option value="left">{__('Left', 'masteriyo')}</option>
+										<option value="right">{__('Right', 'masteriyo')}</option>
+									</Select>
+								</FormControl>
+							</Stack>
+							<Stack direction="row" spacing="8">
+								<FormControl>
+									<FormLabel minW="xs">
+										{__('Thousand Separator', 'masteriyo')}
+									</FormLabel>
+									<Input
+										type="text"
+										{...register('general.thousand_separator')}
+										defaultValue={generalData?.thousand_separator}
+									/>
+								</FormControl>
+								<FormControl>
+									<FormLabel minW="xs">
+										{__('Decimal Separator', 'masteriyo')}
+									</FormLabel>
+									<Input
+										type="text"
+										{...register('general.decimal_separator')}
+										defaultValue={generalData?.decimal_separator}
+									/>
+								</FormControl>
+							</Stack>
+							<Stack direction="row" spacing="8">
+								<FormControl>
+									<FormLabel minW="xs">
+										{__('Number of Decimals', 'masteriyo')}
+									</FormLabel>
+									<Input
+										type="text"
+										{...register('general.number_of_decimals')}
+										defaultValue={generalData?.number_of_decimals}
+									/>
+								</FormControl>
+							</Stack>
+						</Stack>
+					</TabPanel>
+				</TabPanels>
+			</Stack>
+		</Tabs>
 	);
 };
 

c49751ddf0632dcebd94f20c0a6af41e563c3c64
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-10 11:24:30 +0545

    Add dummy code editor

--- assets/js/back-end/components/common/Editor.tsx
+++ assets/js/back-end/components/common/Editor.tsx
@@ -1,26 +1,124 @@
-import { Editor as TinyMceEditor } from '@tinymce/tinymce-react';
+import { Button } from '@chakra-ui/react';
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 import React from 'react';
-import { Controller } from 'react-hook-form';
 
 interface Props {
 	name: `${string}`;
 	control: any;
 	defaultValue?: string;
 }
-const Editor: React.FC<Props> = (props) => {
-	const { name, control, defaultValue } = props;
+const MenuBar = ({ editor }: any) => {
+	if (!editor) {
+		return null;
+	}
+
 	return (
-		<Controller
-			name={name}
-			control={control}
-			render={({ field: { onChange } }) => (
-				<TinyMceEditor
-					initialValue={defaultValue}
-					tinymceScriptSrc="https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.7.1/tinymce.min.js"
-					onEditorChange={onChange}
-				/>
-			)}
-		/>
+		<>
+			<Button
+				onClick={() => editor.chain().focus().toggleBold().run()}
+				className={editor.isActive('bold') ? 'is-active' : ''}>
+				bold
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleItalic().run()}
+				className={editor.isActive('italic') ? 'is-active' : ''}>
+				italic
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleStrike().run()}
+				className={editor.isActive('strike') ? 'is-active' : ''}>
+				strike
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleCode().run()}
+				className={editor.isActive('code') ? 'is-active' : ''}>
+				code
+			</Button>
+			<Button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+				clear marks
+			</Button>
+			<Button onClick={() => editor.chain().focus().clearNodes().run()}>
+				clear nodes
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().setParagraph().run()}
+				className={editor.isActive('paragraph') ? 'is-active' : ''}>
+				paragraph
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+				className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}>
+				h1
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+				className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}>
+				h2
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+				className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}>
+				h3
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+				className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}>
+				h4
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+				className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}>
+				h5
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+				className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}>
+				h6
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleBulletList().run()}
+				className={editor.isActive('bulletList') ? 'is-active' : ''}>
+				bullet list
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleOrderedList().run()}
+				className={editor.isActive('orderedList') ? 'is-active' : ''}>
+				ordered list
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+				className={editor.isActive('codeBlock') ? 'is-active' : ''}>
+				code block
+			</Button>
+			<Button
+				onClick={() => editor.chain().focus().toggleBlockquote().run()}
+				className={editor.isActive('blockquote') ? 'is-active' : ''}>
+				blockquote
+			</Button>
+			<Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+				horizontal rule
+			</Button>
+			<Button onClick={() => editor.chain().focus().setHardBreak().run()}>
+				hard break
+			</Button>
+			<Button onClick={() => editor.chain().focus().undo().run()}>undo</Button>
+			<Button onClick={() => editor.chain().focus().redo().run()}>redo</Button>
+		</>
+	);
+};
+
+const Editor: React.FC<Props> = (props) => {
+	const editor = useEditor({
+		extensions: [StarterKit],
+		content: '<p> Your content </p>',
+	});
+
+	return (
+		<>
+			<MenuBar editor={editor} />
+			<EditorContent editor={editor} />
+		</>
 	);
 };
 

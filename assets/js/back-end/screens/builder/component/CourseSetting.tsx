b51bbb08772052a31772f7cc9fac13c06b2987d8
Aashil <aashil.themegrill@gmail.com>
2021-07-12 12:59:00 +0545

    Integrate api and added course duartion

--- assets/js/back-end/screens/builder/component/CourseSetting.tsx
+++ assets/js/back-end/screens/builder/component/CourseSetting.tsx
@@ -5,6 +5,8 @@ import {
 	FormErrorMessage,
 	FormHelperText,
 	FormLabel,
+	InputGroup,
+	InputRightAddon,
 	NumberDecrementStepper,
 	NumberIncrementStepper,
 	NumberInput,
@@ -13,6 +15,7 @@ import {
 	Radio,
 	RadioGroup,
 	Select,
+	Spinner,
 	Stack,
 	Tab,
 	TabList,
@@ -22,8 +25,11 @@ import {
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import React, { useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+import { Controller, useFormContext } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import urls from '../../../constants/urls';
 import { CourseDataMap } from '../../../types/course';
+import API from '../../../utils/api';
 
 interface Props {
 	courseData?: CourseDataMap | any;
@@ -31,16 +37,42 @@ interface Props {
 const CourseSetting: React.FC<Props> = (props) => {
 	const { courseData } = props;
 
-	const [displayValue, setDisplayValue] = useState('1');
-
+	const [displayValue, setDisplayValue] = useState(
+		courseData?.enrollment_limit != 0 ? '1' : '0'
+	);
+	const difficultiesAPI = new API(urls.difficulties);
 	const {
 		register,
 		formState: { errors },
+		setValue,
 	} = useFormContext();
+
+	const diffultiesQuery = useQuery('difficulties', () =>
+		difficultiesAPI.list()
+	);
+
+	if (diffultiesQuery.isLoading) {
+		return <Spinner />;
+	}
+
+	const renderDifficultiesOption = () => {
+		try {
+			return diffultiesQuery?.data?.map(
+				(page: { id: number; name: string }) => (
+					<option value={page.id} key={page.id}>
+						{page.name}
+					</option>
+				)
+			);
+		} catch (error) {
+			console.error(error);
+			return;
+		}
+	};
 
 	const tabStyles = {
 		justifyContent: 'flex-start',
-		w: '180px',
+		w: '150px',
 		borderLeft: 0,
 		borderRight: '2px solid',
 		borderRightColor: 'transparent',
@@ -72,60 +104,90 @@ const CourseSetting: React.FC<Props> = (props) => {
 									<FormControl>
 										<FormLabel>{__('Difficulty', 'masteriyo')}</FormLabel>
 										<Select
-											defaultValue={courseData?.difficulty}
+											defaultValue={courseData?.difficulty?.id}
 											placeholder={__('Choose Course Level', 'masteriyo')}
-											{...register('difficulty')}>
-											<option value="beginner">
-												{__('Beginner', 'masteriyo')}
-											</option>
-											<option value="intermediate">
-												{__('Intermediate', 'masteriyo')}
-											</option>
-											<option value="advanced">
-												{__('Advanced', 'masteriyo')}
-											</option>
+											{...register('difficulty.id')}>
+											{renderDifficultiesOption()}
 										</Select>
+									</FormControl>
+
+									<FormControl isInvalid={errors?.duration} maxW="xs">
+										<FormLabel>{__('Course Duration', 'masteriyo')}</FormLabel>
+										<Controller
+											name="duration"
+											defaultValue={courseData?.duration}
+											rules={{
+												required: __(
+													'Course duration is required.',
+													'masteriyo'
+												),
+											}}
+											render={({ field }) => (
+												<InputGroup>
+													<NumberInput {...field}>
+														<NumberInputField />
+														<NumberInputStepper>
+															<NumberIncrementStepper />
+															<NumberDecrementStepper />
+														</NumberInputStepper>
+													</NumberInput>
+													<InputRightAddon>
+														{__('Minutes', 'masteriyo')}
+													</InputRightAddon>
+												</InputGroup>
+											)}
+										/>
+
+										<FormHelperText>{__('', 'masteriyo')}</FormHelperText>
 
 										<FormErrorMessage>
-											{errors?.difficulty && errors?.difficulty?.message}
+											{errors?.duration && errors?.duration?.message}
 										</FormErrorMessage>
 									</FormControl>
 
 									<FormControl>
 										<FormLabel>{__('Maximum Students', 'masteriyo')}</FormLabel>
-										<RadioGroup
-											onChange={setDisplayValue}
-											value={displayValue}
-											defaultValue="0">
-											<Stack direction="column" spacing="6">
-												<Stack direction="row" spacing="8" align="flex-start">
-													<Stack direction="column">
-														<Radio value="0">
-															{__('No limit', 'masteriyo')}
-														</Radio>
-														<FormHelperText>
-															{__(
-																'Check this option if there is no limit.',
-																'masteriyo'
-															)}
-														</FormHelperText>
-													</Stack>
-													<Radio value="1">{__('Limit', 'masteriyo')}</Radio>
+										<RadioGroup onChange={setDisplayValue} value={displayValue}>
+											<Stack direction="row" spacing="8" align="flex-start">
+												<Stack direction="column">
+													<Radio
+														onChange={(e: any) =>
+															setValue('enrollment_limit', e.target.value)
+														}
+														value="0">
+														{__('No limit', 'masteriyo')}
+													</Radio>
+													<FormHelperText>
+														{__(
+															'Check this option if there is no limit.',
+															'masteriyo'
+														)}
+													</FormHelperText>
 												</Stack>
-												<Collapse in={displayValue != '0'} animateOpacity>
-													<FormControl>
-														<FormLabel>
-															{__('Number of Students', 'masteriyo')}
-														</FormLabel>
-														<NumberInput defaultValue={50}>
-															<NumberInputField />
-															<NumberInputStepper>
-																<NumberIncrementStepper />
-																<NumberDecrementStepper />
-															</NumberInputStepper>
-														</NumberInput>
-													</FormControl>
-												</Collapse>
+												<Stack direction="column" spacing="6">
+													<Radio value="1">{__('Limit', 'masteriyo')}</Radio>
+													<Collapse in={displayValue != '0'} animateOpacity>
+														<FormControl>
+															<FormLabel>
+																{__('Number of Students', 'masteriyo')}
+															</FormLabel>
+															<Controller
+																name="enrollment_limit"
+																render={({ field }) => (
+																	<NumberInput
+																		{...field}
+																		defaultValue={courseData?.enrollment_limit}>
+																		<NumberInputField />
+																		<NumberInputStepper>
+																			<NumberIncrementStepper />
+																			<NumberDecrementStepper />
+																		</NumberInputStepper>
+																	</NumberInput>
+																)}
+															/>
+														</FormControl>
+													</Collapse>
+												</Stack>
 											</Stack>
 										</RadioGroup>
 									</FormControl>
@@ -138,12 +200,16 @@ const CourseSetting: React.FC<Props> = (props) => {
 										<FormLabel>
 											{__('Curriculum in Single page', 'masteriyo')}
 										</FormLabel>
-										<RadioGroup defaultValue="1">
+										<RadioGroup
+											defaultValue={
+												courseData?.show_curriculum ? 'true' : 'false'
+											}
+											onChange={(value) => setValue('show_curriculum', value)}>
 											<Stack direction="row" spacing="8">
-												<Radio value="1">
+												<Radio value="true">
 													{__('Always Visible', 'masteriyo')}
 												</Radio>
-												<Radio value="2">
+												<Radio value="false">
 													{__('Only Visible to Enrollers', 'masteriyo')}
 												</Radio>
 											</Stack>

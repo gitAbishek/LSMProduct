35d771ddd079d7d321d27c1b0c4a90e4b9ae2e62
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-13 18:58:00 +0545

    Add component to render short answer

--- assets/js/interactive/pages/quiz/components/FieldShortAnswer.tsx
+++ assets/js/interactive/pages/quiz/components/FieldShortAnswer.tsx
@@ -1,58 +1,28 @@
-import {
-	Flex,
-	FormControl,
-	FormErrorMessage,
-	Radio,
-	RadioGroup,
-	Stack,
-	Text,
-} from '@chakra-ui/react';
+import { FormControl, FormErrorMessage, Textarea } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
-import { SingleChoiceSchema } from '../../../../back-end/schemas';
+import { useFormContext } from 'react-hook-form';
 
 interface Props {
-	answers: [];
 	index: any;
 }
 
 const FieldShortAnswer: React.FC<Props> = (props) => {
-	const { answers, index } = props;
+	const { index } = props;
 	const {
+		register,
 		formState: { errors },
 	} = useFormContext();
 
 	return (
 		<>
 			<FormControl isInvalid={errors[index]}>
-				<Controller
-					name={index}
-					rules={{ required: __('Answer is required', 'masteriyo') }}
-					render={({ field }) => (
-						<RadioGroup {...field}>
-							<Stack direction="row" spacing="4">
-								{answers.map((answer: SingleChoiceSchema, index: number) => (
-									<Flex
-										key={index}
-										justify="space-between"
-										align="center"
-										border="1px"
-										borderColor="gray.100"
-										bg="white"
-										rounded="sm"
-										py="3"
-										px="4"
-										minW="200px"
-										shadow="input">
-										<Text fontSize="sm">{answer.name}</Text>
-										<Radio value={index.toString()}></Radio>
-									</Flex>
-								))}
-							</Stack>
-						</RadioGroup>
-					)}
+				<Textarea
+					{...register(index, {
+						required: __('Answer is required', 'masteriyo'),
+					})}
 				/>
+
 				{errors[index] && (
 					<FormErrorMessage fontSize="xs">
 						{errors[index].message}

cac737ee4a5aa2fdfed5a096ba8e6b5674929e7d
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-13 10:05:57 +0545

    Add functionality to save name of the answer

--- assets/js/back-end/screens/quiz/components/answer/types/TrueFalse.tsx
+++ assets/js/back-end/screens/quiz/components/answer/types/TrueFalse.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import { sectionHeaderStyles } from 'Config/styles';
+import { merge } from 'lodash';
 import { nanoid } from 'nanoid';
 import React, { useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
@@ -57,7 +58,7 @@ const TrueFalse: React.FC<Props> = (props) => {
 	};
 
 	const onCheckPress = (id: any, correct: boolean) => {
-		var newAnswers = answers;
+		var newAnswers = { ...answers };
 
 		// uncheck other checkbox
 		for (var key in newAnswers) {
@@ -66,14 +67,25 @@ const TrueFalse: React.FC<Props> = (props) => {
 			}
 		}
 
-		setAnswers({
-			...newAnswers,
+		const mergedData = merge(newAnswers, {
 			[id]: {
-				name: 'New Answer',
-				right: false,
 				correct: correct,
 			},
 		});
+
+		setAnswers(mergedData);
+	};
+
+	const onNameChange = (id: any, name: string) => {
+		var newAnswers = { ...answers };
+
+		setAnswers(
+			merge(newAnswers, {
+				[id]: {
+					name: name,
+				},
+			})
+		);
 	};
 
 	const onDuplicatePress = (name: string) => {
@@ -115,7 +127,9 @@ const TrueFalse: React.FC<Props> = (props) => {
 								<Icon as={Sortable} fontSize="lg" color="gray.500" />
 								<Editable defaultValue={answer?.name}>
 									<EditablePreview />
-									<EditableInput />
+									<EditableInput
+										onChange={(e) => onNameChange(id, e.target.value)}
+									/>
 								</Editable>
 							</Stack>
 							<Stack direction="row" spacing="4">

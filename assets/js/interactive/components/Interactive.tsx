a3169bc12c595a2c12c70ca0866d268790f9c8d0
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-14 17:31:06 +0545

    Try to go to first lesson or quiz of course if url only has course

--- assets/js/interactive/components/Interactive.tsx
+++ assets/js/interactive/components/Interactive.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 import FullScreenLoader from '../../back-end/components/layout/FullScreenLoader';
 import urls from '../../back-end/constants/urls';
 import API from '../../back-end/utils/api';
 import InteractiveRouter from '../router/InteractiveRouter';
 import { CourseProgressMap } from '../schemas';
+import { getNavigationRoute } from './FloatingNavigation';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 const Interactive: React.FC = () => {
 	const { courseId }: any = useParams();
+	const history = useHistory();
+	const location = useLocation();
 	const progressAPI = new API(urls.courseProgress);
 
 	const courseProgressQuery = useQuery<CourseProgressMap>(
@@ -18,6 +21,19 @@ const Interactive: React.FC = () => {
 		() => progressAPI.store({ course_id: courseId }),
 		{
 			enabled: !!courseId,
+			onSuccess: (data: any) => {
+				try {
+					if (location.pathname === `/course/${courseId}`) {
+						history.push(
+							getNavigationRoute(
+								data.items[0].contents[0].item_id,
+								data.items[0].contents[0].item_type,
+								courseId
+							)
+						);
+					}
+				} catch (error) {}
+			},
 		}
 	);
 

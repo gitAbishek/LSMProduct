8960e6c63a36771ff502ca67bd18ac14c9adaf14
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-14 11:24:02 +0545

    Add video source and video source url

--- assets/js/back-end/screens/lessons/components/VideoSource.tsx
+++ assets/js/back-end/screens/lessons/components/VideoSource.tsx
@@ -1,38 +1,36 @@
-import { FormControl, FormLabel } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, Select, Stack } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
-import Select from 'Components/common/Select';
 import React from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 
 interface Props {
-	defaultValue?: any;
+	defaultSource?: any;
+	defaultSourceUrl?: any;
 }
 const VideoSource: React.FC<Props> = (props) => {
-	const { defaultValue } = props;
-	const { control } = useFormContext();
-
-	const options = [
-		{ value: 'youtube', label: 'YouTube' },
-		{ value: 'vimeo', label: 'Vimeo' },
-	];
+	const { defaultSource, defaultSourceUrl } = props;
+	const { register } = useFormContext();
 
 	return (
-		<FormControl>
-			<FormLabel>{__('Video Source', 'masteriyo')}</FormLabel>
-			<Controller
-				render={({ field }) => (
-					<Select
-						{...field}
-						closeMenuOnSelect={false}
-						defaultValue={defaultValue}
-						isMulti
-						options={options}
-					/>
-				)}
-				control={control}
-				name="categories"
-			/>
-		</FormControl>
+		<Stack direction="row" spacing="6">
+			<FormControl>
+				<FormLabel>{__('Video Source', 'masteriyo')}</FormLabel>
+				<Select {...register('video_source')} defaultValue={defaultSource}>
+					<option value="self-hosted">{__('Self Hosted', 'materiyo')}</option>
+					<option value="youtube">{__('YouTube', 'materiyo')}</option>
+					<option value="vimeo">{__('Vimeo', 'materiyo')}</option>
+				</Select>
+			</FormControl>
+
+			<FormControl>
+				<FormLabel>{__('Video Source URL', 'masteriyo')}</FormLabel>
+				<Input
+					type="text"
+					defaultValue={defaultSourceUrl}
+					{...register('video_source_url')}
+				/>
+			</FormControl>
+		</Stack>
 	);
 };
 

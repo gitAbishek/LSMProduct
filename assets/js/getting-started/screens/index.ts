5c9879b27abd97de851b43449865166ba8fe1ced
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-07-16 15:53:40 +0545

    Moved Payment step just before finish

--- assets/js/getting-started/screens/index.ts
+++ assets/js/getting-started/screens/index.ts
@@ -1,6 +1,6 @@
 export { default as Welcome } from './welcome/Welcome';
-export { default as Currency } from './currency/Currency';
 export { default as Course } from './course/Course';
 export { default as Quiz } from './quiz/Quiz';
 export { default as Pages } from './pages/Pages';
+export { default as Payment } from './payment/Payment';
 export { default as Finish } from './finish/Finish';

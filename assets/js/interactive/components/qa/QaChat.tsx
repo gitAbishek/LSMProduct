878eba15c25a02c13acee047efb04ec0ab1b4c54
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-07-19 16:16:26 +0545

    Increased refetch QA chat interval to 60 second and disable refetch QA chat on mount

--- assets/js/interactive/components/qa/QaChat.tsx
+++ assets/js/interactive/components/qa/QaChat.tsx
@@ -44,7 +44,8 @@ const QaChat: React.FC<Props> = (props) => {
 				parent: chatData.parentId,
 			}),
 		{
-			refetchInterval: 30000,
+			refetchInterval: 60000,
+			refetchOnMount: false,
 		}
 	);
 

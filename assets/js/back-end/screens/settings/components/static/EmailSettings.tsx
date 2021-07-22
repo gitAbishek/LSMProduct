ae4a1d0c76d3dbbdfef5ae3c49413642559d0e1f
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-07-22 09:45:27 +0545

    Added `EmailSettings`

---
+++ assets/js/back-end/screens/settings/components/static/EmailSettings.tsx
@@ -0,0 +1,341 @@
+import {
+	Box,
+	FormControl,
+	FormLabel,
+	Icon,
+	Stack,
+	Switch,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
+	Tooltip,
+} from '@chakra-ui/react';
+import { __ } from '@wordpress/i18n';
+import { infoIconStyles } from 'Config/styles';
+import React from 'react';
+import { Controller, useFormContext } from 'react-hook-form';
+import { BiInfoCircle } from 'react-icons/bi';
+
+const EmailSetttings: React.FC = () => {
+	const { register, setValue, control } = useFormContext();
+
+	const tabStyles = {
+		justifyContent: 'flex-start',
+		w: '180px',
+		borderLeft: 0,
+		borderRight: '2px solid',
+		borderRightColor: 'transparent',
+		marginLeft: 0,
+		marginRight: '-2px',
+		pl: 0,
+		fontSize: 'sm',
+		textAlign: 'left',
+	};
+
+	const tabListStyles = {
+		borderLeft: 0,
+		borderRight: '2px solid',
+		borderRightColor: 'gray.200',
+	};
+
+	return (
+		<Tabs orientation="vertical">
+			<Stack direction="row" flex="1">
+				<TabList sx={tabListStyles}>
+					<Tab sx={tabStyles}>
+						{__('General', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'Users "new account" emails are sent to the user when a user signs up via checkout or account pages.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('New Order', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'New order emails are sent to students when a new order is received.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Processing Order', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'This is an order notification sent to students containing order details after payment.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Completed Order', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'Order complete emails are sent to students when their orders are marked completed.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Onhold Order', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'This is an order notification sent to students containing order details after an order is placed on-hold.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Cancelled Order', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'Cancelled order emails are sent to students when orders have been marked cancelled (if they were previously processing or on-hold).',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Enrolled Course', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'Email sent to students when they have enrolled course.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Completed Course', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'Email sent to students when they marked course is completed.',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+					<Tab sx={tabStyles}>
+						{__('Become an Instructor', 'masteriyo')}
+						<Tooltip
+							label={__(
+								'Email sent to user when they requested to become an instructor',
+								'masteriyo'
+							)}
+							hasArrow
+							fontSize="xs">
+							<Box as="span" sx={infoIconStyles}>
+								<Icon as={BiInfoCircle} />
+							</Box>
+						</Tooltip>
+					</Tab>
+				</TabList>
+				<TabPanels flex="1">
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.general.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.new_order.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.processing_order.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.completed_order.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.onhold_order.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.cancelled_order.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.enrolled_course.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.completed_course.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+
+					<TabPanel>
+						<Stack direction="column" spacing="6">
+							<FormControl>
+								<Stack direction="row">
+									<FormLabel minW="160px">
+										{__('Enable', 'masteriyo')}
+									</FormLabel>
+									<Controller
+										name="emails.become_an_instructor.enable"
+										render={({ field }) => (
+											<Switch {...field} defaultChecked={false} />
+										)}
+									/>
+								</Stack>
+							</FormControl>
+						</Stack>
+					</TabPanel>
+				</TabPanels>
+			</Stack>
+		</Tabs>
+	);
+};
+
+export default EmailSetttings;

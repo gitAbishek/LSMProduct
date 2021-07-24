15540e7ac95d9503a8ef2d062802672cb0f801c0
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-24 11:17:49 +0545

    Add schema and deepclean add new lesson

--- assets/js/back-end/screens/lessons/AddNewLesson.tsx
+++ assets/js/back-end/screens/lessons/AddNewLesson.tsx
@@ -25,9 +25,9 @@ import FullScreenLoader from '../../components/layout/FullScreenLoader';
 import HeaderBuilder from '../../components/layout/HeaderBuilder';
 import routes from '../../constants/routes';
 import urls from '../../constants/urls';
-import { SectionSchema } from '../../schemas';
+import { LessonSchema, SectionSchema } from '../../schemas';
 import API from '../../utils/api';
-import { deepMerge } from '../../utils/utils';
+import { deepClean, deepMerge } from '../../utils/utils';
 import Description from './components/Description';
 import FeaturedImage from './components/FeaturedImage';
 import Name from './components/Name';
@@ -48,8 +48,8 @@ const AddNewLesson: React.FC = () => {
 	);
 
 	// adds lesson on the database
-	const addLesson = useMutation((data: object) => lessonAPI.store(data), {
-		onSuccess: (data: any) => {
+	const addLesson = useMutation((data: LessonSchema) => lessonAPI.store(data), {
+		onSuccess: (data: LessonSchema) => {
 			toast({
 				title: __('Lesson Added', 'masteriyo'),
 				description: data.name + __(' is successfully added.', 'masteriyo'),
@@ -63,12 +63,12 @@ const AddNewLesson: React.FC = () => {
 		},
 	});
 
-	const onSubmit = (data: object) => {
+	const onSubmit = (data: LessonSchema) => {
 		const newData = {
 			course_id: courseId,
 			parent_id: sectionId,
 		};
-		addLesson.mutate(deepMerge(data, newData));
+		addLesson.mutate(deepMerge(deepClean(data), newData));
 	};
 
 	if (sectionQuery.isSuccess && sectionQuery.data.course_id == courseId) {

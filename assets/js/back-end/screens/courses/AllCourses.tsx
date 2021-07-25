433066b09e1de7bea0c67c3a8645bbe827fcd16d
Sanjeev Shrestha <sethstha@gmail.com>
2021-07-25 15:18:01 +0545

    Add course filter inside the course list

--- assets/js/back-end/screens/courses/AllCourses.tsx
+++ assets/js/back-end/screens/courses/AllCourses.tsx
@@ -91,39 +91,41 @@ const AllCourses = () => {
 	return (
 		<Stack direction="column" spacing="8" alignItems="center">
 			<Header hideCoursesMenu />
-			<CourseFilter setFilterParams={setFilterParams} />
 			<Container maxW="container.xl">
 				<Box bg="white" py="12" shadow="box" mx="auto">
-					<Stack direction="column" spacing="8">
-						<Table size="sm" sx={tableStyles}>
-							<Thead>
-								<Tr>
-									<Th>{__('Title', 'masteriyo')}</Th>
-									<Th>{__('Categories', 'masteriyo')}</Th>
-									<Th>{__('Author', 'masteriyo')}</Th>
-									<Th>{__('Price', 'masteriyo')}</Th>
-									<Th>{__('Date', 'masteriyo')}</Th>
-									<Th>{__('Actions', 'masteriyo')}</Th>
-								</Tr>
-							</Thead>
-							<Tbody>
-								{courseQuery.isLoading && <SkeletonCourseList />}
-								{courseQuery.isSuccess &&
-									courseQuery.data.map((course: any) => (
-										<CourseList
-											id={course.id}
-											name={course.name}
-											price={course.price}
-											categories={course.categories}
-											key={course.id}
-											createdOn={course.date_created}
-											permalink={course.permalink}
-											author={course.author}
-											onDeletePress={onDeletePress}
-										/>
-									))}
-							</Tbody>
-						</Table>
+					<Stack direction="column" spacing="10">
+						<CourseFilter setFilterParams={setFilterParams} />
+						<Stack direction="column" spacing="8">
+							<Table size="sm" sx={tableStyles}>
+								<Thead>
+									<Tr>
+										<Th>{__('Title', 'masteriyo')}</Th>
+										<Th>{__('Categories', 'masteriyo')}</Th>
+										<Th>{__('Author', 'masteriyo')}</Th>
+										<Th>{__('Price', 'masteriyo')}</Th>
+										<Th>{__('Date', 'masteriyo')}</Th>
+										<Th>{__('Actions', 'masteriyo')}</Th>
+									</Tr>
+								</Thead>
+								<Tbody>
+									{courseQuery.isLoading && <SkeletonCourseList />}
+									{courseQuery.isSuccess &&
+										courseQuery.data.map((course: any) => (
+											<CourseList
+												id={course.id}
+												name={course.name}
+												price={course.price}
+												categories={course.categories}
+												key={course.id}
+												createdOn={course.date_created}
+												permalink={course.permalink}
+												author={course.author}
+												onDeletePress={onDeletePress}
+											/>
+										))}
+								</Tbody>
+							</Table>
+						</Stack>
 					</Stack>
 				</Box>
 			</Container>

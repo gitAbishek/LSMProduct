d3c1ee089c7949b0fcc347936548e31cbdf99b2e
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-08-06 12:16:40 +0545

    Added `masteriyo_get_quiz_attempt_count` function

--- includes/Helper/quiz.php
+++ includes/Helper/quiz.php
@@ -221,3 +221,32 @@ function masteriyo_get_quiz_attempt( $attempt_id ) {
 
 	return $attempt;
 }
+
+/**
+ * Get quiz attempt count based on quiz id.
+ *
+ * @since 0.1.0
+ *
+ * @param int $quiz_id
+ * @param int $user_id
+ * @return int
+ */
+function masteriyo_get_quiz_attempt_count( $quiz_id, $user_id ) {
+	global $wpdb;
+
+	$user_id = $user_id ? $user_id : get_current_user_id();
+
+	$attempt_count = $wpdb->get_var(
+		$wpdb->prepare(
+			"SELECT COUNT(*)
+			FROM 	{$wpdb->prefix}masteriyo_quiz_attempts
+			WHERE user_id =  %d
+				AND quiz_id = %d
+			",
+			$user_id,
+			$quiz_id
+		)
+	);
+
+	return $attempt_count;
+}

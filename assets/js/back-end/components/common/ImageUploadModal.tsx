2340b1c2fa035ca57eb0dd4b000427649f1287fd
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-09-02 23:55:32 +0545

    Added support for video and added loader when image is uploading

--- assets/js/back-end/components/common/ImageUploadModal.tsx
+++ assets/js/back-end/components/common/ImageUploadModal.tsx
@@ -3,8 +3,11 @@ import {
 	Button,
 	ButtonGroup,
 	Center,
+	HStack,
 	Icon,
+	IconButton,
 	Image,
+	Input,
 	Modal,
 	ModalBody,
 	ModalCloseButton,
@@ -12,7 +15,7 @@ import {
 	ModalFooter,
 	ModalHeader,
 	ModalOverlay,
-	Progress,
+	Spinner,
 	Stack,
 	Tab,
 	TabList,
@@ -25,7 +28,7 @@ import {
 import { __ } from '@wordpress/i18n';
 import React, { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import { BiCheck } from 'react-icons/bi';
+import { BiCheck, BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import { useMutation, useQuery } from 'react-query';
 import { MediaSchema } from '../../schemas';
 import MediaAPI from '../../utils/media';
@@ -40,6 +43,9 @@ interface Props {
 	addButtonText?: string;
 }
 
+//@ts-ignore
+const homeURL = window._MASTERIYO_.home_url;
+
 const ImageUploadModal: React.FC<Props> = (props) => {
 	const { isOpen, onClose, onComplete, get, selected, title, addButtonText } =
 		props;
@@ -48,18 +54,37 @@ const ImageUploadModal: React.FC<Props> = (props) => {
 	const [selectedImage, setSelectedImage] = useState<any>(selected);
 	const [imageUrl, setImageUrl] = useState<any>(null);
 	const [tabIndex, setTabIndex] = useState(0);
+	const [imageTitle, setImageTitle] = useState<string>('');
 
-	const imagesQuery = useQuery('medias', () => imageAPi.list());
+	const [page, setPage] = useState(1);
+	const imagesQuery = useQuery(['medias', page], () =>
+		imageAPi.list({ page: page })
+	);
+
 	const uploadMedia = useMutation((image: any) => imageAPi.store(image));
 
+	const prevPage = page > 1 ? page - 1 : 1;
+	const nextPage = page + 1;
+
+	const getImageTitle = (imageTitle: string, mimeType: string) => {
+		// Get only type. Example: 'video/mp4' -> 'mp4', 'image/png' -> 'png'.
+		const mediaType = mimeType.substring(
+			mimeType.lastIndexOf('/') + 1,
+			mimeType.length
+		);
+		const title = imageTitle + '.' + mediaType;
+
+		return title;
+	};
+
 	const onUpload = (file: any) => {
+		setTabIndex(1);
 		let formData = new FormData();
 		formData.append('file', file);
 
 		uploadMedia.mutate(formData, {
 			onSuccess: (data: MediaSchema) => {
 				imagesQuery.refetch();
-				setTabIndex(1);
 				setSelectedImage(data.id);
 				setImageUrl(data.source_url);
 			},
@@ -85,7 +110,8 @@ const ImageUploadModal: React.FC<Props> = (props) => {
 
 	const { getRootProps, getInputProps, isDragAccept, isDragReject } =
 		useDropzone({
-			accept: 'image/jpeg, image/png',
+			accept:
+				'image/jpeg, image/png, video/mp4, video/m4v, video/webm, video/ogv, video/wmv, video/flv',
 			onDrop: onDrop,
 		});
 
@@ -138,7 +164,12 @@ const ImageUploadModal: React.FC<Props> = (props) => {
 									<input {...getInputProps()} multiple={false} />
 
 									{uploadMedia.isLoading ? (
-										<Progress hasStripe size="xs" />
+										<>
+											<Spinner />
+											<Text ml="3">
+												{__('Uploading file, please wait...', 'masteriyo')}
+											</Text>
+										</>
 									) : (
 										<Stack direction="column">
 											<Text>{__('Drop Files To Upload', 'masteriyo')}</Text>
@@ -151,10 +182,23 @@ const ImageUploadModal: React.FC<Props> = (props) => {
 								</Center>
 							</TabPanel>
 							<TabPanel>
-								{imagesQuery.isSuccess && (
-									<Stack direction="row" flexWrap="wrap" spacing="2">
-										{imagesQuery.data.map((image: MediaSchema) => {
-											if (image.media_type === 'image') {
+								{imagesQuery.isLoading && (
+									<Center mt="4"> Loading images...</Center>
+								)}
+								<Stack direction="row" flexWrap="wrap" spacing="2">
+									{uploadMedia.isLoading && (
+										<Center bg="gray.100" w="100px" h="100px">
+											<Box>
+												<Spinner />
+											</Box>
+										</Center>
+									)}
+									{imagesQuery.isSuccess &&
+										imagesQuery.data.data.map((image: MediaSchema) => {
+											if (
+												image.media_type === 'image' ||
+												image.media_type === 'file'
+											) {
 												return (
 													<Box
 														key={image.id}
@@ -172,8 +216,22 @@ const ImageUploadModal: React.FC<Props> = (props) => {
 															onClick={() => {
 																setSelectedImage(image.id);
 																setImageUrl(image.source_url);
+																setImageTitle(
+																	getImageTitle(
+																		image.title.rendered,
+																		image.mime_type
+																	)
+																);
 															}}
-															src={image.source_url}
+															onDoubleClick={() =>
+																window.open(image.source_url, '_blank')
+															}
+															src={
+																image.media_type === 'image'
+																	? image.source_url
+																	: homeURL +
+																	  '/wp-includes/images/media/video.png'
+															}
 															objectFit="cover"
 															h="full"
 															w="full"
@@ -194,6 +252,53 @@ const ImageUploadModal: React.FC<Props> = (props) => {
 												);
 											}
 										})}
+								</Stack>
+								{imagesQuery.isSuccess && (
+									<Stack
+										mt="8"
+										w="full"
+										direction="row"
+										justifyContent="space-between"
+										pb="4"
+										fontSize="sm">
+										<Text>{imageTitle}</Text>
+										<HStack>
+											<IconButton
+												shadow="none"
+												rounded="sm"
+												_hover={{ bg: 'blue.500', color: 'white' }}
+												icon={<Icon fontSize="xl" as={BiChevronLeft} />}
+												size="sm"
+												aria-label="Previous page"
+												onClick={() => {
+													setPage(prevPage);
+												}}
+												disabled={page < 2}
+											/>
+											<Input
+												size="sm"
+												w="10"
+												type="number"
+												min={1}
+												value={page}
+												isReadOnly={true}
+											/>
+											<IconButton
+												shadow="none"
+												rounded="sm"
+												_hover={{ bg: 'blue.500', color: 'white' }}
+												icon={<Icon fontSize="xl" as={BiChevronRight} />}
+												aria-label="next page"
+												size="sm"
+												onClick={() => {
+													setPage(nextPage);
+												}}
+												disabled={
+													page ===
+													parseInt(imagesQuery.data.headers['x-wp-totalpages'])
+												}
+											/>
+										</HStack>
 									</Stack>
 								)}
 							</TabPanel>

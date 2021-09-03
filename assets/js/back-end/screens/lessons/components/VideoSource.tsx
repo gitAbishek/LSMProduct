7480931ac2b32f85aa1e5d52f72549a1478ba0bb
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-09-03 17:25:10 +0545

    Added self hosted video support

--- assets/js/back-end/screens/lessons/components/VideoSource.tsx
+++ assets/js/back-end/screens/lessons/components/VideoSource.tsx
@@ -1,32 +1,72 @@
 import {
+	AspectRatio,
+	Button,
+	ButtonGroup,
+	Center,
 	FormControl,
 	FormErrorMessage,
 	FormLabel,
 	Input,
 	Select,
+	Spinner,
 	Stack,
+	useDisclosure,
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import ImageUploadModal from '../../../components/common/ImageUploadModal';
+import { MediaSchema } from '../../../schemas';
+import MediaAPI from '../../../utils/media';
 
 interface Props {
 	defaultSource?: any;
 	defaultSourceUrl?: any;
+	defaultSourceID?: number;
 }
 const VideoSource: React.FC<Props> = (props) => {
-	const { defaultSource, defaultSourceUrl } = props;
+	const { defaultSource, defaultSourceUrl, defaultSourceID } = props;
+	const [videoId, setVideoId] = useState<any>(null);
+	const { onOpen, onClose, isOpen } = useDisclosure();
 	const {
 		register,
 		control,
 		formState: { errors },
+		setValue,
 	} = useFormContext();
+
+	const imageAPi = new MediaAPI();
+
+	useEffect(() => {
+		setVideoId(defaultSourceID || null);
+	}, [defaultSourceID]);
+
+	const mediaQuery = useQuery<MediaSchema>(
+		[`videoSource${videoId}`, videoId],
+		() => imageAPi.get(videoId),
+		{
+			enabled: !!videoId,
+			refetchOnWindowFocus: true,
+		}
+	);
 
 	const watchSource = useWatch({
 		name: 'video_source',
 		defaultValue: defaultSource || 'self-hosted',
 		control,
 	});
+
+	const onComplete = (videoId: number) => {
+		setVideoId(videoId);
+		setValue('video_source_url', videoId.toString());
+		onClose();
+	};
+
+	const onDelete = () => {
+		setVideoId(null);
+		setValue('video_source_url', ' ');
+	};
 
 	return (
 		<Stack direction="row" spacing="6">
@@ -57,6 +97,54 @@ const VideoSource: React.FC<Props> = (props) => {
 					<FormErrorMessage>
 						{errors?.video_source_url && errors?.video_source_url?.message}
 					</FormErrorMessage>
+				</FormControl>
+			)}
+			{watchSource === 'self-hosted' && (
+				<FormControl>
+					<FormLabel>{__('Self Hosted Video', 'masteriyo')}</FormLabel>
+					{mediaQuery.isLoading && (
+						<Center mb="4" mt="4">
+							<Spinner />
+						</Center>
+					)}
+					{mediaQuery.isSuccess ? (
+						<Stack direction="column" spacing="4">
+							<AspectRatio ratio={16 / 9}>
+								<video
+									src={mediaQuery?.data?.source_url}
+									title={mediaQuery?.data?.title?.rendered}
+									controls
+								/>
+							</AspectRatio>
+							<ButtonGroup d="flex" justifyContent="space-between">
+								<Button variant="outline" onClick={onDelete} colorScheme="red">
+									{__('Remove Video', 'masteriyo')}
+								</Button>
+								<Button variant="outline" onClick={onOpen} colorScheme="blue">
+									{__('Add New Video', 'masteriyo')}
+								</Button>
+							</ButtonGroup>
+						</Stack>
+					) : (
+						<ButtonGroup d="flex" justifyContent="space-between">
+							<Button
+								variant="outline"
+								isFullWidth
+								onClick={onOpen}
+								colorScheme="blue">
+								{__('Add Video', 'masteriyo')}
+							</Button>
+						</ButtonGroup>
+					)}
+					<ImageUploadModal
+						title={__('Video', 'masteriyo')}
+						addButtonText={__('Set Video', 'masteriyo')}
+						isOpen={isOpen}
+						onClose={onClose}
+						onComplete={onComplete}
+						selected={videoId}
+						mediaType="video"
+					/>
 				</FormControl>
 			)}
 		</Stack>

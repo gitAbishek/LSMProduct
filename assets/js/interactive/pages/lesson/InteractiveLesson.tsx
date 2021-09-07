b5aa12e1d5a9520c44976d2d7dbd7f29b08ab97f
Sanjeev Shrestha <sethstha@gmail.com>
2021-09-07 08:44:25 +0545

    Narrow down the width of container

--- assets/js/interactive/pages/lesson/InteractiveLesson.tsx
+++ assets/js/interactive/pages/lesson/InteractiveLesson.tsx
@@ -89,7 +89,7 @@ const InteractiveLesson = () => {
 	};
 
 	return (
-		<Container centerContent maxW="container.xl" py="16">
+		<Container centerContent maxW="container.lg" py="16">
 			<Box bg="white" p="14" shadow="box" w="full">
 				<Stack direction="column" spacing="8">
 					<Heading as="h5">{lessonQuery?.data?.name}</Heading>

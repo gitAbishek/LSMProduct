50dcc2292439a35da4fe1e40193312ecf754b24b
Sanjeev Shrestha <sethstha@gmail.com>
2021-09-15 22:30:13 +0545

    Don't show video box when video is not selected on interactive page

--- assets/js/interactive/pages/lesson/InteractiveLesson.tsx
+++ assets/js/interactive/pages/lesson/InteractiveLesson.tsx
@@ -93,10 +93,12 @@ const InteractiveLesson = () => {
 			<Box bg="white" p="14" shadow="box" w="full">
 				<Stack direction="column" spacing="8">
 					<Heading as="h5">{lessonQuery?.data?.name}</Heading>
-					<VideoPlayer
-						type={lessonQuery?.data?.video_source}
-						url={lessonQuery?.data?.video_source_url}
-					/>
+					{lessonQuery?.data?.video_source_url && (
+						<VideoPlayer
+							type={lessonQuery?.data?.video_source}
+							url={lessonQuery?.data?.video_source_url}
+						/>
+					)}
 					<Image src={imageQuery?.data?.source_url} />
 					<Text
 						dangerouslySetInnerHTML={{ __html: lessonQuery?.data?.description }}

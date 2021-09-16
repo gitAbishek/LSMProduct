d311041544759ef1f08caadc9f0b95acb0ddf08f
Sanjeev Shrestha <sethstha@gmail.com>
2021-09-16 12:44:38 +0545

    Reduce spacing on interactive

--- assets/js/interactive/pages/lesson/InteractiveLesson.tsx
+++ assets/js/interactive/pages/lesson/InteractiveLesson.tsx
@@ -90,7 +90,7 @@ const InteractiveLesson = () => {
 
 	return (
 		<Container centerContent maxW="container.lg" py="16">
-			<Box bg="white" p="14" shadow="box" w="full">
+			<Box bg="white" p={['5', null, '14']} shadow="box" w="full">
 				<Stack direction="column" spacing="8">
 					<Heading as="h5">{lessonQuery?.data?.name}</Heading>
 					{lessonQuery?.data?.video_source_url && (

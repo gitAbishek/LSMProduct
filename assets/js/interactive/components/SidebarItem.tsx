886bbe3c24f12753e33d33b7812e057a4678cd4f
Sanjeev Shrestha <sethstha@gmail.com>
2021-09-24 18:38:23 +0545

    Fix item type showing on sidebar

--- assets/js/interactive/components/SidebarItem.tsx
+++ assets/js/interactive/components/SidebarItem.tsx
@@ -82,7 +82,6 @@ const SidebarItem: React.FC<Props> = (props) => {
 												content.item_type,
 												courseId
 											)}>
-											{content.item_type}
 											<Stack direction="row" spacing="2" alignItems="center">
 												<Icon
 													as={getContentIcon(content.item_type, content.video)}

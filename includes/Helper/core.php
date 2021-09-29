9f5ecbc7544206e42d5e669238e5a7f7c1e9afe6
Aashil Bijukshe <aashil.themegrill@gmail.com>
2021-09-29 14:45:44 +0545

    Added masteriyo_color_luminance function for getting hex value based on percentage

--- includes/Helper/core.php
+++ includes/Helper/core.php
@@ -3019,3 +3019,33 @@ function masteriyo_get_allowed_svg_elements() {
 		)
 	);
 }
+
+/**
+ * Lightens/darkens a given colour (hex format), returning the altered colour in hex format.
+ * @see: https://gist.github.com/stephenharris/5532899
+ *
+ * @since 1.0.4
+ *
+ * @param string $hex Colour as hexadecimal (with or without hash);
+ * @percent float $percent Decimal ( 0.2 = lighten by 20%(), -0.4 = darken by 40%() )
+ * @return string Lightened/Darkend colour as hexadecimal (with hash);
+ */
+function masteriyo_color_luminance( $hex, $percent ) {
+	// validate hex string
+	$hex     = preg_replace( '/[^0-9a-f]/i', '', $hex );
+	$new_hex = '#';
+
+	if ( strlen( $hex ) < 6 ) {
+		$hex = $hex[0] + $hex[0] + $hex[1] + $hex[1] + $hex[2] + $hex[2];
+	}
+
+	// convert to decimal and change luminosity
+	for ( $i = 0; $i < 3; $i++ ) {
+		$dec      = hexdec( substr( $hex, $i * 2, 2 ) );
+		$dec      = min( max( 0, $dec + $dec * $percent ), 255 );
+		$new_hex .= str_pad( dechex( $dec ), 2, 0, STR_PAD_LEFT );
+	}
+
+	return $new_hex;
+}
+

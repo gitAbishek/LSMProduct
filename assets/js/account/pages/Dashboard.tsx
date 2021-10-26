6ef08970ba11508c48cc22b74d203ee0251c037f
Sushma Chaudhary <susmachaudhary04@gmail.com>
2021-10-26 10:29:44 +0545

    added TabPanels

--- assets/js/account/pages/Dashboard.tsx
+++ assets/js/account/pages/Dashboard.tsx
@@ -6,6 +6,11 @@ import {
 	Container,
 	Flex,
 	Stack,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
 	Text,
 } from '@chakra-ui/react';
 import React from 'react';
@@ -41,7 +46,24 @@ const Dashboard = () => {
 						<Center>Dashboard</Center>
 					</Box>
 				</Stack>
+				<Stack direction="column" mt={10} px={10}>
+					<Tabs variant="soft-rounded" colorScheme="green" direction="column">
+						<TabList>
+							<Tab>Tab 1</Tab>
+							<Tab>Tab 2</Tab>
+						</TabList>
+						<TabPanels>
+							<TabPanel>
+								<p>one!</p>
+							</TabPanel>
+							<TabPanel>
+								<p>two!</p>
+							</TabPanel>
+						</TabPanels>
+					</Tabs>
+				</Stack>
 			</Container>
+
 			<Courses />
 		</Flex>
 	);

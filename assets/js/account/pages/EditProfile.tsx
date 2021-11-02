bb2eb259c30e3dbf9621348d3d52406794b46983
Sushma Chaudhary <susmachaudhary04@gmail.com>
2021-11-02 12:08:15 +0545

    added react-hook-form in edit-profile page

--- assets/js/account/pages/EditProfile.tsx
+++ assets/js/account/pages/EditProfile.tsx
@@ -17,8 +17,28 @@ import {
 } from '@chakra-ui/react';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const EditProfile = () => {
+type IFormInputs = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	contact: number;
+	address: string;
+	city: string;
+	state: string;
+	zipcode: string;
+	country: string;
+};
+
+const EditProfile: React.FC = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IFormInputs>();
+	const onSubmit = handleSubmit((data) => console.log(data));
+
 	return (
 		<Box bg="white" width="full">
 			<Container maxWidth="container.lg">
@@ -38,79 +58,162 @@ const EditProfile = () => {
 
 						<TabPanels>
 							<TabPanel>
-								<Stack>
+								<Stack mt="10">
 									<Center>
 										<Avatar
 											size="xl"
 											name="Rose Pett"
-											mt="10"
 											src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+											style={{
+												filter: 'drop-shadow(0px 2px 8px rgba(0, 0, 0, 0.2))',
+											}}
 										/>{' '}
 									</Center>
 								</Stack>
-								<Stack spacing="8" mt="8">
-									<Stack direction="row" spacing="8">
-										<FormControl id="Fisrt Name">
-											<FormLabel> {__('First Name', 'masteriyo')}</FormLabel>
-											<Input type="text" />
-										</FormControl>
-										<FormControl id="Last Name">
-											<FormLabel>{__('Last Name', 'masteriyo')}</FormLabel>
-											<Input type="text" />
-										</FormControl>
+								<form onSubmit={onSubmit}>
+									<Stack spacing="8" mt="8">
+										<Stack direction="row" spacing="8">
+											<FormControl id="Fisrt Name">
+												<FormLabel> {__('First Name', 'masteriyo')}</FormLabel>
+												<Input
+													type="text"
+													{...register('firstName', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+												{errors?.firstName && <p>{errors.firstName.message}</p>}
+											</FormControl>
+											<FormControl id="Last Name">
+												<FormLabel>{__('Last Name', 'masteriyo')}</FormLabel>
+												<Input
+													type="text"
+													{...register('lastName', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+												{errors?.lastName && <p>{errors.lastName.message}</p>}
+											</FormControl>
+										</Stack>
+
+										<Stack>
+											<FormControl id="email">
+												<FormLabel>{__('Email', 'masteriyo')}</FormLabel>
+												<Input
+													type="email"
+													{...register('email', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+												{errors?.email && <p>{errors.email.message}</p>}
+											</FormControl>
+										</Stack>
+										<Stack>
+											<FormControl id="contact">
+												<FormLabel>
+													{__('Contact Number', 'masteriyo')}
+												</FormLabel>
+												<Input
+													type="number"
+													{...register('contact', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+											</FormControl>
+										</Stack>
+										<Stack>
+											<FormControl id="address">
+												<FormLabel>{__('Address', 'masteriyo')}</FormLabel>
+												<Input
+													type="type"
+													{...register('address', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+											</FormControl>
+										</Stack>
+										<Stack direction="row" spacing="8">
+											<FormControl id="city">
+												<FormLabel>{__('City', 'masteriyo')}</FormLabel>
+												<Input
+													type="text"
+													{...register('city', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+											</FormControl>
+											<FormControl id="state">
+												<FormLabel>{__('State', 'masteriyo')}</FormLabel>
+												<Input
+													type="text"
+													{...register('state', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+											</FormControl>
+										</Stack>
+
+										<Stack direction="row" spacing="8">
+											<FormControl id="zipcode">
+												<FormLabel>{__('Zip Code', 'masteriyo')}</FormLabel>
+												<Input
+													type="number"
+													{...register('zipcode', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+											</FormControl>
+											<FormControl id="country">
+												<FormLabel>{__('Country', 'masteriyo')}</FormLabel>
+												<Input
+													type="text"
+													{...register('country', {
+														required: __(
+															'This field cannot be empty',
+															'masteriyo'
+														),
+													})}
+												/>
+											</FormControl>
+										</Stack>
 									</Stack>
 
-									<Stack>
-										<FormControl id="email">
-											<FormLabel>{__('Email', 'masteriyo')}</FormLabel>
-											<Input type="email" />
-										</FormControl>
+									<Stack py="10">
+										<ButtonGroup>
+											<Button
+												colorScheme="blue"
+												rounded="full"
+												fontSize="small"
+												type="submit"
+												px="18">
+												{__('SAVE', 'masteriyo')}
+											</Button>
+										</ButtonGroup>
 									</Stack>
-									<Stack>
-										<FormControl id="contact">
-											<FormLabel>{__('Contact Number', 'masteriyo')}</FormLabel>
-											<Input type="number" />
-										</FormControl>
-									</Stack>
-									<Stack>
-										<FormControl id="address">
-											<FormLabel>{__('Address', 'masteriyo')}</FormLabel>
-											<Input type="type" />
-										</FormControl>
-									</Stack>
-									<Stack direction="row" spacing="8">
-										<FormControl id="city">
-											<FormLabel>{__('City', 'masteriyo')}</FormLabel>
-											<Input type="text" />
-										</FormControl>
-										<FormControl id="state">
-											<FormLabel>{__('State', 'masteriyo')}</FormLabel>
-											<Input type="text" />
-										</FormControl>
-									</Stack>
-
-									<Stack direction="row" spacing="8">
-										<FormControl id="zipcode">
-											<FormLabel>{__('Zip Code', 'masteriyo')}</FormLabel>
-											<Input type="number" />
-										</FormControl>
-										<FormControl id="country">
-											<FormLabel>{__('Country', 'masteriyo')}</FormLabel>
-											<Input type="text" />
-										</FormControl>
-									</Stack>
-								</Stack>
-								<Stack py="10">
-									<ButtonGroup>
-										<Button
-											colorScheme="blue"
-											rounded="full"
-											fontSize="small"
-											px="18">
-											{__('SAVE', 'masteriyo')}
-										</Button>
-									</ButtonGroup>
-								</Stack>
+								</form>
 							</TabPanel>
 						</TabPanels>
 					</Stack>

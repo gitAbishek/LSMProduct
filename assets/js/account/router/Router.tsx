b91dc7a7f28c373f38e8d4d43f08412f9b29294c
Sanjeev Shrestha <sethstha@gmail.com>
2021-11-10 08:47:22 +0545

    Add dynamic sidebar for the all the pages on account

--- assets/js/account/router/Router.tsx
+++ assets/js/account/router/Router.tsx
@@ -1,5 +1,7 @@
+import { Stack } from '@chakra-ui/react';
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
 import routes from '../constants/routes';
 import MyCourses from '../pages/courses/MyCouses';
 import Dashboard from '../pages/dashboard/Dashboard';
@@ -10,20 +12,23 @@ const Router: React.FC = () => {
 	return (
 		<HashRouter>
 			<Header />
-			<Switch>
-				<Route path={routes.dashboard}>
-					<Dashboard />
-				</Route>
-				<Route path={routes.myCourses}>
-					<MyCourses />
-				</Route>
-				<Route path={routes.myOrderHistory}>
-					<OrderHistory />
-				</Route>
-				<Route>
-					<Dashboard />
-				</Route>
-			</Switch>
+			<Stack direction="row" spacing="8">
+				<Sidebar />
+				<Switch>
+					<Route path={routes.dashboard}>
+						<Dashboard />
+					</Route>
+					<Route path={routes.myCourses}>
+						<MyCourses />
+					</Route>
+					<Route path={routes.myOrderHistory}>
+						<OrderHistory />
+					</Route>
+					<Route>
+						<Dashboard />
+					</Route>
+				</Switch>
+			</Stack>
 		</HashRouter>
 	);
 };

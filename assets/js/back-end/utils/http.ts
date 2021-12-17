1c2c1a64a3a67317b125d62f732d00723498e345
Sanjeev Shrestha <sethstha@gmail.com>
2021-12-17 16:05:33 +0545

    Remove comment from http

--- assets/js/back-end/utils/http.ts
+++ assets/js/back-end/utils/http.ts
@@ -1,15 +1,5 @@
 import http from '@wordpress/api-fetch';
 
-// const http = axios.create({
-// 	//@ts-ignore
-// 	baseURL: _MASTERIYO_.rootApiUrl,
-// 	headers: {
-// 		'Content-Type': 'application/json',
-// 		//@ts-ignore
-// 		'X-WP-Nonce': _MASTERIYO_.nonce,
-// 	},
-// });
-
 //@ts-ignore
 http.use(http.createNonceMiddleware(_MASTERIYO_.nonce));
 

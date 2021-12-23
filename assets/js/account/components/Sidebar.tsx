47cce5a8046e6b6cb7cf60654a7bab87f39cda73
Sanjeev Shrestha <sethstha@gmail.com>
2021-12-23 10:00:18 +0545

    Fix - issue on sidebar

--- assets/js/account/components/Sidebar.tsx
+++ assets/js/account/components/Sidebar.tsx
@@ -12,6 +12,7 @@ import React from 'react';
 import { BiBook, BiGrid, BiHistory, BiLogOut, BiUser } from 'react-icons/bi';
 import { useQuery } from 'react-query';
 import { NavLink, useLocation } from 'react-router-dom';
+import FullScreenLoader from '../../back-end/components/layout/FullScreenLoader';
 import urls from '../../back-end/constants/urls';
 import { UserSchema } from '../../back-end/schemas';
 import API from '../../back-end/utils/api';
@@ -40,84 +41,88 @@ const Sidebar = () => {
 		borderColor: 'blue.500',
 		color: 'blue.500',
 	};
-	return (
-		<List
-			mr="6"
-			flex="0 0 200px"
-			borderRight="1px"
-			borderRightColor="gray.100"
-			spacing="6"
-			fontSize="sm"
-			fontWeight="medium"
-			color="gray.600">
-			<ListItem>
-				<Stack direction="row" align="center" spacing="3">
-					<Avatar
-						size="sm"
-						name={data?.first_name}
-						src={data?.avatar_url}
-						showBorder
-						shadow="md"
-					/>
-					<Stack direction="column" spacing="0">
-						<Heading as="h5" fontSize="xs" fontWeight="medium">
-							{data?.first_name} {data?.last_name}
-						</Heading>
+	if (isSuccess) {
+		return (
+			<List
+				mr="6"
+				flex="0 0 200px"
+				borderRight="1px"
+				borderRightColor="gray.100"
+				spacing="6"
+				fontSize="sm"
+				fontWeight="medium"
+				color="gray.600">
+				<ListItem>
+					<Stack direction="row" align="center" spacing="3">
+						<Avatar
+							size="sm"
+							name={data?.first_name}
+							src={data?.avatar_url}
+							showBorder
+							shadow="md"
+						/>
+						<Stack direction="column" spacing="0">
+							<Heading as="h5" fontSize="xs" fontWeight="medium">
+								{data?.first_name} {data?.last_name}
+							</Heading>
+						</Stack>
 					</Stack>
-				</Stack>
-			</ListItem>
-			<ListItem>
-				<Link
-					as={NavLink}
-					sx={navLinkStyles}
-					_activeLink={navActiveStyles}
-					to={routes.dashboard}>
-					<ListIcon fontSize="md" mr="3" as={BiGrid} />
-					{__('Dashboard', 'masteriyo')}
-				</Link>
-			</ListItem>
-			<ListItem>
-				<Link
-					as={NavLink}
-					sx={navLinkStyles}
-					_activeLink={navActiveStyles}
-					to={routes.myCourses}>
-					<ListIcon fontSize="md" mr="3" as={BiBook} />
-					{__('My Courses')}
-				</Link>
-			</ListItem>
-			<ListItem>
-				<Link
-					as={NavLink}
-					sx={
-						location.pathname?.includes('/user')
-							? navActiveStyles
-							: navLinkStyles
-					}
-					_activeLink={navActiveStyles}
-					to={routes.user.profile}>
-					<ListIcon fontSize="md" mr="3" as={BiUser} />
-					{__('My Profile')}
-				</Link>
-			</ListItem>
+				</ListItem>
+				<ListItem>
+					<Link
+						as={NavLink}
+						sx={navLinkStyles}
+						_activeLink={navActiveStyles}
+						to={routes.dashboard}>
+						<ListIcon fontSize="md" mr="3" as={BiGrid} />
+						{__('Dashboard', 'masteriyo')}
+					</Link>
+				</ListItem>
+				<ListItem>
+					<Link
+						as={NavLink}
+						sx={navLinkStyles}
+						_activeLink={navActiveStyles}
+						to={routes.myCourses}>
+						<ListIcon fontSize="md" mr="3" as={BiBook} />
+						{__('My Courses')}
+					</Link>
+				</ListItem>
+				<ListItem>
+					<Link
+						as={NavLink}
+						sx={
+							location.pathname?.includes('/user')
+								? navActiveStyles
+								: navLinkStyles
+						}
+						_activeLink={navActiveStyles}
+						to={routes.user.profile}>
+						<ListIcon fontSize="md" mr="3" as={BiUser} />
+						{__('My Profile')}
+					</Link>
+				</ListItem>
 
-			<ListItem>
-				<Link
-					as={NavLink}
-					sx={navLinkStyles}
-					_activeLink={navActiveStyles}
-					to={routes.myOrderHistory}>
-					<ListIcon fontSize="md" mr="3" as={BiHistory} />
-					{__('My Order History')}
-				</Link>
-			</ListItem>
+				<ListItem>
+					<Link
+						as={NavLink}
+						sx={navLinkStyles}
+						_activeLink={navActiveStyles}
+						to={routes.myOrderHistory}>
+						<ListIcon fontSize="md" mr="3" as={BiHistory} />
+						{__('My Order History')}
+					</Link>
+				</ListItem>
 
-			<ListItem>
-				<ListIcon fontSize="md" mr="3" as={BiLogOut} />
-				{__('Logout')}
-			</ListItem>
-		</List>
-	);
+				<ListItem>
+					<ListIcon fontSize="md" mr="3" as={BiLogOut} />
+					{__('Logout')}
+				</ListItem>
+			</List>
+		);
+	}
+
+	return <FullScreenLoader />;
 };
 
 export default Sidebar;

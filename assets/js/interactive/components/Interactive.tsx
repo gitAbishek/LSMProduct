30f06ca1b08d91b10624b0eae67a2ae6a3f0c299
Sanjeev Shrestha <sethstha@gmail.com>
2022-01-19 09:05:04 +0545

    Make lesson and quiz routing on the parent

--- assets/js/interactive/components/Interactive.tsx
+++ assets/js/interactive/components/Interactive.tsx
@@ -5,11 +5,8 @@ import { useHistory, useLocation, useParams } from 'react-router-dom';
 import FullScreenLoader from '../../back-end/components/layout/FullScreenLoader';
 import urls from '../../back-end/constants/urls';
 import API from '../../back-end/utils/api';
-import InteractiveRouter from '../router/InteractiveRouter';
 import { CourseProgressMap } from '../schemas';
 import { getNavigationRoute } from './FloatingNavigation';
-import Header from './Header';
-import Sidebar from './Sidebar';
 
 const Interactive: React.FC = () => {
 	const { courseId }: any = useParams();
@@ -45,23 +42,7 @@ const Interactive: React.FC = () => {
 	if (courseProgressQuery.isSuccess) {
 		return (
 			<Box h="full" overflowX="hidden" pos="relative">
-				<Header
-					summary={courseProgressQuery.data.summary}
-					isOpen={isHeaderOpen}
-					onToggle={onHeaderToggle}
-				/>
-
-				<Sidebar
-					isOpen={isSidebarOpen}
-					onToggle={onSidebarToggle}
-					isHeaderOpen={isHeaderOpen}
-					items={courseProgressQuery.data.items}
-					name={courseProgressQuery.data.name}
-					coursePermalink={courseProgressQuery.data.course_permalink}
-				/>
-				<Box transition="all 0.35s" ml={isSidebarOpen ? '300px' : 0}>
-					<InteractiveRouter />
-				</Box>
+				<Box transition="all 0.35s" ml={isSidebarOpen ? '300px' : 0}></Box>
 			</Box>
 		);
 	}
--- assets/js/interactive/router/Router.tsx
+++ assets/js/interactive/router/Router.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Interactive from '../components/Interactive';
 import routes from '../constants/routes';
+import { InteractiveLesson, InteractiveQuiz } from '../pages';
 
 const Router: React.FC = () => {
 	return (
 		<HashRouter>
 			<Switch>
-				<Route path={routes.course}>
+				<Route path={routes.course} exact>
 					<Interactive />
+				</Route>
+				<Route path={routes.lesson} exact>
+					<InteractiveLesson />
+				</Route>
+				<Route path={routes.quiz} exact>
+					<InteractiveQuiz />
 				</Route>
 			</Switch>
 		</HashRouter>

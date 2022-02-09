85abb24acd406cbec12a50d556d12d0f271f3478
Sagar Tamang <mi5t4n@gmail.com>
2022-02-09 14:07:15 +0545

    Refactor masteriyo_color_liminane()

--- includes/Helper/core.php
+++ includes/Helper/core.php
@@ -3069,7 +3069,7 @@ function masteriyo_color_luminance( $hex, $percent ) {
 		return false;
 	}
 
-	if ( strlen( $hex ) > 3 && strlen( $hex ) < 6 ) {
+	if ( strlen( $hex ) >= 3 && strlen( $hex ) < 6 ) {
 		$hex = $hex[0] + $hex[0] + $hex[1] + $hex[1] + $hex[2] + $hex[2];
 	}
 

b38c1827e8e78d18b4ae26a27d3006850ad3e1d4
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2022-04-08 15:16:08 +0545

    Fix - Some sub categories being hidden (#701)

--- assets/js/back-end/utils/categories.ts
+++ assets/js/back-end/utils/categories.ts
@@ -6,6 +6,7 @@ export const makeCategoriesHierarchy = (
 	excludeId: number = -1
 ): CourseCategoryHierarchy[] => {
 	let newList: CourseCategoryHierarchy[] = [];
+	let includedCategoryIds: number[] = [];
 
 	const makeCategoryChildren = (
 		category: CourseCategorySchema,
@@ -16,6 +17,7 @@ export const makeCategoriesHierarchy = (
 		categories
 			.filter((item) => item.parent_id === category.id && item.id !== excludeId)
 			.forEach((child) => {
+				includedCategoryIds.push(child.id);
 				children.push({ ...child, depth });
 				children = children.concat(makeCategoryChildren(child, depth + 1));
 			});
@@ -26,9 +28,16 @@ export const makeCategoriesHierarchy = (
 	categories
 		.filter((cat) => cat.parent_id === 0 && cat.id !== excludeId)
 		.forEach((cat) => {
+			includedCategoryIds.push(cat.id);
 			newList.push({ ...cat, depth: 0 });
 			newList = newList.concat(makeCategoryChildren(cat));
 		});
+
+	// Include the categories that were left out because of its top level parent not being present.
+	newList = categories
+		.filter((category) => !includedCategoryIds.includes(category.id))
+		.map((category) => ({ ...category, depth: 0 }))
+		.concat(newList);
 
 	return newList;
 };

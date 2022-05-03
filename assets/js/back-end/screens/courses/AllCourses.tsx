57134f597a3180cd3d88d4747e2a330897b442c1
Aashil Bijukshe <aashil.themegrill@gmail.com>
2022-05-03 20:20:40 +0545

    Course status tab skeleton fixes

--- assets/js/back-end/screens/courses/AllCourses.tsx
+++ assets/js/back-end/screens/courses/AllCourses.tsx
@@ -35,7 +35,6 @@ import { Table, Tbody, Th, Thead, Tr } from 'react-super-responsive-table';
 import EmptyInfo from '../../components/common/EmptyInfo';
 import Header from '../../components/common/Header';
 import MasteriyoPagination from '../../components/common/MasteriyoPagination';
-import FullScreenLoader from '../../components/layout/FullScreenLoader';
 import { navActiveStyles } from '../../config/styles';
 import routes from '../../constants/routes';
 import urls from '../../constants/urls';
@@ -71,14 +70,11 @@ const AllCourses = () => {
 		trash: null,
 	});
 
-	const [isFetchedOnce, setIsFetchedOnce] = useState<boolean>(false);
-
 	const courseQuery = useQuery(
 		['courseList', filterParams],
 		() => courseAPI.list(filterParams),
 		{
 			onSuccess: (data: any) => {
-				setIsFetchedOnce(true);
 				setCourseStatusCount({
 					any: data?.meta?.courses_count?.any,
 					publish: data?.meta?.courses_count?.publish,
@@ -182,135 +178,128 @@ const AllCourses = () => {
 				{courseCount !== null ? (
 					<Badge color="inherit">{courseCount}</Badge>
 				) : (
-					<SkeletonCircle size="4" ml="1" mb="1" />
+					<SkeletonCircle size="3" w="17px" ml="1" mb="1" rounded="sm" />
 				)}
 			</Button>
 		);
 	};
 
-	if (isFetchedOnce) {
-		return (
-			<Stack direction="column" spacing="8" alignItems="center">
-				<Header
-					thirdBtn={{
-						label: __('Add New Course', 'masteriyo'),
-						action: () => history.push(routes.courses.add),
-						icon: <Icon as={BiPlus} fontSize="md" />,
-					}}>
-					<List d="flex">
-						<ListItem mb="0">{courseStatusButton('any', BiBook)}</ListItem>
-						<ListItem mb="0">
-							{courseStatusButton('publish', BiBookOpen)}
-						</ListItem>
-						<ListItem mb="0">
-							{courseStatusButton('draft', BiBookBookmark)}
-						</ListItem>
-						<ListItem mb="0">{courseStatusButton('trash', BiTrash)}</ListItem>
-					</List>
-				</Header>
-				<Container maxW="container.xl">
-					<Box bg="white" py={{ base: 6, md: 12 }} shadow="box" mx="auto">
-						<Stack direction="column" spacing="10">
-							<CourseFilter
-								setFilterParams={setFilterParams}
-								filterParams={filterParams}
-								courseStatus={active}
-							/>
-
-							<Stack direction="column" spacing="8">
-								<Table>
-									<Thead>
-										<Tr>
-											<Th>{__('Title', 'masteriyo')}</Th>
-											<Th>{__('Categories', 'masteriyo')}</Th>
-											<Th>{__('Instructor', 'masteriyo')}</Th>
-											<Th>{__('Price', 'masteriyo')}</Th>
-											<Th>{__('Date', 'masteriyo')}</Th>
-											<Th>{__('Actions', 'masteriyo')}</Th>
-										</Tr>
-									</Thead>
-									<Tbody>
-										{courseQuery.isLoading && <SkeletonCourseList />}
-										{courseQuery.isSuccess &&
-										isEmpty(courseQuery?.data?.data) ? (
-											<EmptyInfo
-												message={__('No courses found.', 'masteriyo')}
-											/>
-										) : (
-											courseQuery?.data?.data?.map((course: any) => (
-												<CourseList
-													id={course?.id}
-													name={course?.name}
-													price={course?.price}
-													categories={course?.categories}
-													key={course?.id}
-													createdOn={course?.date_created}
-													permalink={course?.permalink}
-													editPostLink={course?.edit_post_link}
-													author={course?.author}
-													onDeletePress={onDeletePress}
-													onTrashPress={onTrashPress}
-													onRestorePress={onRestorePress}
-													status={course?.status}
-												/>
-											))
-										)}
-									</Tbody>
-								</Table>
-							</Stack>
-						</Stack>
-					</Box>
-					{courseQuery.isSuccess && !isEmpty(courseQuery?.data?.data) && (
-						<MasteriyoPagination
-							metaData={courseQuery?.data?.meta}
+	return (
+		<Stack direction="column" spacing="8" alignItems="center">
+			<Header
+				thirdBtn={{
+					label: __('Add New Course', 'masteriyo'),
+					action: () => history.push(routes.courses.add),
+					icon: <Icon as={BiPlus} fontSize="md" />,
+				}}>
+				<List d="flex">
+					<ListItem mb="0">{courseStatusButton('any', BiBook)}</ListItem>
+					<ListItem mb="0">
+						{courseStatusButton('publish', BiBookOpen)}
+					</ListItem>
+					<ListItem mb="0">
+						{courseStatusButton('draft', BiBookBookmark)}
+					</ListItem>
+					<ListItem mb="0">{courseStatusButton('trash', BiTrash)}</ListItem>
+				</List>
+			</Header>
+			<Container maxW="container.xl">
+				<Box bg="white" py={{ base: 6, md: 12 }} shadow="box" mx="auto">
+					<Stack direction="column" spacing="10">
+						<CourseFilter
 							setFilterParams={setFilterParams}
-							perPageText={__('Courses Per Page:', 'masteriyo')}
-							extraFilterParams={{
-								search: filterParams?.search,
-								status: filterParams?.status,
-								category: filterParams?.category,
-								price_type: filterParams?.price,
-							}}
+							filterParams={filterParams}
+							courseStatus={active}
 						/>
-					)}
-				</Container>
-				<AlertDialog
-					isOpen={isOpen}
-					onClose={onClose}
-					isCentered
-					leastDestructiveRef={cancelRef}>
-					<AlertDialogOverlay>
-						<AlertDialogContent>
-							<AlertDialogHeader>
-								{__('Deleting Course', 'masteriyo')} {name}
-							</AlertDialogHeader>
-							<AlertDialogBody>
-								{__(
-									'Are you sure? You can’t restore after deleting.',
-									'masteriyo'
-								)}
-							</AlertDialogBody>
-							<AlertDialogFooter>
-								<ButtonGroup>
-									<Button onClick={onClose} variant="outline" ref={cancelRef}>
-										{__('Cancel', 'masteriyo')}
-									</Button>
-									<Button
-										colorScheme="red"
-										isLoading={deleteCourse.isLoading}
-										onClick={onDeleteConfirm}>
-										{__('Delete', 'masteriyo')}
-									</Button>
-								</ButtonGroup>
-							</AlertDialogFooter>
-						</AlertDialogContent>
-					</AlertDialogOverlay>
-				</AlertDialog>
-			</Stack>
-		);
-	}
 
-	return <FullScreenLoader />;
+						<Stack direction="column" spacing="8">
+							<Table>
+								<Thead>
+									<Tr>
+										<Th>{__('Title', 'masteriyo')}</Th>
+										<Th>{__('Categories', 'masteriyo')}</Th>
+										<Th>{__('Instructor', 'masteriyo')}</Th>
+										<Th>{__('Price', 'masteriyo')}</Th>
+										<Th>{__('Date', 'masteriyo')}</Th>
+										<Th>{__('Actions', 'masteriyo')}</Th>
+									</Tr>
+								</Thead>
+								<Tbody>
+									{courseQuery.isLoading && <SkeletonCourseList />}
+									{courseQuery.isSuccess && isEmpty(courseQuery?.data?.data) ? (
+										<EmptyInfo message={__('No courses found.', 'masteriyo')} />
+									) : (
+										courseQuery?.data?.data?.map((course: any) => (
+											<CourseList
+												id={course?.id}
+												name={course?.name}
+												price={course?.price}
+												categories={course?.categories}
+												key={course?.id}
+												createdOn={course?.date_created}
+												permalink={course?.permalink}
+												editPostLink={course?.edit_post_link}
+												author={course?.author}
+												onDeletePress={onDeletePress}
+												onTrashPress={onTrashPress}
+												onRestorePress={onRestorePress}
+												status={course?.status}
+											/>
+										))
+									)}
+								</Tbody>
+							</Table>
+						</Stack>
+					</Stack>
+				</Box>
+				{courseQuery.isSuccess && !isEmpty(courseQuery?.data?.data) && (
+					<MasteriyoPagination
+						metaData={courseQuery?.data?.meta}
+						setFilterParams={setFilterParams}
+						perPageText={__('Courses Per Page:', 'masteriyo')}
+						extraFilterParams={{
+							search: filterParams?.search,
+							status: filterParams?.status,
+							category: filterParams?.category,
+							price_type: filterParams?.price,
+						}}
+					/>
+				)}
+			</Container>
+			<AlertDialog
+				isOpen={isOpen}
+				onClose={onClose}
+				isCentered
+				leastDestructiveRef={cancelRef}>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader>
+							{__('Deleting Course', 'masteriyo')} {name}
+						</AlertDialogHeader>
+						<AlertDialogBody>
+							{__(
+								'Are you sure? You can’t restore after deleting.',
+								'masteriyo'
+							)}
+						</AlertDialogBody>
+						<AlertDialogFooter>
+							<ButtonGroup>
+								<Button onClick={onClose} variant="outline" ref={cancelRef}>
+									{__('Cancel', 'masteriyo')}
+								</Button>
+								<Button
+									colorScheme="red"
+									isLoading={deleteCourse.isLoading}
+									onClick={onDeleteConfirm}>
+									{__('Delete', 'masteriyo')}
+								</Button>
+							</ButtonGroup>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+		</Stack>
+	);
 };
 
 export default AllCourses;

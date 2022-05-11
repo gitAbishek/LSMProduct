68460e3cd525b191eb1e8c6c571e776b954d2330
Aayush Dongol <70308478+ADongol123@users.noreply.github.com>
2022-05-11 10:08:13 +0545

    Responsive banner for addon page (#719)

--- assets/js/back-end/screens/add-ons/AddOns.tsx
+++ assets/js/back-end/screens/add-ons/AddOns.tsx
@@ -59,88 +59,92 @@ const AddOns = () => {
 					</Container>
 				</Box>
 			</Stack>
-			<Stack
-				width="100%"
-				h="auto"
-				py="4"
-				bg="linear-gradient(90.03deg, #4584FF 0.57%, #7761FF 99.97%)"
-				color="white">
+			<Stack direction="column" spacing="10" align="center">
+				<Box
+					w="full"
+					h="auto"
+					py="4"
+					bg="linear-gradient(90.03deg, #4584FF 0.57%, #7761FF 99.97%)"
+					color="white">
+					<Container maxW="container.xl">
+						<Stack
+							direction="row"
+							justifyContent="space-between"
+							flexWrap="wrap">
+							<Stack direction="row" spacing="4" flexWrap="wrap" mb="4px">
+								<Box pos="relative" w="300px" mb="4px">
+									<Heading
+										pos="absolute"
+										fontSize="md"
+										color="white"
+										left="25px"
+										top="8px">
+										{__('SPECIAL INTRODUCTORY OFFER', 'masteriyo')}
+									</Heading>
+									<Ribbon />
+								</Box>
+								<Text fontSize="15px" fontWeight="400">
+									{__('We are giving', 'masteriyo')}
+									<Center
+										d="inline-flex"
+										w="40px"
+										h="40px"
+										bg="rgba(255,255,255, 0.5)"
+										fontWeight="bold"
+										rounded="full">
+										75
+										<Text as="span" fontSize="xx-small">
+											%
+										</Text>
+									</Center>
+									{__('off on all our premium plans.', 'masteriyo')}
+								</Text>
+							</Stack>
+							<Stack direction="row" spacing="4">
+								<Link
+									href="https://masteriyo.com/wordpress-lms/pricing/"
+									target="_blank">
+									<Button color="black">{__('Buy now', 'masteriyo')}</Button>
+								</Link>
+								<Link
+									href="https://masteriyo.com/wordpress-lms/contact/"
+									target="_blank">
+									<Button
+										variant="unstyled"
+										textDecor="underline"
+										color="rgba(255,255,255,0.8)"
+										_hover={{ color: 'white' }}>
+										{__('Any Queries?', 'masteriyo')}
+									</Button>
+								</Link>
+							</Stack>
+						</Stack>
+					</Container>
+				</Box>
 				<Container maxW="container.xl">
-					<Stack direction="row" justifyContent="space-between">
-						<Stack direction="row" spacing="4">
-							<Box pos="relative" w="300px">
-								<Heading
-									pos="absolute"
-									fontSize="md"
-									color="white"
-									left="16px"
-									top="10px">
-									{__('SPECIAL INTRODUCTORY OFFER', 'masteriyo')}
-								</Heading>
-								<Ribbon />
-							</Box>
-
-							<Text fontSize="15px" fontWeight="400">
-								{__('We are giving', 'masteriyo')}
-								<Center
-									d="inline-flex"
-									w="40px"
-									h="40px"
-									bg="rgba(255,255,255, 0.5)"
-									fontWeight="bold"
-									rounded="full">
-									75
-									<Text as="span" fontSize="xx-small">
-										%
-									</Text>
-								</Center>
-								{__('off on all our premium plans.', 'masteriyo')}
-							</Text>
-						</Stack>
-						<Stack direction="row" spacing="5">
-							<Link
-								href="https://masteriyo.com/wordpress-lms/pricing/"
-								target="_blank">
-								<Button color="black">{__('Buy now', 'masteriyo')}</Button>
-							</Link>
-							<Link
-								href="https://masteriyo.com/wordpress-lms/contact/"
-								target="_blank">
-								<Button
-									variant="unstyled"
-									textDecor="underline"
-									color="rgba(255,255,255,0.8)"
-									_hover={{ color: 'white' }}>
-									{__('Any Queries?', 'masteriyo')}
-								</Button>
-							</Link>
-						</Stack>
-					</Stack>
+					<Row>
+						<Col md={3}>
+							<AddonItem
+								addOnName={__('Stripe', 'masteriyo')}
+								addOnDescription={__(
+									'Easily sell online courses and accept credit card payments via Stripe. It supports major cards like Visa, MasterCard, American Express, Discover, debit cards, etc.',
+									'masteriyo'
+								)}
+								thumbnailSrc={Stripe}
+							/>
+						</Col>
+						<Col md={3}>
+							<AddonItem
+								addOnName={__('Download Materials', 'masteriyo')}
+								addOnDescription={__(
+									'Attach unlimited lesson materials such as PDF, Doc, etc. Provide students with quick and easy access to download materials, and preview without download.',
+									'masteriyo'
+								)}
+								thumbnailSrc={DownloadMaterial}
+							/>
+						</Col>
+					</Row>
 				</Container>
-			</Stack>
-			<Stack width="container.xl" ml="auto" mr="auto" mt="10">
-				<Row>
-					<Col md={3}>
-						<AddonItem
-							addOnName={__('Stripe', 'masteriyo')}
-							addOnDescription={__(
-								'Easily sell online courses and accept credit card payments via Stripe. It supports major cards like Visa, MasterCard, American Express, Discover, debit cards, etc.',
-								'masteriyo'
-							)}
-							thumbnailSrc={Stripe}
-						/>
-					</Col>
-					<Col md={3}>
-						<AddonItem
-							addOnName={__('Download Materials', 'masteriyo')}
-							addOnDescription={__(
-								'Attach unlimited lesson materials such as PDF, Doc, etc. Provide students with quick and easy access to download materials, and preview without download.',
-								'masteriyo'
-							)}
-							thumbnailSrc={DownloadMaterial}
-						/>
-					</Col>
-				</Row>
 			</Stack>
 		</Tabs>
 	);

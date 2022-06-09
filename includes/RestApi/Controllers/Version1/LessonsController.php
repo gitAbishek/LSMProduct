f0662fe2382c8465f0c7fb6e912b1cd6f206ccbf
Sagar Tamang <mi5t4n@gmail.com>
2022-06-09 15:04:30 +0545

    Added force paramter to lessons DELETE rest endpoint

--- includes/RestApi/Controllers/Version1/LessonsController.php
+++ includes/RestApi/Controllers/Version1/LessonsController.php
@@ -127,6 +127,13 @@ class LessonsController extends PostsController {
 					'methods'             => \WP_REST_Server::DELETABLE,
 					'callback'            => array( $this, 'delete_item' ),
 					'permission_callback' => array( $this, 'delete_item_permissions_check' ),
+					'args'                => array(
+						'force' => array(
+							'default'     => false,
+							'description' => __( 'Whether to bypass trash and force deletion.', 'masteriyo' ),
+							'type'        => 'boolean',
+						),
+					),
 				),
 				'schema' => array( $this, 'get_public_item_schema' ),
 			)
